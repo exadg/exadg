966af7077d28ddce0555a77de05b801b293ae629
peterrum <peterrmuench@aol.com>
2018-03-05 08:07:45 +0100

    Introduce USE_DEAL_SPECTRUM preprocessor varibale and add empty body of DealSpectrumWrapper

--- include/incompressible_navier_stokes/postprocessor/energy_spectrum_calculation.h
+++ include/incompressible_navier_stokes/postprocessor/energy_spectrum_calculation.h
@@ -13,6 +13,29 @@
 #include <cmath>
 #include <memory>
 
+#ifdef USE_DEAL_SPECTRUM
+    #include "../../../3rdparty/deal.spectrum/src/deal-spectrum.h"
+#else
+    namespace dealspectrum{
+
+        class DealSpectrumWrapper{
+
+        public:
+            DealSpectrumWrapper(bool , bool ){}
+
+            virtual ~DealSpectrumWrapper() {}
+
+            void init(int , int , int , int , int ){ }
+
+            void execute(const double* ){ }
+
+            int get_results(double*& , double*& , double*& ){ return 0; } 
+
+        };
+
+    }
+#endif
+
 template<int dim, int fe_degree, typename Number>
 class KineticEnergySpectrumCalculator
 {
@@ -20,16 +43,24 @@ public:
   KineticEnergySpectrumCalculator()
     :
     clear_files(true),
-    matrix_free_data(nullptr)
+    dsw(false,true)
   {}
 
   void setup(MatrixFree<dim,Number> const    &matrix_free_data_in,
              DofQuadIndexData const          &dof_quad_index_data_in,
              KineticEnergySpectrumData const &data_in)
   {
-    matrix_free_data = &matrix_free_data_in;
-    dof_quad_index_data = dof_quad_index_data_in;
+      
     data = data_in;
+    
+    int local_cells = matrix_free_data_in.n_physical_cells();
+    int       cells = local_cells;
+    MPI_Allreduce(MPI_IN_PLACE, &cells, 1, MPI_INTEGER, MPI_SUM, MPI_COMM_WORLD);
+    cells = round(pow(cells,1.0/dim));
+    
+    //std::cout << local_cells << std::endl; std::cout << cells << std::endl; exit(0);
+    
+    dsw.init(dim, cells,fe_degree+1,fe_degree+1,local_cells);
 
   }
 
@@ -50,22 +81,49 @@ public:
 
 private:
   void do_evaluate(parallel::distributed::Vector<Number> const &velocity,
-                   double const                                time,
+                   double const                                ,
                    unsigned int const                          time_step_number)
   {
-    if((time_step_number-1)%data.calculate_every_time_steps == 0)
-    {
-      if(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD)==0)
-      {
-        std::cout << "Calculate kinetic energy spectrum" << std::endl;
-      }
-    }
+        if((time_step_number-1)%data.calculate_every_time_steps == 0) {
+            if(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD)==0) 
+                std::cout << "Calculate kinetic energy spectrum" << std::endl;
+            
+            const double& temp = velocity.local_element(0);
+            dsw.execute(&temp);
+
+            // write output file
+            if(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD)==0) {
+                std::ostringstream filename;
+                filename << data.filename_prefix;
+
+                std::ofstream f;
+                if(clear_files == true) {
+                    f.open(filename.str().c_str(),std::ios::trunc);
+                    f << "  Bin  Wave length          Energy" << std::endl;
+                    clear_files = false;
+                } else {
+                    f.open(filename.str().c_str(),std::ios::app);
+                }
+
+                // get tabularized results ...
+                double* kappa; double* E; double* C;
+                int len = dsw.get_results(kappa, E, C);
+                
+                // ... and print it line by line:
+                for(int i = 0; i < len; i++)
+                    //if(E[i] > data.output_precision)
+                        f << std::scientific << std::setprecision(0) << std::setw(2+ceil(std::max(3.0, log(len)/log(10)))) 
+                          << i
+                          << std::scientific << std::setprecision(precision) << std::setw(precision+8) 
+                          << kappa[i] << "   " << E[i] << std::endl;
+            }
+        }
   }
 
   bool clear_files;
-  MatrixFree<dim,Number> const * matrix_free_data;
-  DofQuadIndexData dof_quad_index_data;
+  dealspectrum::DealSpectrumWrapper dsw;
   KineticEnergySpectrumData data;
+  const unsigned int precision = 12;
 };
 
 
