87417f887609a568d3560e6257af71c197b1d86a
Martin Kronbichler <kronbichler@lnm.mw.tum.de>
2018-04-04 10:26:13 +0200

    Add possibility for curved manifold (currently disabled).

--- applications/incompressible_navier_stokes_test_cases/3D_taylor_green_vortex.h
+++ applications/incompressible_navier_stokes_test_cases/3D_taylor_green_vortex.h
@@ -268,7 +268,7 @@ void InputParametersNavierStokes<dim>::set_input_parameters()
   kinetic_energy_data.calculate_every_time_steps = 1;
   kinetic_energy_data.viscosity = VISCOSITY;
   kinetic_energy_data.filename_prefix = OUTPUT_FOLDER + OUTPUT_NAME;
-  
+
   kinetic_energy_spectrum_data.calculate = true;
   kinetic_energy_spectrum_data.calculate_every_time_steps = 100;
   kinetic_energy_spectrum_data.filename_prefix = OUTPUT_FOLDER + "spectrum";
@@ -375,6 +375,89 @@ template<int dim>
  }
 
 
+template <int dim>
+class DeformedCubeManifold : public ChartManifold<dim,dim,dim>
+{
+public:
+  DeformedCubeManifold(const double left,
+                       const double right,
+                       const double deformation,
+                       const unsigned int frequency = 1)
+    :
+    left(left),
+    right(right),
+    deformation(deformation),
+    frequency(frequency)
+  {}
+
+  Point<dim> push_forward(const Point<dim> &chart_point) const
+  {
+    double sinval = deformation;
+    for (unsigned int d=0; d<dim; ++d)
+      sinval *= std::sin(frequency*numbers::PI*(chart_point(d)-left)/(right-left));
+    Point<dim> space_point;
+    for (unsigned int d=0; d<dim; ++d)
+      space_point(d) = chart_point(d) + sinval;
+    return space_point;
+  }
+
+  Point<dim> pull_back(const Point<dim> &space_point) const
+  {
+    Point<dim> x = space_point;
+    Point<dim> one;
+    for (unsigned int d=0; d<dim; ++d)
+      one(d) = 1.;
+
+    // Newton iteration to solve the nonlinear equation given by the point
+    Tensor<1,dim> sinvals;
+    for (unsigned int d=0; d<dim; ++d)
+      sinvals[d] = std::sin(frequency*numbers::PI*(x(d)-left)/(right-left));
+
+    double sinval = deformation;
+    for (unsigned int d=0; d<dim; ++d)
+      sinval *= sinvals[d];
+    Tensor<1,dim> residual = space_point - x - sinval*one;
+    unsigned int its = 0;
+    while (residual.norm() > 1e-12 && its < 100)
+      {
+        Tensor<2,dim> jacobian;
+        for (unsigned int d=0; d<dim; ++d)
+          jacobian[d][d] = 1.;
+        for (unsigned int d=0; d<dim; ++d)
+          {
+            double sinval_der = deformation * frequency / (right-left) * numbers::PI *
+              std::cos(frequency*numbers::PI*(x(d)-left)/(right-left));
+            for (unsigned int e=0; e<dim; ++e)
+              if (e!=d)
+                sinval_der *= sinvals[e];
+            for (unsigned int e=0; e<dim; ++e)
+              jacobian[e][d] += sinval_der;
+          }
+
+        x += invert(jacobian) * residual;
+
+        for (unsigned int d=0; d<dim; ++d)
+          sinvals[d] = std::sin(frequency*numbers::PI*(x(d)-left)/(right-left));
+
+        sinval = deformation;
+        for (unsigned int d=0; d<dim; ++d)
+          sinval *= sinvals[d];
+        residual = space_point - x - sinval*one;
+        ++its;
+      }
+    AssertThrow (residual.norm() < 1e-12,
+                 ExcMessage("Newton for point did not converge."));
+    return x;
+  }
+
+private:
+  const double left;
+  const double right;
+  const double deformation;
+  const unsigned int frequency;
+};
+
+
 /**************************************************************************************/
 /*                                                                                    */
 /*         GENERATE GRID, SET BOUNDARY INDICATORS AND FILL BOUNDARY DESCRIPTOR        */
@@ -393,6 +476,9 @@ void create_grid_and_set_boundary_conditions(
   const double pi = numbers::PI;
   const double left = - pi * L, right = pi * L;
   GridGenerator::hyper_cube(triangulation,left,right);
+  //triangulation.set_all_manifold_ids(1);
+  //static DeformedCubeManifold<dim> manifold(left, right, 0.5, 2);
+  //triangulation.set_manifold(1, manifold);
 
   AssertThrow(dim == 3, ExcMessage("This test case can only be used for dim==3!"));
   // periodicity in x-,y-, and z-direction
