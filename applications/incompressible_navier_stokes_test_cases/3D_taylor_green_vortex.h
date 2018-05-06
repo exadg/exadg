6be4b904d782eea427ee354b7e3f9f244ccddc43
peterrum <peterrmuench@aol.com>
2018-05-06 14:52:22 +0200

    Correct boundary treatment in 3D_taylor_green_vortex (incompressible) for N_CELLS_1D_COARSE_GRID>1

--- applications/incompressible_navier_stokes_test_cases/3D_taylor_green_vortex.h
+++ applications/incompressible_navier_stokes_test_cases/3D_taylor_green_vortex.h
@@ -424,17 +424,29 @@ void create_grid_and_set_boundary_conditions(
   }
 
   AssertThrow(dim == 3, ExcMessage("This test case can only be used for dim==3!"));
-  // periodicity in x-,y-, and z-direction
-  // x-direction
-  triangulation.begin()->face(0)->set_all_boundary_ids(0);
-  triangulation.begin()->face(1)->set_all_boundary_ids(1);
-  // x-direction
-  triangulation.begin()->face(2)->set_all_boundary_ids(2);
-  triangulation.begin()->face(3)->set_all_boundary_ids(3);
-  // z-direction
-  triangulation.begin()->face(4)->set_all_boundary_ids(4);
-  triangulation.begin()->face(5)->set_all_boundary_ids(5);
 
+   typename Triangulation<dim>::cell_iterator cell = triangulation.begin(), endc = triangulation.end();
+   for(;cell!=endc;++cell)
+   {
+     for(unsigned int face_number=0;face_number < GeometryInfo<dim>::faces_per_cell;++face_number)
+     {
+       // x-direction
+       if((std::fabs(cell->face(face_number)->center()(0) - left)< 1e-12))
+         cell->face(face_number)->set_all_boundary_ids (0);
+       else if((std::fabs(cell->face(face_number)->center()(0) - right)< 1e-12))
+         cell->face(face_number)->set_all_boundary_ids (1);
+       // y-direction
+       else if((std::fabs(cell->face(face_number)->center()(1) - left)< 1e-12))
+         cell->face(face_number)->set_all_boundary_ids (2);
+       else if((std::fabs(cell->face(face_number)->center()(1) - right)< 1e-12))
+         cell->face(face_number)->set_all_boundary_ids (3);
+       // z-direction
+       else if((std::fabs(cell->face(face_number)->center()(2) - left)< 1e-12))
+         cell->face(face_number)->set_all_boundary_ids (4);
+       else if((std::fabs(cell->face(face_number)->center()(2) - right)< 1e-12))
+         cell->face(face_number)->set_all_boundary_ids (5);
+     }
+   }
   GridTools::collect_periodic_faces(triangulation, 0, 1, 0 /*x-direction*/, periodic_faces);
   GridTools::collect_periodic_faces(triangulation, 2, 3, 1 /*y-direction*/, periodic_faces);
   GridTools::collect_periodic_faces(triangulation, 4, 5, 2 /*z-direction*/, periodic_faces);
