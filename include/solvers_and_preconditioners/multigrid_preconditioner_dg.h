3e8f93d6629463abd33546a527849f4a1a8d95e2
peterrum <peterrmuench@aol.com>
2018-07-01 20:10:18 +0200

    Add initialize_mg_constrained_dofs and initialize_mg_matrix to MyMultigridPreconditionerDG

--- include/solvers_and_preconditioners/multigrid_preconditioner_dg.h
+++ include/solvers_and_preconditioners/multigrid_preconditioner_dg.h
@@ -18,19 +18,30 @@ public:
   MyMultigridPreconditionerDG() : 
     MyMultigridPreconditionerBase<dim,value_type,Operator>(std::shared_ptr<Operator>(new Operator())){}
 
+  const Mapping<dim> *mapping;
+  const UnderlyingOperator* underlying_operator;
+  
+  typedef MyMultigridPreconditionerBase<dim, value_type, Operator> BASE;
+  typedef std::vector<GridTools::PeriodicFacePair<typename
+                    Triangulation<dim>::cell_iterator> > VectorPeriodicFacePair;
+  
+  const VectorPeriodicFacePair* periodic_face_pairs_level0;
+    
   virtual ~MyMultigridPreconditionerDG(){};
 
   virtual void update(MatrixOperatorBase const * /*matrix_operator*/){}
 
-  void initialize(const MultigridData                                     &/*mg_data_in*/,
-                  const DoFHandler<dim>                                   &/*dof_handler*/,
-                  const Mapping<dim>                                      &/*mapping*/,
-                  const UnderlyingOperator                                &/*underlying_operator*/,
-                  const std::vector<GridTools::PeriodicFacePair<typename
-                    Triangulation<dim>::cell_iterator> >                  &/*periodic_face_pairs_level0*/)
+  void initialize(const MultigridData          &mg_data_in,
+                  const DoFHandler<dim>        &dof_handler,
+                  const Mapping<dim>           &mapping,
+                  const UnderlyingOperator     &underlying_operator,
+                  const VectorPeriodicFacePair &periodic_face_pairs_level0)
   {
+    // save mg-setup
+    this->mapping = &mapping;
+    this->underlying_operator = &  underlying_operator;
+    this->periodic_face_pairs_level0 = &periodic_face_pairs_level0;
       
-      AssertThrow(false, ExcMessage("Function not implemented yet!"));
 //    this->mg_data = mg_data_in;
 //
 //    const parallel::Triangulation<dim> *tria =
@@ -49,34 +60,28 @@ public:
 //    this->initialize_mg_transfer(dof_handler, periodic_face_pairs_level0);
 //
 //    this->initialize_multigrid_preconditioner(dof_handler);
+      BASE::initialize(mg_data_in, dof_handler);
   }
 
 private:
   void initialize_mg_constrained_dofs(const DoFHandler<dim> &dof_handler,
-                                      MGConstrainedDoFs &/*constrained_dofs*/)
+                                      MGConstrainedDoFs &constrained_dofs)
   {
     // initialize mg_contstrained_dofs
-    this->mg_constrained_dofs.clear();
-    this->mg_constrained_dofs.initialize(dof_handler);
+    constrained_dofs.clear();
+    constrained_dofs.initialize(dof_handler);
   }
 
   /*
    *  This function initializes mg_matrices on all levels with level >= 0.
    */
-  virtual void initialize_mg_matrices(const DoFHandler<dim>                           &dof_handler,
-                                      const Mapping<dim>                              &mapping,
-                                      const UnderlyingOperator                        &underlying_operator,
-                                      const std::vector<GridTools::PeriodicFacePair<
-                                        typename Triangulation<dim>::cell_iterator> > &periodic_face_pairs_level0)
+  virtual void initialize_mg_matrix(const DoFHandler<dim> &dof_handler,
+        Operator * matrix, int /*level*/, int tria_level)
   {
-    // resize
-    this->mg_matrices.resize(0, this->n_global_levels-1);
+      
+      matrix->initialize_mg_matrix(tria_level, dof_handler, 
+              *this->mapping, *this->underlying_operator, *this->periodic_face_pairs_level0);
 
-    // initialize mg_matrices on all levels
-    for (unsigned int level = 0; level<this->n_global_levels; ++level)
-    {
-      this->mg_matrices[level]->initialize_mg_matrix(level, dof_handler, mapping, underlying_operator, periodic_face_pairs_level0);
-    }
   }
 };
 
