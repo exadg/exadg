49a515c6f7b420a18101737a09d0658396d76524
peterrum <peterrmuench@aol.com>
2018-07-04 14:13:38 +0200

    Move more implementation to diffusive_operator.cpp and format code

--- include/convection_diffusion/spatial_discretization/operators/diffusive_operator.cpp
+++ include/convection_diffusion/spatial_discretization/operators/diffusive_operator.cpp
@@ -3,12 +3,170 @@
 namespace ConvDiff {
 
 template <int dim, int fe_degree, typename value_type>
+void DiffusiveOperator<dim, fe_degree, value_type>::initialize(
+    Mapping<dim> const &mapping, MatrixFree<dim, value_type> const &mf_data,
+    DiffusiveOperatorData<dim> const &operator_data_in) {
+  ConstraintMatrix cm;
+  Parent::reinit(mf_data, cm, operator_data_in);
+
+  IP::calculate_penalty_parameter<dim, fe_degree, value_type>(
+      array_penalty_parameter, *this->data, mapping, this->ad.dof_index);
+
+  diffusivity = this->ad.diffusivity;
+}
+
+template <int dim, int fe_degree, typename value_type>
+void DiffusiveOperator<dim, fe_degree, value_type>::apply_add(
+    VNumber &dst, VNumber const &src) const {
+  AssertThrow(diffusivity > 0.0, ExcMessage("Diffusivity is not set!"));
+  Parent::apply_add(dst, src);
+}
+
+template <int dim, int fe_degree, typename value_type>
+inline DEAL_II_ALWAYS_INLINE VectorizedArray<value_type>
+DiffusiveOperator<dim, fe_degree, value_type>::calculate_value_flux(
+    VectorizedArray<value_type> const &jump_value) const {
+  return -0.5 * diffusivity * jump_value;
+}
+
+template <int dim, int fe_degree, typename value_type>
 void DiffusiveOperator<dim, fe_degree, value_type>::do_cell_integral(
     FEEvalCell &fe_eval) const {
 
   for (unsigned int q = 0; q < fe_eval.n_q_points; ++q)
     fe_eval.submit_gradient(fe_eval.get_gradient(q)*diffusivity,q);
   
+}
+
+template <int dim, int fe_degree, typename value_type>
+inline DEAL_II_ALWAYS_INLINE VectorizedArray<value_type>
+DiffusiveOperator<dim, fe_degree, value_type>::calculate_interior_value(
+    unsigned int const q, FEEvalFace const &fe_eval,
+    OperatorType const &operator_type) const {
+  VectorizedArray<value_type> value_m = make_vectorized_array<value_type>(0.0);
+
+  if (operator_type == OperatorType::full ||
+      operator_type == OperatorType::homogeneous) {
+    value_m = fe_eval.get_value(q);
+  } else if (operator_type == OperatorType::inhomogeneous) {
+    value_m = make_vectorized_array<value_type>(0.0);
+  } else {
+    AssertThrow(false,
+                ExcMessage("Specified OperatorType is not implemented!"));
+  }
+
+  return value_m;
+}
+
+template <int dim, int fe_degree, typename value_type>
+inline DEAL_II_ALWAYS_INLINE VectorizedArray<value_type>
+DiffusiveOperator<dim, fe_degree, value_type>::calculate_exterior_value(
+    VectorizedArray<value_type> const &value_m, unsigned int const q,
+    FEEvalFace const &fe_eval, OperatorType const &operator_type,
+    BoundaryType const &boundary_type,
+    types::boundary_id const boundary_id) const {
+  VectorizedArray<value_type> value_p = make_vectorized_array<value_type>(0.0);
+
+  if (boundary_type == BoundaryType::dirichlet) {
+    if (operator_type == OperatorType::full ||
+        operator_type == OperatorType::inhomogeneous) {
+      VectorizedArray<value_type> g = make_vectorized_array<value_type>(0.0);
+      typename std::map<types::boundary_id,
+                        std::shared_ptr<Function<dim>>>::iterator it;
+      it = this->ad.bc->dirichlet_bc.find(boundary_id);
+      Point<dim, VectorizedArray<value_type>> q_points =
+          fe_eval.quadrature_point(q);
+      evaluate_scalar_function(g, it->second, q_points, this->eval_time);
+
+      value_p = -value_m + 2.0 * g;
+    } else if (operator_type == OperatorType::homogeneous) {
+      value_p = -value_m;
+    } else {
+      AssertThrow(false,
+                  ExcMessage("Specified OperatorType is not implemented!"));
+    }
+  } else if (boundary_type == BoundaryType::neumann) {
+    value_p = value_m;
+  } else {
+    AssertThrow(
+        false,
+        ExcMessage("Boundary type of face is invalid or not implemented."));
+  }
+
+  return value_p;
+}
+
+template <int dim, int fe_degree, typename value_type>
+inline DEAL_II_ALWAYS_INLINE VectorizedArray<value_type>
+DiffusiveOperator<dim, fe_degree, value_type>::calculate_gradient_flux(
+    VectorizedArray<value_type> const &normal_gradient_m,
+    VectorizedArray<value_type> const &normal_gradient_p,
+    VectorizedArray<value_type> const &jump_value,
+    VectorizedArray<value_type> const &penalty_parameter) const {
+  return diffusivity * 0.5 * (normal_gradient_m + normal_gradient_p) -
+         diffusivity * penalty_parameter * jump_value;
+}
+
+template <int dim, int fe_degree, typename value_type>
+inline DEAL_II_ALWAYS_INLINE VectorizedArray<value_type>
+DiffusiveOperator<dim, fe_degree, value_type>::
+    calculate_interior_normal_gradient(
+        unsigned int const q, FEEvalFace const &fe_eval,
+        OperatorType const &operator_type) const {
+  VectorizedArray<value_type> normal_gradient_m =
+      make_vectorized_array<value_type>(0.0);
+
+  if (operator_type == OperatorType::full ||
+      operator_type == OperatorType::homogeneous) {
+    normal_gradient_m = fe_eval.get_normal_gradient(q);
+  } else if (operator_type == OperatorType::inhomogeneous) {
+    normal_gradient_m = make_vectorized_array<value_type>(0.0);
+  } else {
+    AssertThrow(false,
+                ExcMessage("Specified OperatorType is not implemented!"));
+  }
+
+  return normal_gradient_m;
+}
+
+template <int dim, int fe_degree, typename value_type>
+inline DEAL_II_ALWAYS_INLINE VectorizedArray<value_type>
+DiffusiveOperator<dim, fe_degree, value_type>::
+    calculate_exterior_normal_gradient(
+        VectorizedArray<value_type> const &normal_gradient_m,
+        unsigned int const q, FEEvalFace const &fe_eval,
+        OperatorType const &operator_type, BoundaryType const &boundary_type,
+        types::boundary_id const boundary_id) const {
+  VectorizedArray<value_type> normal_gradient_p =
+      make_vectorized_array<value_type>(0.0);
+
+  if (boundary_type == BoundaryType::dirichlet) {
+    normal_gradient_p = normal_gradient_m;
+  } else if (boundary_type == BoundaryType::neumann) {
+    if (operator_type == OperatorType::full ||
+        operator_type == OperatorType::inhomogeneous) {
+      VectorizedArray<value_type> h = make_vectorized_array<value_type>(0.0);
+      typename std::map<types::boundary_id,
+                        std::shared_ptr<Function<dim>>>::iterator it;
+      it = this->ad.bc->neumann_bc.find(boundary_id);
+      Point<dim, VectorizedArray<value_type>> q_points =
+          fe_eval.quadrature_point(q);
+      evaluate_scalar_function(h, it->second, q_points, this->eval_time);
+
+      normal_gradient_p = -normal_gradient_m + 2.0 * h;
+    } else if (operator_type == OperatorType::homogeneous) {
+      normal_gradient_p = -normal_gradient_m;
+    } else {
+      AssertThrow(false,
+                  ExcMessage("Specified OperatorType is not implemented!"));
+    }
+  } else {
+    AssertThrow(
+        false,
+        ExcMessage("Boundary type of face is invalid or not implemented."));
+  }
+
+  return normal_gradient_p;
 }
 
 template <int dim, int fe_degree, typename value_type>
@@ -37,7 +195,6 @@ void DiffusiveOperator<dim, fe_degree, value_type>::do_face_integral(
     fe_eval.submit_value(-gradient_flux, q);
     fe_eval_neighbor.submit_value(gradient_flux, q); // + sign since n⁺ = -n⁻
   }
-
 }
 
 template <int dim, int fe_degree, typename value_type>
@@ -64,7 +221,6 @@ void DiffusiveOperator<dim, fe_degree, value_type>::do_face_int_integral(
     fe_eval.submit_normal_gradient(value_flux, q);
     fe_eval.submit_value(-gradient_flux, q);
   }
-
 }
 
 template <int dim, int fe_degree, typename value_type>
@@ -93,7 +249,10 @@ void DiffusiveOperator<dim, fe_degree, value_type>::do_face_ext_integral(
                                             q); // minus sign since n⁺ = -n⁻
     fe_eval_neighbor.submit_value(-gradient_flux, q);
   }
+<<<<<<< HEAD
 
+=======
+>>>>>>> Move more implementation to diffusive_operator.cpp and format code
 }
 
 template <int dim, int fe_degree, typename value_type>
@@ -126,6 +285,9 @@ void DiffusiveOperator<dim, fe_degree, value_type>::do_boundary_integral(
     fe_eval.submit_normal_gradient(value_flux, q);
     fe_eval.submit_value(-gradient_flux, q);
   }
+<<<<<<< HEAD
 
+=======
+>>>>>>> Move more implementation to diffusive_operator.cpp and format code
 }
 }
--- include/convection_diffusion/spatial_discretization/operators/diffusive_operator.h
+++ include/convection_diffusion/spatial_discretization/operators/diffusive_operator.h
@@ -3,65 +3,48 @@
 
 #include "../../../operators/operation_base.h"
 
-namespace ConvDiff
-{
-template<int dim>
-struct DiffusiveOperatorData : public OperatorBaseData<dim, BoundaryType, OperatorType, ConvDiff::BoundaryDescriptor<dim>> 
-{
-  DiffusiveOperatorData ()
-    : OperatorBaseData<dim, BoundaryType, OperatorType, ConvDiff::BoundaryDescriptor<dim>>(
-        0, 0, false, true, false, false, true, false,
-                              true, true, true, true, // face
-                              true, true, true, true  // boundary
-                              ), IP_factor(1.0), diffusivity(1.0) {}
+namespace ConvDiff {
+template <int dim>
+struct DiffusiveOperatorData
+    : public OperatorBaseData<dim, BoundaryType, OperatorType,
+                              ConvDiff::BoundaryDescriptor<dim>> {
+  DiffusiveOperatorData()
+      : OperatorBaseData<dim, BoundaryType, OperatorType,
+                         ConvDiff::BoundaryDescriptor<dim>>(
+            0, 0, false, true, false, false, true, false, // cell
+            true, true, true, true,                       // face
+            true, true, true, true                        // boundary
+            ),
+        IP_factor(1.0), diffusivity(1.0) {}
 
   double IP_factor;
   double diffusivity;
 };
 
-
-
 template <int dim, int fe_degree, typename value_type>
-class DiffusiveOperator 
-    : public OperatorBase<dim, fe_degree, value_type, DiffusiveOperatorData<dim>>
-{
+class DiffusiveOperator : public OperatorBase<dim, fe_degree, value_type,
+                                              DiffusiveOperatorData<dim>> {
 public:
   typedef DiffusiveOperator<dim, fe_degree, value_type> This;
-  typedef OperatorBase<dim, fe_degree, value_type, DiffusiveOperatorData<dim>> Parent;
+  typedef OperatorBase<dim, fe_degree, value_type, DiffusiveOperatorData<dim>>
+      Parent;
   typedef typename Parent::FEEvalCell FEEvalCell;
   typedef typename Parent::FEEvalFace FEEvalFace;
   typedef typename Parent::VNumber VNumber;
 
-  DiffusiveOperator() : /*data(nullptr),*/ diffusivity(-1.0) {}
+  DiffusiveOperator() : diffusivity(-1.0) {}
 
-  void initialize(Mapping<dim> const               &mapping,
-                  MatrixFree<dim,value_type> const &mf_data,
-                  DiffusiveOperatorData<dim> const &operator_data_in)
-  {
-      ConstraintMatrix cm;
-      Parent::reinit(mf_data, cm, operator_data_in);
+  void initialize(Mapping<dim> const &mapping,
+                  MatrixFree<dim, value_type> const &mf_data,
+                  DiffusiveOperatorData<dim> const &operator_data_in);
 
-    IP::calculate_penalty_parameter<dim, fe_degree, value_type>(array_penalty_parameter,
-                                                                *this->data,
-                                                                mapping,
-                                                                this->ad.dof_index);
+  void apply_add(VNumber &dst, VNumber const &src) const;
 
-    diffusivity = this->ad.diffusivity;
-  }
-
-  void apply_add(VNumber &dst, VNumber const &src) const{
-      AssertThrow(diffusivity > 0.0, ExcMessage("Diffusivity is not set!"));
-      Parent::apply_add(dst, src);
-  }
-  
   /*
    *  Calculation of "value_flux".
    */
   inline DEAL_II_ALWAYS_INLINE VectorizedArray<value_type>
-  calculate_value_flux (VectorizedArray<value_type> const &jump_value) const
-  {
-    return -0.5 * diffusivity * jump_value;
-  }
+  calculate_value_flux(VectorizedArray<value_type> const &jump_value) const;
 
   /*
    *  The following two functions calculate the interior_value/exterior_value
@@ -78,90 +61,32 @@ public:
    *  | inhomogeneous operator  | phi⁻ = 0, phi⁺ = 2g  | phi⁻ = 0, phi⁺ = 0 |
    *  +-------------------------+----------------------+--------------------+
    */
-  template<typename FEEvaluation>
   inline DEAL_II_ALWAYS_INLINE VectorizedArray<value_type>
-  calculate_interior_value(unsigned int const q,
-                           FEEvaluation const &fe_eval,
-                           OperatorType const &operator_type) const
-  {
-    VectorizedArray<value_type> value_m = make_vectorized_array<value_type>(0.0);
+  calculate_interior_value(unsigned int const q, FEEvalFace const &fe_eval,
+                           OperatorType const &operator_type) const;
 
-    if(operator_type == OperatorType::full ||
-       operator_type == OperatorType::homogeneous)
-    {
-      value_m = fe_eval.get_value(q);
-    }
-    else if(operator_type == OperatorType::inhomogeneous)
-    {
-      value_m = make_vectorized_array<value_type>(0.0);
-    }
-    else
-    {
-      AssertThrow(false, ExcMessage("Specified OperatorType is not implemented!"));
-    }
-
-    return value_m;
-  }
-
-  template<typename FEEvaluation>
   inline DEAL_II_ALWAYS_INLINE VectorizedArray<value_type>
   calculate_exterior_value(VectorizedArray<value_type> const &value_m,
-                           unsigned int const                q,
-                           FEEvaluation const                &fe_eval,
-                           OperatorType const                &operator_type,
-                           BoundaryType const                &boundary_type,
-                           types::boundary_id const          boundary_id) const
-  {
-    VectorizedArray<value_type> value_p = make_vectorized_array<value_type>(0.0);
-
-    if(boundary_type == BoundaryType::dirichlet)
-    {
-      if(operator_type == OperatorType::full ||
-         operator_type == OperatorType::inhomogeneous)
-      {
-        VectorizedArray<value_type> g = make_vectorized_array<value_type>(0.0);
-        typename std::map<types::boundary_id,std::shared_ptr<Function<dim> > >::iterator it;
-        it = this->ad.bc->dirichlet_bc.find(boundary_id);
-        Point<dim,VectorizedArray<value_type> > q_points = fe_eval.quadrature_point(q);
-        evaluate_scalar_function(g, it->second, q_points, this->eval_time);
-
-        value_p = - value_m + 2.0*g;
-      }
-      else if(operator_type == OperatorType::homogeneous)
-      {
-        value_p = - value_m;
-      }
-      else
-      {
-        AssertThrow(false, ExcMessage("Specified OperatorType is not implemented!"));
-      }
-    }
-    else if(boundary_type == BoundaryType::neumann)
-    {
-      value_p = value_m;
-    }
-    else
-    {
-      AssertThrow(false, ExcMessage("Boundary type of face is invalid or not implemented."));
-    }
-
-    return value_p;
-  }
+                           unsigned int const q, FEEvalFace const &fe_eval,
+                           OperatorType const &operator_type,
+                           BoundaryType const &boundary_type,
+                           types::boundary_id const boundary_id) const;
 
   /*
-   *  Calculation of gradient flux. Strictly speaking, this value is not a numerical flux since
-   *  the flux is multiplied by the normal vector, i.e., "gradient_flux" = numerical_flux * normal,
+   *  Calculation of gradient flux. Strictly speaking, this value is not a
+   * numerical flux since
+   *  the flux is multiplied by the normal vector, i.e., "gradient_flux" =
+   * numerical_flux * normal,
    *  where normal denotes the normal vector of element e⁻.
    */
   inline DEAL_II_ALWAYS_INLINE VectorizedArray<value_type>
-  calculate_gradient_flux (VectorizedArray<value_type> const &normal_gradient_m,
-                           VectorizedArray<value_type> const &normal_gradient_p,
-                           VectorizedArray<value_type> const &jump_value,
-                           VectorizedArray<value_type> const &penalty_parameter) const
-  {
-    return diffusivity * 0.5 * (normal_gradient_m + normal_gradient_p) - diffusivity * penalty_parameter * jump_value;
-  }
+  calculate_gradient_flux(
+      VectorizedArray<value_type> const &normal_gradient_m,
+      VectorizedArray<value_type> const &normal_gradient_p,
+      VectorizedArray<value_type> const &jump_value,
+      VectorizedArray<value_type> const &penalty_parameter) const;
 
+  // clang-format off
   /*
    *  The following two functions calculate the interior/exterior velocity gradient
    *  in normal direction depending on the operator type, the type of the boundary face
@@ -187,91 +112,37 @@ public:
    *  | inhomogeneous operator  | {{grad(phi)}}*n = 0                 | {{grad(phi)}}*n = h                   |
    *  +-------------------------+-------------------------------------+---------------------------------------+
    */
-  template<typename FEEvaluation>
+  // clang-format on
   inline DEAL_II_ALWAYS_INLINE VectorizedArray<value_type>
   calculate_interior_normal_gradient(unsigned int const q,
-                                     FEEvaluation const &fe_eval,
-                                     OperatorType const &operator_type) const
-  {
-    VectorizedArray<value_type> normal_gradient_m = make_vectorized_array<value_type>(0.0);
+                                     FEEvalFace const &fe_eval,
+                                     OperatorType const &operator_type) const;
 
-    if(operator_type == OperatorType::full ||
-       operator_type == OperatorType::homogeneous)
-    {
-      normal_gradient_m = fe_eval.get_normal_gradient(q);
-    }
-    else if(operator_type == OperatorType::inhomogeneous)
-    {
-      normal_gradient_m = make_vectorized_array<value_type>(0.0);
-    }
-    else
-    {
-      AssertThrow(false, ExcMessage("Specified OperatorType is not implemented!"));
-    }
-
-    return normal_gradient_m;
-  }
-
-  template<typename FEEvaluation>
   inline DEAL_II_ALWAYS_INLINE VectorizedArray<value_type>
-  calculate_exterior_normal_gradient(VectorizedArray<value_type> const &normal_gradient_m,
-                                     unsigned int const                q,
-                                     FEEvaluation const                &fe_eval,
-                                     OperatorType const                &operator_type,
-                                     BoundaryType const                &boundary_type,
-                                     types::boundary_id const          boundary_id) const
-  {
-    VectorizedArray<value_type> normal_gradient_p = make_vectorized_array<value_type>(0.0);
-
-    if(boundary_type == BoundaryType::dirichlet)
-    {
-      normal_gradient_p = normal_gradient_m;
-    }
-    else if(boundary_type == BoundaryType::neumann)
-    {
-      if(operator_type == OperatorType::full ||
-         operator_type == OperatorType::inhomogeneous)
-      {
-        VectorizedArray<value_type> h = make_vectorized_array<value_type>(0.0);
-        typename std::map<types::boundary_id,std::shared_ptr<Function<dim> > >::iterator it;
-        it = this->ad.bc->neumann_bc.find(boundary_id);
-        Point<dim,VectorizedArray<value_type> > q_points = fe_eval.quadrature_point(q);
-        evaluate_scalar_function(h, it->second, q_points, this->eval_time);
-
-        normal_gradient_p = -normal_gradient_m + 2.0 * h;
-      }
-      else if(operator_type == OperatorType::homogeneous)
-      {
-        normal_gradient_p = -normal_gradient_m;
-      }
-      else
-      {
-        AssertThrow(false, ExcMessage("Specified OperatorType is not implemented!"));
-      }
-    }
-    else
-    {
-      AssertThrow(false,ExcMessage("Boundary type of face is invalid or not implemented."));
-    }
-
-    return normal_gradient_p;
-  }
+  calculate_exterior_normal_gradient(
+      VectorizedArray<value_type> const &normal_gradient_m,
+      unsigned int const q, FEEvalFace const &fe_eval,
+      OperatorType const &operator_type, BoundaryType const &boundary_type,
+      types::boundary_id const boundary_id) const;
 
   void do_cell_integral(FEEvalCell &fe_eval) const;
 
-  void do_face_integral(FEEvalFace &fe_eval,FEEvalFace &fe_eval_neighbor) const;
+  void do_face_integral(FEEvalFace &fe_eval,
+                        FEEvalFace &fe_eval_neighbor) const;
 
-  void do_face_int_integral(FEEvalFace &fe_eval,FEEvalFace &fe_eval_neighbor) const;
+  void do_face_int_integral(FEEvalFace &fe_eval,
+                            FEEvalFace &fe_eval_neighbor) const;
 
-  void do_face_ext_integral(FEEvalFace &fe_eval,FEEvalFace &fe_eval_neighbor) const;
+  void do_face_ext_integral(FEEvalFace &fe_eval,
+                            FEEvalFace &fe_eval_neighbor) const;
 
-  void do_boundary_integral(FEEvalFace &fe_eval, OperatorType const &operator_type, 
-          types::boundary_id const &boundary_id) const;
+  void do_boundary_integral(FEEvalFace &fe_eval,
+                            OperatorType const &operator_type,
+                            types::boundary_id const &boundary_id) const;
 
-  AlignedVector<VectorizedArray<value_type> > array_penalty_parameter;
+  AlignedVector<VectorizedArray<value_type>> array_penalty_parameter;
   double diffusivity;
 };
-    
 }
 
 #include "diffusive_operator.cpp"
