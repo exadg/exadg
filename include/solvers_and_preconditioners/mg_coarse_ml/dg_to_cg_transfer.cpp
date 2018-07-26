27303a3ed28d675fb43f77b593f49198f2c874e9
peterrum <peterrmuench@aol.com>
2018-07-26 13:42:31 +0200

    Fix reordering in CGToDGTransfer

--- include/solvers_and_preconditioners/mg_coarse_ml/dg_to_cg_transfer.cpp
+++ include/solvers_and_preconditioners/mg_coarse_ml/dg_to_cg_transfer.cpp
@@ -70,12 +70,14 @@ void CGToDGTransfer<dim, Number>::transfer(VNumber &dst, const VNumber &src,
         // bring dof_values into the right order
         // (needed: numbering of shape functions of fe_q and fe_dgq different)
         for (unsigned int j = 0; j < temp_src.size(); j++)
-          temp_dst[j] = temp_src[num_dst[num_src[j]]];
+          temp_dst[j] = temp_src[num_src[j]];
+        for (unsigned int j = 0; j < temp_src.size(); j++)
+          temp_src[num_dst[j]] = temp_dst[j];
 
         // scatter values (TODO: any alternatives?)
         cell2->get_mg_dof_indices(dof_indices2);
         for (unsigned int i = 0; i < dof_indices2.size(); i++)
-          dst[dof_indices2[i]] += temp_dst[i];
+          dst[dof_indices2[i]] += temp_src[i];
       }
   } else {
     // get iterator
@@ -92,10 +94,12 @@ void CGToDGTransfer<dim, Number>::transfer(VNumber &dst, const VNumber &src,
         // bring dof_values into the right order
         // (needed: numbering of shape functions of fe_q and fe_dgq different)
         for (unsigned int j = 0; j < temp_src.size(); j++)
-          temp_dst[j] = temp_src[num_dst[num_src[j]]];
-
+          temp_dst[j] = temp_src[num_src[j]];
+        for (unsigned int j = 0; j < temp_src.size(); j++)
+          temp_src[num_dst[j]] = temp_dst[j];
+        
         // scatter values
-        cell2->distribute_local_to_global(temp_dst, dst);
+        cell2->distribute_local_to_global(temp_src, dst);
       }
   }
 }
