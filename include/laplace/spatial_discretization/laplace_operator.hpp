0877bce0b62a911c513f8e5d807144134fab4a6d
peterrum <peterrmuench@aol.com>
2018-08-03 10:48:25 +0200

    Use OP_DOUBLE and OP_FLOAT in laplace_operator

--- include/laplace/spatial_discretization/laplace_operator.hpp
+++ include/laplace/spatial_discretization/laplace_operator.hpp
@@ -5,48 +5,66 @@
 namespace Laplace
 {
 
-#if DIM_2 && DEGREE_1
+#if DIM_2 && DEGREE_1 && (OP_FLOAT || MG_FLOAT)
 template class LaplaceOperator<2, 1, float>;
+#endif
+#if DIM_2 && DEGREE_1 && (OP_DOUBLE || MG_DOUBLE)
 template class LaplaceOperator<2, 1, double>;
 #endif
 
-#if DIM_2 && DEGREE_2
+#if DIM_2 && DEGREE_2 && (OP_FLOAT || MG_FLOAT)
 template class LaplaceOperator<2, 2, float>;
+#endif
+#if DIM_2 && DEGREE_2 && (OP_DOUBLE || MG_DOUBLE)
 template class LaplaceOperator<2, 2, double>;
 #endif
 
-#if DIM_2 && DEGREE_3
+#if DIM_2 && DEGREE_3 && (OP_FLOAT || MG_FLOAT)
 template class LaplaceOperator<2, 3, float>;
+#endif
+#if DIM_2 && DEGREE_3 && (OP_DOUBLE || MG_DOUBLE)
 template class LaplaceOperator<2, 3, double>;
 #endif
 
-#if DIM_2 && DEGREE_4
+#if DIM_2 && DEGREE_4 && (OP_FLOAT || MG_FLOAT)
 template class LaplaceOperator<2, 4, float>;
+#endif
+#if DIM_2 && DEGREE_4 && (OP_DOUBLE || MG_DOUBLE)
 template class LaplaceOperator<2, 4, double>;
 #endif
 
-#if DIM_2 && DEGREE_5
+#if DIM_2 && DEGREE_5 && (OP_FLOAT || MG_FLOAT)
 template class LaplaceOperator<2, 5, float>;
+#endif
+#if DIM_2 && DEGREE_5 && (OP_DOUBLE || MG_DOUBLE)
 template class LaplaceOperator<2, 5, double>;
 #endif
 
-#if DIM_2 && DEGREE_6
+#if DIM_2 && DEGREE_6 && (OP_FLOAT || MG_FLOAT)
 template class LaplaceOperator<2, 6, float>;
+#endif
+#if DIM_2 && DEGREE_6 && (OP_DOUBLE || MG_DOUBLE)
 template class LaplaceOperator<2, 6, double>;
 #endif
 
-#if DIM_2 && DEGREE_7
+#if DIM_2 && DEGREE_7 && (OP_FLOAT || MG_FLOAT)
 template class LaplaceOperator<2, 7, float>;
+#endif
+#if DIM_2 && DEGREE_7 && (OP_DOUBLE || MG_DOUBLE)
 template class LaplaceOperator<2, 7, double>;
 #endif
 
-#if DIM_2 && DEGREE_8
+#if DIM_2 && DEGREE_8 && (OP_FLOAT || MG_FLOAT)
 template class LaplaceOperator<2, 8, float>;
+#endif
+#if DIM_2 && DEGREE_8 && (OP_DOUBLE || MG_DOUBLE)
 template class LaplaceOperator<2, 8, double>;
 #endif
 
-#if DIM_2 && DEGREE_9
+#if DIM_2 && DEGREE_9 && (OP_FLOAT || MG_FLOAT)
 template class LaplaceOperator<2, 9, float>;
+#endif
+#if DIM_2 && DEGREE_9 && (OP_DOUBLE || MG_DOUBLE)
 template class LaplaceOperator<2, 9, double>;
 #endif
 
