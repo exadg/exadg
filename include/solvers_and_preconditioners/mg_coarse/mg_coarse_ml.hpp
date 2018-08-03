6d9356df9b77f2b0b5e44ac3a770f7b3574fb9b7
peterrum <peterrmuench@aol.com>
2018-08-03 11:40:47 +0200

    Use OP_FLOAT and OP_DOUBLE in mg_coarse_ml and multigrid_preconditioner_adapter_base

--- include/solvers_and_preconditioners/mg_coarse/mg_coarse_ml.hpp
+++ include/solvers_and_preconditioners/mg_coarse/mg_coarse_ml.hpp
@@ -4,12 +4,22 @@
 
 #include "../../operators/matrix_operator_base_new.h"
 
-#if DIM_2
+#if DIM_2 && OP_FLOAT && MG_FLOAT
 template class MGCoarseML<MatrixOperatorBaseNew<2, float>, float>;
+#endif
+#if DIM_2 && OP_DOUBLE && MG_FLOAT
 template class MGCoarseML<MatrixOperatorBaseNew<2, float>, double>;
 #endif
+#if DIM_2 && OP_DOUBLE && MG_DOUBLE
+template class MGCoarseML<MatrixOperatorBaseNew<2, double>, double>;
+#endif
 
-#if DIM_3
+#if DIM_3 && OP_FLOAT && MG_FLOAT
 template class MGCoarseML<MatrixOperatorBaseNew<3, float>, float>;
+#endif
+#if DIM_3 && OP_DOUBLE && MG_FLOAT
 template class MGCoarseML<MatrixOperatorBaseNew<3, float>, double>;
+#endif
+#if DIM_3 && OP_DOUBLE && MG_DOUBLE
+template class MGCoarseML<MatrixOperatorBaseNew<3, double>, double>;
 #endif
--- include/solvers_and_preconditioners/multigrid/multigrid_preconditioner_adapter_base.hpp
+++ include/solvers_and_preconditioners/multigrid/multigrid_preconditioner_adapter_base.hpp
@@ -2,20 +2,22 @@
 
 #include <navier-constants.h>
 
-#if DIM_2 && MG_FLOAT
+#if DIM_2 && OP_FLOAT && MG_FLOAT
 template class MyMultigridPreconditionerBase<2,float, MatrixOperatorBaseNew<2,float>>;
+#endif
+#if DIM_2 && OP_DOUBLE && MG_FLOAT
 template class MyMultigridPreconditionerBase<2,double, MatrixOperatorBaseNew<2,float>>;
 #endif
-
-//#if DIM_2 && MG_DOUBLE
-//template class MyMultigridPreconditionerBase<2,double, MatrixOperatorBaseNew<2,double>>;
-//#endif
-
-#if DIM_3 && MG_FLOAT
-template class MyMultigridPreconditionerBase<3,float, MatrixOperatorBaseNew<3,float>>;
-template class MyMultigridPreconditionerBase<3,double, MatrixOperatorBaseNew<3,float>>;
+#if DIM_2 && OP_DOUBLE && MG_DOUBLE
+template class MyMultigridPreconditionerBase<2,double, MatrixOperatorBaseNew<2,double>>;
 #endif
 
-//#if DIM_3 && MG_DOUBLE
-//template class MyMultigridPreconditionerBase<3,double, MatrixOperatorBaseNew<3,double>>;
-//#endif
+#if DIM_3 && OP_FLOAT && MG_FLOAT
+template class MyMultigridPreconditionerBase<3,float, MatrixOperatorBaseNew<3,float>>;
+#endif
+#if DIM_3 && OP_DOUBLE && MG_FLOAT
+template class MyMultigridPreconditionerBase<3,double, MatrixOperatorBaseNew<3,float>>;
+#endif
+#if DIM_3 && OP_DOUBLE && MG_DOUBLE
+template class MyMultigridPreconditionerBase<3,double, MatrixOperatorBaseNew<3,double>>;
+#endif
