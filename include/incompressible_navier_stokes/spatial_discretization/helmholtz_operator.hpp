9b5adb5d436fa76f0453564ff306d1c96ce59585
peterrum <peterrmuench@aol.com>
2018-08-04 09:32:53 +0200

    Instantiate more operators

--- include/incompressible_navier_stokes/spatial_discretization/helmholtz_operator.hpp
+++ include/incompressible_navier_stokes/spatial_discretization/helmholtz_operator.hpp
@@ -7,47 +7,128 @@ namespace IncNS
 
 #if DIM_2 && DEGREE_1
 template class HelmholtzOperator<2, 1, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_1
 template class HelmholtzOperator<2, 1, 1, 1, double>;
 #endif
 
 #if DIM_2 && DEGREE_2
 template class HelmholtzOperator<2, 2, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_2
 template class HelmholtzOperator<2, 2, 1, 1, double>;
 #endif
 
 #if DIM_2 && DEGREE_3
 template class HelmholtzOperator<2, 3, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_3
 template class HelmholtzOperator<2, 3, 1, 1, double>;
 #endif
 
 #if DIM_2 && DEGREE_4
 template class HelmholtzOperator<2, 4, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_4
 template class HelmholtzOperator<2, 4, 1, 1, double>;
 #endif
 
 #if DIM_2 && DEGREE_5
 template class HelmholtzOperator<2, 5, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_5
 template class HelmholtzOperator<2, 5, 1, 1, double>;
 #endif
 
 #if DIM_2 && DEGREE_6
 template class HelmholtzOperator<2, 6, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_6
 template class HelmholtzOperator<2, 6, 1, 1, double>;
 #endif
 
 #if DIM_2 && DEGREE_7
 template class HelmholtzOperator<2, 7, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_7
 template class HelmholtzOperator<2, 7, 1, 1, double>;
 #endif
 
 #if DIM_2 && DEGREE_8
 template class HelmholtzOperator<2, 8, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_8
 template class HelmholtzOperator<2, 8, 1, 1, double>;
 #endif
 
 #if DIM_2 && DEGREE_9
 template class HelmholtzOperator<2, 9, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_9
 template class HelmholtzOperator<2, 9, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_1
+template class HelmholtzOperator<3, 1, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_1
+template class HelmholtzOperator<3, 1, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_2
+template class HelmholtzOperator<3, 2, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_2
+template class HelmholtzOperator<3, 2, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_3
+template class HelmholtzOperator<3, 3, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_3
+template class HelmholtzOperator<3, 3, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_4
+template class HelmholtzOperator<3, 4, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_4
+template class HelmholtzOperator<3, 4, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_5
+template class HelmholtzOperator<3, 5, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_5
+template class HelmholtzOperator<3, 5, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_6
+template class HelmholtzOperator<3, 6, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_6
+template class HelmholtzOperator<3, 6, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_7
+template class HelmholtzOperator<3, 7, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_7
+template class HelmholtzOperator<3, 7, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_8
+template class HelmholtzOperator<3, 8, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_8
+template class HelmholtzOperator<3, 8, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_9
+template class HelmholtzOperator<3, 9, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_9
+template class HelmholtzOperator<3, 9, 1, 1, double>;
 #endif
 
 }
--- include/incompressible_navier_stokes/spatial_discretization/velocity_convection_diffusion_operator.hpp
+++ include/incompressible_navier_stokes/spatial_discretization/velocity_convection_diffusion_operator.hpp
@@ -5,49 +5,130 @@
 namespace IncNS
 {
 
-#if DIM_2 && DEGREE_1
+#if DIM_2 && DEGREE_1 && (OP_FLOAT || MG_FLOAT)
 template class VelocityConvDiffOperator<2, 1, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_1 && (OP_DOUBLE || MG_DOUBLE)
 template class VelocityConvDiffOperator<2, 1, 1, 1, double>;
 #endif
 
-#if DIM_2 && DEGREE_2
+#if DIM_2 && DEGREE_2 && (OP_FLOAT || MG_FLOAT)
 template class VelocityConvDiffOperator<2, 2, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_2 && (OP_DOUBLE || MG_DOUBLE)
 template class VelocityConvDiffOperator<2, 2, 1, 1, double>;
 #endif
 
-#if DIM_2 && DEGREE_3
+#if DIM_2 && DEGREE_3 && (OP_FLOAT || MG_FLOAT)
 template class VelocityConvDiffOperator<2, 3, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_3 && (OP_DOUBLE || MG_DOUBLE)
 template class VelocityConvDiffOperator<2, 3, 1, 1, double>;
 #endif
 
-#if DIM_2 && DEGREE_4
+#if DIM_2 && DEGREE_4 && (OP_FLOAT || MG_FLOAT)
 template class VelocityConvDiffOperator<2, 4, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_4 && (OP_DOUBLE || MG_DOUBLE)
 template class VelocityConvDiffOperator<2, 4, 1, 1, double>;
 #endif
 
-#if DIM_2 && DEGREE_5
+#if DIM_2 && DEGREE_5 && (OP_FLOAT || MG_FLOAT)
 template class VelocityConvDiffOperator<2, 5, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_5 && (OP_DOUBLE || MG_DOUBLE)
 template class VelocityConvDiffOperator<2, 5, 1, 1, double>;
 #endif
 
-#if DIM_2 && DEGREE_6
+#if DIM_2 && DEGREE_6 && (OP_FLOAT || MG_FLOAT)
 template class VelocityConvDiffOperator<2, 6, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_6 && (OP_DOUBLE || MG_DOUBLE)
 template class VelocityConvDiffOperator<2, 6, 1, 1, double>;
 #endif
 
-#if DIM_2 && DEGREE_7
+#if DIM_2 && DEGREE_7 && (OP_FLOAT || MG_FLOAT)
 template class VelocityConvDiffOperator<2, 7, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_7 && (OP_DOUBLE || MG_DOUBLE)
 template class VelocityConvDiffOperator<2, 7, 1, 1, double>;
 #endif
 
-#if DIM_2 && DEGREE_8
+#if DIM_2 && DEGREE_8 && (OP_FLOAT || MG_FLOAT)
 template class VelocityConvDiffOperator<2, 8, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_8 && (OP_DOUBLE || MG_DOUBLE)
 template class VelocityConvDiffOperator<2, 8, 1, 1, double>;
 #endif
 
-#if DIM_2 && DEGREE_9
+#if DIM_2 && DEGREE_9 && (OP_FLOAT || MG_FLOAT)
 template class VelocityConvDiffOperator<2, 9, 1, 1, float>;
+#endif
+#if DIM_2 && DEGREE_9 && (OP_DOUBLE || MG_DOUBLE)
 template class VelocityConvDiffOperator<2, 9, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_1 && (OP_FLOAT || MG_FLOAT)
+template class VelocityConvDiffOperator<3, 1, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_1 && (OP_DOUBLE || MG_DOUBLE)
+template class VelocityConvDiffOperator<3, 1, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_2 && (OP_FLOAT || MG_FLOAT)
+template class VelocityConvDiffOperator<3, 2, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_2 && (OP_DOUBLE || MG_DOUBLE)
+template class VelocityConvDiffOperator<3, 2, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_3 && (OP_FLOAT || MG_FLOAT)
+template class VelocityConvDiffOperator<3, 3, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_3 && (OP_DOUBLE || MG_DOUBLE)
+template class VelocityConvDiffOperator<3, 3, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_4 && (OP_FLOAT || MG_FLOAT)
+template class VelocityConvDiffOperator<3, 4, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_4 && (OP_DOUBLE || MG_DOUBLE)
+template class VelocityConvDiffOperator<3, 4, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_5 && (OP_FLOAT || MG_FLOAT)
+template class VelocityConvDiffOperator<3, 5, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_5 && (OP_DOUBLE || MG_DOUBLE)
+template class VelocityConvDiffOperator<3, 5, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_6 && (OP_FLOAT || MG_FLOAT)
+template class VelocityConvDiffOperator<3, 6, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_6 && (OP_DOUBLE || MG_DOUBLE)
+template class VelocityConvDiffOperator<3, 6, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_7 && (OP_FLOAT || MG_FLOAT)
+template class VelocityConvDiffOperator<3, 7, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_7 && (OP_DOUBLE || MG_DOUBLE)
+template class VelocityConvDiffOperator<3, 7, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_8 && (OP_FLOAT || MG_FLOAT)
+template class VelocityConvDiffOperator<3, 8, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_8 && (OP_DOUBLE || MG_DOUBLE)
+template class VelocityConvDiffOperator<3, 8, 1, 1, double>;
+#endif
+
+#if DIM_3 && DEGREE_9 && (OP_FLOAT || MG_FLOAT)
+template class VelocityConvDiffOperator<3, 9, 1, 1, float>;
+#endif
+#if DIM_3 && DEGREE_9 && (OP_DOUBLE || MG_DOUBLE)
+template class VelocityConvDiffOperator<3, 9, 1, 1, double>;
 #endif
 
 }
