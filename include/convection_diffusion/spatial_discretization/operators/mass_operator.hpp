8880d91b0ecea18113942e9c88c2f94e68f9f275
Niklas Fehn <fehn@lnm.mw.tum.de>
2018-11-05 15:06:23 +0100

    also build operators for degree=0 to avoid build errors

--- include/convection_diffusion/spatial_discretization/operators/mass_operator.hpp
+++ include/convection_diffusion/spatial_discretization/operators/mass_operator.hpp
@@ -116,6 +116,13 @@ template class MassMatrixOperator<2, 15, float>;
 template class MassMatrixOperator<2, 15, double>;
 #endif
 
+#if DIM_3 && DEGREE_0 && (OP_FLOAT || MG_FLOAT)
+template class MassMatrixOperator<3, 0, float>;
+#endif
+#if DIM_3 && DEGREE_0 && (OP_DOUBLE || MG_DOUBLE)
+template class MassMatrixOperator<3, 0, double>;
+#endif
+
 #if DIM_3 && DEGREE_1 && (OP_FLOAT || MG_FLOAT)
 template class MassMatrixOperator<3, 1, float>;
 #endif
