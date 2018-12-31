895f8ae7d4d3ac3ccbff512bfacce6aa202acaec
peterrum <peterrmuench@aol.com>
2018-12-31 10:16:59 +0100

    Test helmholtz

--- tests/operators/operation-base-vector/src/main.cc
+++ tests/operators/operation-base-vector/src/main.cc
@@ -18,6 +18,7 @@ using namespace dealii;
 
 #include "../../../../include/incompressible_navier_stokes/spatial_discretization/operators/mass_matrix_operator.h"
 #include "operators/mass_operator.h"
+#include "operators/helmholtz_operator.h"
 #include "../../operation-base-util/sparse_matrix_util.h"
 
 const int      best_of = 10;
@@ -38,6 +39,7 @@ public:
 
     GridGenerator::hyper_cube(triangulation, left, right);
 
+#if false
     for(auto & cell : triangulation)
       for(unsigned int face_number = 0; face_number < GeometryInfo<dim>::faces_per_cell;
           ++face_number)
@@ -61,11 +63,12 @@ public:
 
     std::vector<GridTools::PeriodicFacePair<typename Triangulation<dim>::cell_iterator>>
       periodic_faces;
-    GridTools::collect_periodic_faces(triangulation, 0, 1, 0 /*x-direction*/, periodic_faces);
-    GridTools::collect_periodic_faces(triangulation, 2, 3, 1 /*y-direction*/, periodic_faces);
+    GridTools::collect_periodic_faces(triangulation, 0, 1, 0, periodic_faces);
+    GridTools::collect_periodic_faces(triangulation, 2, 3, 1, periodic_faces);
     if (dim==3)
-        GridTools::collect_periodic_faces(triangulation, 4, 5, 2 /*z-direction*/, periodic_faces);
+        GridTools::collect_periodic_faces(triangulation, 4, 5, 2, periodic_faces);
     triangulation.add_periodicity(periodic_faces);
+#endif
     
     
     const double deformation = +0.1;
@@ -85,7 +88,17 @@ public:
     dof_handler_u.distribute_dofs(*fe_u);
     
     typename MatrixFree<dim, Number>::AdditionalData additional_data;
-    additional_data.mapping_update_flags = (update_JxW_values | update_values);
+    additional_data.mapping_update_flags =
+    (update_gradients | update_JxW_values | update_quadrature_points | update_normal_vectors |
+     update_values);
+
+  additional_data.mapping_update_flags_inner_faces =
+    (update_gradients | update_JxW_values | update_quadrature_points | update_normal_vectors |
+     update_values);
+
+  additional_data.mapping_update_flags_boundary_faces =
+    (update_gradients | update_JxW_values | update_quadrature_points | update_normal_vectors |
+     update_values);
 
     MatrixFree<dim, Number> data;
     
@@ -101,6 +114,9 @@ public:
     IncNS::MassMatrixOperatorDataNew<dim> mmo2;
     mm2.reinit(data, constraint_u, mmo2);
     
+    std::cout << "EXPERIMENTS " <<
+            (use_dg ? "DG " : "CG ") <<
+            dim << "D " << fe_degree << " degree" << std::endl;
 
     {
         // test apply/vmult
@@ -116,6 +132,33 @@ public:
         mm1.apply(v3,v1);
         mm2.apply_add(v4,v2);
         std::cout << v3.l2_norm() << " " << v4.l2_norm() << std::endl;
+    }
+    
+    {
+        // create Helmholtz-operator
+        IncNS::HelmholtzOperatorNew<dim,fe_degree,Number> ho2;
+        IncNS::HelmholtzOperatorDataNew<dim> hoo2;
+#if true
+      std::shared_ptr<Function<dim> > zero_function_vectorial;
+      zero_function_vectorial.reset(new Functions::ZeroFunction<dim>(dim));
+      IncNS::BoundaryDescriptorU<dim>* bd = new IncNS::BoundaryDescriptorU<dim>();
+      bd->dirichlet_bc.insert(
+          std::pair<types::boundary_id,std::shared_ptr<Function<dim> > >(0,zero_function_vectorial));
+      hoo2.bc.reset(bd);
+#endif
+        
+        ho2.reinit(mapping, data, constraint_u, hoo2);
+        
+        // test apply/vmult
+        LinearAlgebra::distributed::Vector<Number> v2,v4;
+
+        ho2.do_initialize_dof_vector(v2);
+        ho2.do_initialize_dof_vector(v4);
+        v2 = 0.0;
+        v2 = 1.0;
+
+        ho2.apply_add(v4,v2);
+        std::cout << v4.l2_norm() << std::endl;
     }
 
     {
