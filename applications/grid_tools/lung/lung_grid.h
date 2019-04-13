58bbd60b9208e17c6545ef8c6a3e699efffe5206
Peter Munch <munch@lnm.mw.tum.de>
2019-04-13 10:53:47 +0200

    Extend fully distributed triangulation for material ids and mapping

--- applications/grid_tools/lung/lung_grid.h
+++ applications/grid_tools/lung/lung_grid.h
@@ -249,10 +249,6 @@ void lung_unrefined(dealii::Triangulation<3> &                                  
 
   timings["create_triangulation_1_load_data"] = timer.wall_time();
 
-  parallel::fullydistributed::AdditionalData ad;
-  ad.partition_group_size = 1;
-  ad.partition_group      = parallel::fullydistributed::single;
-
   timer.restart();
   // ... by processing each tree
   std::vector<CellData<3>> cell_data_3d;
@@ -650,13 +646,18 @@ void lung(dealii::parallel::fullydistributed::Triangulation<3> &         tria,
   Timer timer;
   timer.restart();
 
+  parallel::fullydistributed::AdditionalData ad;
+  ad.partition_group_size = 1;
+  ad.partition_group      = parallel::fullydistributed::single;
+
   std::map<types::material_id, DeformTransfinitelyViaSplines<3>> deform;
   // create partitioned triangulation ...
   tria.reinit(refinements2, [&](auto & tria) mutable {
     // ... by creating a refined sequential triangulation and partition it
     lung_unrefined(tria, create_tree, timings, outlet_id_first, outlet_id_last, bspline_file, deform, branch_filter);
     tria.refine_global(refinements1);
-  });
+    //update_mapping(tria, deform);
+  }, ad);
   update_mapping(tria, deform);
 
   outlet_id_last = Utilities::MPI::max(outlet_id_last, MPI_COMM_WORLD);
