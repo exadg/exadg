21b42fb8c6f8d9777d840d07d1af8f6100fb03bc
Niklas Fehn <fehn@lnm.mw.tum.de>
2019-05-13 13:44:39 +0200

    add matrix_free folder to fix compilation errors

---
+++ include/matrix_free/resolve_templates_double_3d.cpp
@@ -0,0 +1,21 @@
+/*
+ * resolve_templates_double_3d.cpp
+ *
+ *  Created on: May 2, 2019
+ *      Author: fehn
+ */
+
+#include "which_degrees.h"
+
+#include <deal.II/matrix_free/evaluation_template_factory.templates.h>
+
+template struct dealii::internal::FEEvaluationFactory<3,1,double>;
+template struct dealii::internal::FEEvaluationFactory<3,3,double>;
+
+template struct dealii::internal::FEFaceEvaluationFactory<3,1,double>;
+template struct dealii::internal::FEFaceEvaluationFactory<3,3,double>;
+
+// inverse mass
+template struct dealii::internal::CellwiseInverseMassFactory<3,1,double>;
+template struct dealii::internal::CellwiseInverseMassFactory<3,3,double>;
+template struct dealii::internal::CellwiseInverseMassFactory<3,3+2,double>;
---
+++ include/matrix_free/resolve_templates_float_3d.cpp
@@ -0,0 +1,21 @@
+/*
+ * resolve_templates_float_3d.cpp
+ *
+ *  Created on: May 2, 2019
+ *      Author: fehn
+ */
+
+#include "which_degrees.h"
+
+#include <deal.II/matrix_free/evaluation_template_factory.templates.h>
+
+template struct dealii::internal::FEEvaluationFactory<3,1,float>;
+template struct dealii::internal::FEEvaluationFactory<3,3,float>;
+
+template struct dealii::internal::FEFaceEvaluationFactory<3,1,float>;
+template struct dealii::internal::FEFaceEvaluationFactory<3,3,float>;
+
+// inverse mass
+template struct dealii::internal::CellwiseInverseMassFactory<3,1,float>;
+template struct dealii::internal::CellwiseInverseMassFactory<3,3,float>;
+template struct dealii::internal::CellwiseInverseMassFactory<3,3+2,float>;
