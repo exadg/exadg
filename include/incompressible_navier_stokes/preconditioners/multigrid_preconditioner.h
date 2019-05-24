exadg/exadg

commit 2b1b392fab3bae3840de34db63b91f10b0fac5b4

use consistently coarse_level and fine_level when looping over (all) levels

include/incompressible_navier_stokes/preconditioners/multigrid_preconditioner.h
@@ -248,7 +248,7 @@ private:
     this->get_operator(this->fine_level)->set_solution_linearization(vector_linearization);
 
     // interpolate velocity from fine to coarse level
-    for(auto level = this->fine_level; level > this->coarse_level; --level)
+    for(unsigned int level = this->fine_level; level > this->coarse_level; --level)
     {
       auto & vector_fine_level   = this->get_operator(level - 0)->get_solution_linearization();
       auto   vector_coarse_level = this->get_operator(level - 1)->get_solution_linearization();
@@ -266,7 +266,7 @@ private:
   void
   set_evaluation_time(double const & evaluation_time)
   {
-    for(int level = this->n_levels - 1; level >= 0; --level)
+    for(unsigned int level = this->fine_level; level >= this->coarse_level; --level)
     {
       get_operator(level)->set_evaluation_time(evaluation_time);
     }
@@ -280,7 +280,7 @@ private:
   void
   set_scaling_factor_time_derivative_term(double const & scaling_factor_time_derivative_term)
   {
-    for(int level = this->n_levels - 1; level >= 0; --level)
+    for(unsigned int level = this->fine_level; level >= this->coarse_level; --level)
     {
       get_operator(level)->set_scaling_factor_time_derivative_term(
         scaling_factor_time_derivative_term);
@@ -294,8 +294,8 @@ private:
   void
   update_smoothers()
   {
-    // Start with level = 1!
-    for(unsigned int level = 1; level < this->n_levels; ++level)
+    // Skip coarsest level
+    for(unsigned int level = this->coarse_level + 1; level <= this->fine_level; ++level)
     {
       this->update_smoother(level);
     }

include/convection_diffusion/preconditioners/multigrid_preconditioner.h
@@ -297,7 +297,7 @@ private:
     this->get_operator(this->fine_level)->set_velocity(velocity);
 
     // interpolate velocity from fine to coarse level
-    for(auto level = this->fine_level; level > this->coarse_level; --level)
+    for(unsigned int level = this->fine_level; level > this->coarse_level; --level)
     {
       auto & vector_fine_level   = this->get_operator(level - 0)->get_velocity();
       auto   vector_coarse_level = this->get_operator(level - 1)->get_velocity();
@@ -315,7 +315,7 @@ private:
   void
   set_evaluation_time(double const & evaluation_time)
   {
-    for(auto level = this->coarse_level; level <= this->fine_level; ++level)
+    for(unsigned int level = this->coarse_level; level <= this->fine_level; ++level)
       this->get_operator(level)->set_evaluation_time(evaluation_time);
   }
 
@@ -328,7 +328,7 @@ private:
   void
   set_scaling_factor_time_derivative_term(double const & scaling_factor)
   {
-    for(auto level = this->coarse_level; level <= this->fine_level; ++level)
+    for(unsigned int level = this->coarse_level; level <= this->fine_level; ++level)
       this->get_operator(level)->set_scaling_factor_time_derivative_term(scaling_factor);
   }
 
@@ -341,8 +341,8 @@ private:
   void
   update_smoothers()
   {
-    // Skip coarsest level!
-    for(auto level = this->coarse_level + 1; level <= this->fine_level; ++level)
+    // Skip coarsest level
+    for(unsigned int level = this->coarse_level + 1; level <= this->fine_level; ++level)
       this->update_smoother(level);
   }
 

include/incompressible_navier_stokes/preconditioners/compatible_laplace_multigrid_preconditioner.h
@@ -166,9 +166,9 @@ public:
     std::vector<MGDoFHandlerIdentifier> p_levels_velocity;
 
     // setup global velocity levels
-    for(auto & i : this->level_info)
+    for(auto & level : this->level_info)
       level_info_velocity.push_back(
-        {i.level, i.degree + operator_data.degree_u - operator_data.degree_p, i.is_dg});
+        {level.level, level.degree + operator_data.degree_u - operator_data.degree_p, level.is_dg});
 
     // setup p velocity levels
     for(auto level : level_info_velocity)

include/solvers_and_preconditioners/multigrid/multigrid_preconditioner_base.cpp
@@ -287,7 +287,7 @@ MultigridPreconditionerBase<dim, Number, MultigridNumber>::
     constrained_dofs[level] = map_constraints[p_level];
   }
 
-  for(unsigned int level = 0; level < level_info.size(); level++)
+  for(unsigned int level = coarse_level; level <= fine_level; level++)
   {
     auto constraint_own = new AffineConstraints<double>;
 
@@ -310,7 +310,7 @@ MultigridPreconditionerBase<dim, Number, MultigridNumber>::initialize_matrix_fre
 {
   this->matrix_free_objects.resize(0, this->n_levels - 1);
 
-  for(unsigned int level = 0; level < this->n_levels; level++)
+  for(unsigned int level = coarse_level; level <= fine_level; level++)
     this->matrix_free_objects[level] = this->initialize_matrix_free(level, mapping);
 }
 
@@ -338,7 +338,7 @@ MultigridPreconditionerBase<dim, Number, MultigridNumber>::initialize_operators(
   this->operators.resize(0, this->n_levels - 1);
 
   // create and setup operator on each level
-  for(unsigned int level = 0; level < this->n_levels; level++)
+  for(unsigned int level = coarse_level; level <= fine_level; level++)
     operators[level] = this->initialize_operator(level);
 }
 
@@ -362,7 +362,8 @@ MultigridPreconditionerBase<dim, Number, MultigridNumber>::initialize_smoothers(
 {
   this->smoothers.resize(0, this->n_levels - 1);
 
-  for(unsigned int level = 1; level < this->n_levels; level++)
+  // skip the coarsest level
+  for(unsigned int level = coarse_level + 1; level <= fine_level; level++)
     this->initialize_smoother(*this->operators[level], level);
 }
 

commit 957fb58a91065c0192435fee49a0ff4d9e2968c1

fixed a bug introduced in 2b1b392f

include/incompressible_navier_stokes/preconditioners/multigrid_preconditioner.h
@@ -266,7 +266,7 @@ private:
   void
   set_evaluation_time(double const & evaluation_time)
   {
-    for(unsigned int level = this->fine_level; level >= this->coarse_level; --level)
+    for(unsigned int level = this->coarse_level; level <= this->fine_level; ++level)
     {
       get_operator(level)->set_evaluation_time(evaluation_time);
     }
@@ -280,7 +280,7 @@ private:
   void
   set_scaling_factor_time_derivative_term(double const & scaling_factor_time_derivative_term)
   {
-    for(unsigned int level = this->fine_level; level >= this->coarse_level; --level)
+    for(unsigned int level = this->coarse_level; level <= this->fine_level; ++level)
     {
       get_operator(level)->set_scaling_factor_time_derivative_term(
         scaling_factor_time_derivative_term);