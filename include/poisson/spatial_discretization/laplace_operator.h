758a3c514eb08ab4fdfc4481621190ea1c9de356
Niklas Fehn <fehn@lnm.mw.tum.de>
2019-05-27 17:09:41 +0200

    add comments for LaplaceOperatorData similar to other operators

--- include/poisson/spatial_discretization/laplace_operator.h
+++ include/poisson/spatial_discretization/laplace_operator.h
@@ -15,9 +15,13 @@ struct LaplaceOperatorData : public OperatorBaseData
 public:
   LaplaceOperatorData()
     // clang-format off
-    : OperatorBaseData(0, 0,
-          false, true, false, false, true, false, // cell
-          true,  true,        true,  true         // face
+    : OperatorBaseData(
+          0, // dof_index
+          0, // quad_index
+          false, true, false, // cell evaluate
+          false, true, false, // cell integrate
+          true,  true,        // face evaluate
+          true,  true         // face integrate
       ),
       // clang-format on
       IP_factor(1.0),
