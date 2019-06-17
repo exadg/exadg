4617becf2bd0d1fe7496ac68a7763b8888a4521e
Niklas Fehn <fehn@lnm.mw.tum.de>
2019-06-17 19:43:49 +0200

    a first draft for a merged IncNS::MomentumOperator

--- include/incompressible_navier_stokes/spatial_discretization/momentum_operator.h
+++ include/incompressible_navier_stokes/spatial_discretization/momentum_operator.h
@@ -19,6 +19,7 @@
 
 #include "../../operators/elementwise_operator.h"
 #include "../../operators/linear_operator_base.h"
+#include "../../operators/operator_base.h"
 #include "../../solvers_and_preconditioners/util/invert_diagonal.h"
 #include "../../solvers_and_preconditioners/util/verify_calculation_of_diagonal.h"
 
@@ -397,6 +398,508 @@ private:
   mutable std::shared_ptr<PRECONDITIONER_BASE>  elementwise_preconditioner;
   mutable std::shared_ptr<ELEMENTWISE_SOLVER>   elementwise_solver;
 };
+
+struct MomentumOperatorMergedData : public OperatorBaseData
+{
+  MomentumOperatorMergedData()
+    : OperatorBaseData(0 /* dof_index */, 0 /* quad_index */),
+      unsteady_problem(false),
+      convective_problem(false),
+      viscous_problem(false)
+  {
+  }
+
+  bool unsteady_problem;
+  bool convective_problem;
+  bool viscous_problem;
+
+  Operators::ConvectiveKernelData convective_kernel_data;
+};
+
+template<int dim, typename Number>
+class MomentumOperatorMerged : public OperatorBase<dim, Number, MomentumOperatorMergedData, dim>
+{
+private:
+  typedef VectorizedArray<Number>                 scalar;
+  typedef Tensor<1, dim, VectorizedArray<Number>> vector;
+  typedef Tensor<2, dim, VectorizedArray<Number>> tensor;
+
+  typedef OperatorBase<dim, Number, MomentumOperatorMergedData, dim> Base;
+
+  typedef typename Base::VectorType     VectorType;
+  typedef typename Base::IntegratorCell IntegratorCell;
+  typedef typename Base::IntegratorFace IntegratorFace;
+
+public:
+  void
+  rhs(VectorType & dst) const
+  {
+    (void)dst;
+
+    AssertThrow(false,
+                ExcMessage("The function rhs() does not make sense for the momentum operator."));
+  }
+
+  void
+  rhs_add(VectorType & dst) const
+  {
+    (void)dst;
+
+    AssertThrow(
+      false, ExcMessage("The function rhs_add() does not make sense for the momentum operator."));
+  }
+
+  void
+  evaluate(VectorType & dst, VectorType const & src) const
+  {
+    (void)dst;
+    (void)src;
+
+    AssertThrow(
+      false, ExcMessage("The function evaluate() does not make sense for the momentum operator."));
+  }
+
+  void
+  evaluate_add(VectorType & dst, VectorType const & src) const
+  {
+    (void)dst;
+    (void)src;
+
+    AssertThrow(false,
+                ExcMessage(
+                  "The function evaluate_add() does not make sense for the momentum operator."));
+  }
+
+  void
+  reinit_cell(unsigned int const cell) const
+  {
+    Base::reinit_cell(cell);
+
+    if(this->operator_data.convective_problem)
+      convective_kernel.reinit_cell(cell);
+  }
+
+  void
+  reinit_face(unsigned int const face) const
+  {
+    Base::reinit_face(face);
+
+    if(this->operator_data.convective_problem)
+      convective_kernel.reinit_face(face);
+
+    if(this->operator_data.viscous_problem)
+      viscous_kernel.reinit_face(*this->integrator_m, *this->integrator_p);
+  }
+
+  void
+  reinit_boundary_face(unsigned int const face) const
+  {
+    Base::reinit_boundary_face(face);
+
+    if(this->operator_data.convective_problem)
+      convective_kernel.reinit_boundary_face(face);
+
+    if(this->operator_data.viscous_problem)
+      viscous_kernel.reinit_boundary_face(*this->integrator_m);
+  }
+
+  void
+  reinit_face_cell_based(unsigned int const       cell,
+                         unsigned int const       face,
+                         types::boundary_id const boundary_id) const
+  {
+    Base::reinit_face_cell_based(cell, face, boundary_id);
+
+    if(this->operator_data.convective_problem)
+      convective_kernel.reinit_face_cell_based(cell, face, boundary_id);
+
+    if(this->operator_data.viscous_problem)
+      viscous_kernel.reinit_face_cell_based(boundary_id, *this->integrator_m, *this->integrator_p);
+  }
+
+  // linearized operator
+  void
+  do_cell_integral(IntegratorCell & integrator) const
+  {
+    bool const get_value =
+      this->operator_data.unsteady_problem || this->operator_data.convective_problem;
+
+    bool const get_gradient = this->operator_data.viscous_problem ||
+                              (this->operator_data.convective_problem &&
+                               this->operator_data.convective_kernel_data.formulation ==
+                                 FormulationConvectiveTerm::ConvectiveFormulation);
+
+    bool const submit_value = this->operator_data.unsteady_problem ||
+                              (this->operator_data.convective_problem &&
+                               this->operator_data.convective_kernel_data.formulation ==
+                                 FormulationConvectiveTerm::ConvectiveFormulation);
+
+    bool const submit_gradient = this->operator_data.viscous_problem ||
+                                 (this->operator_data.convective_problem &&
+                                  this->operator_data.convective_kernel_data.formulation ==
+                                    FormulationConvectiveTerm::DivergenceFormulation);
+
+    for(unsigned int q = 0; q < integrator.n_q_points; ++q)
+    {
+      vector value_flux;
+      tensor gradient_flux;
+
+      vector value;
+      if(get_value)
+        value = integrator.get_value(q);
+
+      tensor gradient;
+      if(get_gradient)
+        gradient = integrator.get_gradient(q);
+
+      if(this->operator_data.unsteady_problem)
+        value_flux += mass_kernel.get_volume_flux(value);
+
+      if(this->operator_data.convective_problem)
+      {
+        vector u = convective_kernel.get_velocity_cell(q);
+
+        if(this->operator_data.convective_kernel_data.formulation ==
+           FormulationConvectiveTerm::DivergenceFormulation)
+        {
+          gradient_flux +=
+            convective_kernel.get_volume_flux_linearized_divergence_formulation(u, value);
+        }
+        else if(this->operator_data.convective_kernel_data.formulation ==
+                FormulationConvectiveTerm::ConvectiveFormulation)
+        {
+          tensor grad_u = convective_kernel.get_velocity_gradient_cell(q);
+
+          value_flux += convective_kernel.get_volume_flux_linearized_convective_formulation(
+            u, value, grad_u, gradient);
+        }
+        else
+        {
+          AssertThrow(false, ExcMessage("Not implemented."));
+        }
+      }
+
+      if(this->operator_data.viscous_problem)
+      {
+        scalar viscosity = viscous_kernel.get_viscosity_cell(integrator.get_cell_index(), q);
+        gradient_flux += viscous_kernel.get_volume_flux(gradient, viscosity);
+      }
+
+      if(submit_value)
+        integrator.submit_value(value_flux, q);
+
+      if(submit_gradient)
+        integrator.submit_gradient(gradient_flux, q);
+    }
+  }
+
+  // linearized operator
+  void
+  do_face_integral(IntegratorFace & integrator_m, IntegratorFace & integrator_p) const
+  {
+    for(unsigned int q = 0; q < integrator_m.n_q_points; ++q)
+    {
+      vector value_m  = integrator_m.get_value(q);
+      vector value_p  = integrator_p.get_value(q);
+      vector normal_m = integrator_m.get_normal_vector(q);
+
+      vector value_flux_m, value_flux_p;
+      tensor gradient_flux;
+
+      if(this->operator_data.convective_problem)
+      {
+        vector u_m = convective_kernel.get_velocity_m(q);
+        vector u_p = convective_kernel.get_velocity_p(q);
+
+        std::tuple<vector, vector> flux =
+          convective_kernel.calculate_flux_linearized_interior_and_neighbor(
+            u_m, u_p, value_m, value_p, normal_m);
+
+        value_flux_m += std::get<0>(flux);
+        value_flux_p += std::get<1>(flux);
+      }
+
+      if(this->operator_data.viscous_problem)
+      {
+        scalar average_viscosity =
+          viscous_kernel.get_viscosity_interior_face(integrator_m.get_face_index(), q);
+
+        gradient_flux =
+          viscous_kernel.calculate_gradient_flux(value_m, value_p, normal_m, average_viscosity);
+
+        vector normal_gradient_m = viscous_kernel.calculate_normal_gradient(q, integrator_m);
+        vector normal_gradient_p = viscous_kernel.calculate_normal_gradient(q, integrator_p);
+
+        vector value_flux = viscous_kernel.calculate_value_flux(
+          normal_gradient_m, normal_gradient_p, value_m, value_p, normal_m, average_viscosity);
+
+        value_flux_m += -value_flux;
+        value_flux_p += value_flux; // + sign since n⁺ = -n⁻
+      }
+
+      integrator_m.submit_value(value_flux_m, q);
+      integrator_p.submit_value(value_flux_p, q);
+
+      if(this->operator_data.viscous_problem)
+      {
+        integrator_m.submit_gradient(gradient_flux, q);
+        integrator_p.submit_gradient(gradient_flux, q);
+      }
+    }
+  }
+
+  // linearized operator
+  void
+  do_face_int_integral(IntegratorFace & integrator_m, IntegratorFace & integrator_p) const
+  {
+    (void)integrator_p;
+
+    for(unsigned int q = 0; q < integrator_m.n_q_points; ++q)
+    {
+      vector value_m = integrator_m.get_value(q);
+      vector value_p; // set exterior value to zero
+      vector normal_m = integrator_m.get_normal_vector(q);
+
+      vector value_flux_m;
+      tensor gradient_flux;
+
+      if(this->operator_data.convective_problem)
+      {
+        vector u_m = convective_kernel.get_velocity_m(q);
+        vector u_p = convective_kernel.get_velocity_p(q);
+
+        value_flux_m += convective_kernel.calculate_flux_linearized_interior(
+          u_m, u_p, value_m, value_p, normal_m);
+      }
+
+      if(this->operator_data.viscous_problem)
+      {
+        scalar average_viscosity =
+          viscous_kernel.get_viscosity_interior_face(integrator_m.get_face_index(), q);
+
+        gradient_flux +=
+          viscous_kernel.calculate_gradient_flux(value_m, value_p, normal_m, average_viscosity);
+
+        vector normal_gradient_m = viscous_kernel.calculate_normal_gradient(q, integrator_m);
+        vector normal_gradient_p; // set exterior gradient to zero
+
+        vector value_flux = viscous_kernel.calculate_value_flux(
+          normal_gradient_m, normal_gradient_p, value_m, value_p, normal_m, average_viscosity);
+
+        value_flux_m += -value_flux;
+      }
+
+      integrator_m.submit_value(value_flux_m, q);
+
+      if(this->operator_data.viscous_problem)
+      {
+        integrator_m.submit_gradient(gradient_flux, q);
+      }
+    }
+  }
+
+  // linearized operator
+
+  // TODO
+  // This function is currently only needed due to limitations of deal.II which do
+  // currently not allow to access neighboring data in case of cell-based face loops.
+  // Once this functionality is available, this function should be removed again.
+  void
+  do_face_int_integral_cell_based(IntegratorFace & integrator_m,
+                                  IntegratorFace & integrator_p) const
+  {
+    (void)integrator_p;
+
+    for(unsigned int q = 0; q < integrator_m.n_q_points; ++q)
+    {
+      vector value_m = integrator_m.get_value(q);
+      vector value_p; // set exterior value to zero
+      vector normal_m = integrator_m.get_normal_vector(q);
+
+      vector value_flux_m;
+      tensor gradient_flux;
+
+      if(this->operator_data.convective_problem)
+      {
+        vector u_m = convective_kernel.get_velocity_m(q);
+        // TODO
+        // Accessing exterior data is currently not available in deal.II/matrixfree.
+        // Hence, we simply use the interior value, but note that the diagonal and block-diagonal
+        // are not calculated exactly.
+        vector u_p = u_m;
+
+        value_flux_m += convective_kernel.calculate_flux_linearized_interior(
+          u_m, u_p, value_m, value_p, normal_m);
+      }
+
+      if(this->operator_data.viscous_problem)
+      {
+        scalar average_viscosity =
+          viscous_kernel.get_viscosity_interior_face(integrator_m.get_face_index(), q);
+
+        gradient_flux +=
+          viscous_kernel.calculate_gradient_flux(value_m, value_p, normal_m, average_viscosity);
+
+        vector normal_gradient_m = viscous_kernel.calculate_normal_gradient(q, integrator_m);
+        vector normal_gradient_p; // set exterior gradient to zero
+
+        vector value_flux = viscous_kernel.calculate_value_flux(
+          normal_gradient_m, normal_gradient_p, value_m, value_p, normal_m, average_viscosity);
+
+        value_flux_m += -value_flux;
+      }
+
+      integrator_m.submit_value(value_flux_m, q);
+
+      if(this->operator_data.viscous_problem)
+      {
+        integrator_m.submit_gradient(gradient_flux, q);
+      }
+    }
+  }
+
+  // linearized operator
+  void
+  do_face_ext_integral(IntegratorFace & integrator_m, IntegratorFace & integrator_p) const
+  {
+    (void)integrator_m;
+
+    for(unsigned int q = 0; q < integrator_p.n_q_points; ++q)
+    {
+      vector value_m; // set exterior values to zero
+      vector value_p = integrator_p.get_value(q);
+      // multiply by -1.0 to get the correct normal vector !!!
+      vector normal_p = -integrator_p.get_normal_vector(q);
+
+      vector value_flux_p;
+      tensor gradient_flux;
+
+      if(this->operator_data.convective_problem)
+      {
+        vector u_m = convective_kernel.get_velocity_m(q);
+        vector u_p = convective_kernel.get_velocity_p(q);
+
+        value_flux_p += convective_kernel.calculate_flux_linearized_interior(
+          u_p, u_m, value_p, value_m, normal_p);
+      }
+
+      if(this->operator_data.viscous_problem)
+      {
+        scalar average_viscosity =
+          viscous_kernel.get_viscosity_interior_face(integrator_p.get_face_index(), q);
+
+        gradient_flux +=
+          viscous_kernel.calculate_gradient_flux(value_p, value_m, normal_p, average_viscosity);
+
+        // set exterior gradient to zero
+        vector normal_gradient_m;
+        // multiply by -1.0 since normal vector n⁺ = -n⁻ !!!
+        vector normal_gradient_p = -viscous_kernel.calculate_normal_gradient(q, integrator_p);
+
+        vector value_flux = viscous_kernel.calculate_value_flux(
+          normal_gradient_p, normal_gradient_m, value_p, value_m, normal_p, average_viscosity);
+
+        value_flux_p += -value_flux;
+      }
+
+      integrator_p.submit_value(value_flux_p, q);
+
+      if(this->operator_data.viscous_problem)
+      {
+        integrator_p.submit_gradient(gradient_flux, q);
+      }
+    }
+  }
+
+  // linearized operator
+  void
+  do_boundary_integral(IntegratorFace &           integrator,
+                       OperatorType const &       operator_type,
+                       types::boundary_id const & boundary_id) const
+  {
+    // make sure that this function is only accessed for OperatorType::homogeneous
+    AssertThrow(
+      operator_type == OperatorType::homogeneous,
+      ExcMessage(
+        "For the linearized momentum operator, only OperatorType::homogeneous makes sense."));
+
+    BoundaryTypeU boundary_type = this->operator_data.bc->get_boundary_type(boundary_id);
+
+    for(unsigned int q = 0; q < integrator.n_q_points; ++q)
+    {
+      vector value_m = integrator.get_value(q);
+      vector value_p;
+      vector normal_m = integrator.get_normal_vector(q);
+
+      vector value_flux_m;
+      tensor gradient_flux;
+
+      if(this->operator_data.convective_problem)
+      {
+        // value_p is calculated differently for the convective term and the viscous term
+        value_p = convective_kernel.calculate_exterior_value_linearized(value_m,
+                                                                        q,
+                                                                        integrator,
+                                                                        boundary_type);
+
+        vector u_m = convective_kernel.get_velocity_m(q);
+        vector u_p = convective_kernel.calculate_exterior_value_nonlinear(
+          u_m, q, integrator, boundary_type, boundary_id, this->operator_data.bc, this->eval_time);
+
+        value_flux_m += convective_kernel.calculate_flux_linearized_boundary(
+          u_m, u_p, value_m, value_p, normal_m, boundary_type);
+      }
+
+      if(this->operator_data.viscous_problem)
+      {
+        // value_p is calculated differently for the convective term and the viscous term
+        value_p = calculate_exterior_value(value_m,
+                                           q,
+                                           integrator,
+                                           operator_type,
+                                           boundary_type,
+                                           boundary_id,
+                                           this->operator_data.bc,
+                                           this->eval_time);
+
+        scalar viscosity =
+          viscous_kernel.get_viscosity_boundary_face(integrator.get_face_index(), q);
+        gradient_flux +=
+          viscous_kernel.calculate_gradient_flux(value_m, value_p, normal_m, viscosity);
+
+        vector normal_gradient_m =
+          viscous_kernel.calculate_interior_normal_gradient(q, integrator, operator_type);
+        vector normal_gradient_p = calculate_exterior_normal_gradient(normal_gradient_m,
+                                                                      q,
+                                                                      integrator,
+                                                                      operator_type,
+                                                                      boundary_type,
+                                                                      boundary_id,
+                                                                      this->operator_data.bc,
+                                                                      this->eval_time);
+
+        vector value_flux = viscous_kernel.calculate_value_flux(
+          normal_gradient_m, normal_gradient_p, value_m, value_p, normal_m, viscosity);
+
+        value_flux_m += -value_flux;
+      }
+
+      integrator.submit_value(value_flux_m, q);
+
+      if(this->operator_data.viscous_problem)
+      {
+        integrator.submit_gradient(gradient_flux, q);
+      }
+    }
+  }
+
+
+private:
+  Operators::MassMatrixKernel<dim, Number> mass_kernel;
+  Operators::ConvectiveKernel<dim, Number> convective_kernel;
+  Operators::ViscousKernel<dim, Number>    viscous_kernel;
+};
+
 
 
 } // namespace IncNS
