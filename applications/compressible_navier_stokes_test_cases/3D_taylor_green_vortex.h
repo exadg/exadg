dbfc7c0fbbd26641249d72a6e54000cef995711c
Niklas Fehn <fehn@lnm.mw.tum.de>
2019-09-01 15:04:58 +0200

    Implement periodic box only once

--- applications/compressible_navier_stokes_test_cases/3D_taylor_green_vortex.h
+++ applications/compressible_navier_stokes_test_cases/3D_taylor_green_vortex.h
@@ -8,7 +8,7 @@
 #ifndef APPLICATIONS_COMPRESSIBLE_NAVIER_STOKES_TEST_CASES_3D_TAYLOR_GREEN_VORTEX_H_
 #define APPLICATIONS_COMPRESSIBLE_NAVIER_STOKES_TEST_CASES_3D_TAYLOR_GREEN_VORTEX_H_
 
-#include "../grid_tools/deformed_cube_manifold.h"
+#include "../grid_tools/periodic_box.h"
 #include "../../include/compressible_navier_stokes/postprocessor/postprocessor.h"
 
 /************************************************************************************************************/
@@ -158,73 +158,30 @@ void create_grid_and_set_boundary_ids(std::shared_ptr<parallel::TriangulationBas
 {
   double const pi = numbers::PI;
   double const left = - pi * L, right = pi * L;
-  GridGenerator::subdivided_hyper_cube(*triangulation,n_subdivisions,left,right);
+  double const deformation = 0.1;
 
+  bool curvilinear_mesh = false;
   if(MESH_TYPE == MeshType::Cartesian)
   {
     // do nothing
   }
   else if(MESH_TYPE == MeshType::Curvilinear)
   {
-    double const deformation = 0.1;
-    unsigned int const frequency = 2;
-    static DeformedCubeManifold<dim> manifold(left, right, deformation, frequency);
-    triangulation->set_all_manifold_ids(1);
-    triangulation->set_manifold(1, manifold);
-
-    std::vector<bool> vertex_touched(triangulation->n_vertices(), false);
-
-    for(typename Triangulation<dim>::cell_iterator cell = triangulation->begin();
-        cell != triangulation->end(); ++cell)
-    {
-      for (unsigned int v=0; v < GeometryInfo<dim>::vertices_per_cell; ++v)
-      {
-        if (vertex_touched[cell->vertex_index(v)]==false)
-        {
-          Point<dim> &vertex = cell->vertex(v);
-          Point<dim> new_point = manifold.push_forward(vertex);
-          vertex = new_point;
-          vertex_touched[cell->vertex_index(v)] = true;
-        }
-      }
-    }
+    curvilinear_mesh = true;
   }
-
-  // set boundary indicators
-  AssertThrow(dim == 3, ExcMessage("This test case can only be used for dim==3!"));
-
-  typename Triangulation<dim>::cell_iterator cell = triangulation->begin(), endc = triangulation->end();
-  for(;cell!=endc;++cell)
+  else
   {
-   for(unsigned int face_number=0;face_number < GeometryInfo<dim>::faces_per_cell;++face_number)
-   {
-     // x-direction
-     if((std::fabs(cell->face(face_number)->center()(0) - left)< 1e-12))
-       cell->face(face_number)->set_all_boundary_ids (0);
-     else if((std::fabs(cell->face(face_number)->center()(0) - right)< 1e-12))
-       cell->face(face_number)->set_all_boundary_ids (1);
-     // y-direction
-     else if((std::fabs(cell->face(face_number)->center()(1) - left)< 1e-12))
-       cell->face(face_number)->set_all_boundary_ids (2);
-     else if((std::fabs(cell->face(face_number)->center()(1) - right)< 1e-12))
-       cell->face(face_number)->set_all_boundary_ids (3);
-     // z-direction
-     else if((std::fabs(cell->face(face_number)->center()(2) - left)< 1e-12))
-       cell->face(face_number)->set_all_boundary_ids (4);
-     else if((std::fabs(cell->face(face_number)->center()(2) - right)< 1e-12))
-       cell->face(face_number)->set_all_boundary_ids (5);
-   }
+    AssertThrow(false, ExcMessage("Not implemented."));
   }
 
-  auto tria = dynamic_cast<Triangulation<dim>*>(&*triangulation);
-  GridTools::collect_periodic_faces(*tria, 0, 1, 0 /*x-direction*/, periodic_faces);
-  GridTools::collect_periodic_faces(*tria, 2, 3, 1 /*y-direction*/, periodic_faces);
-  GridTools::collect_periodic_faces(*tria, 4, 5, 2 /*z-direction*/, periodic_faces);
-
-  triangulation->add_periodicity(periodic_faces);
-
-  // perform global refinements
-  triangulation->refine_global(n_refine_space);
+  create_periodic_box(triangulation,
+                      n_refine_space,
+                      periodic_faces,
+                      n_subdivisions,
+                      left,
+                      right,
+                      curvilinear_mesh,
+                      deformation);
 }
 
 /************************************************************************************************************/
