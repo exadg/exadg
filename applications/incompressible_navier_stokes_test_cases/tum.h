d8156cb08f67f658565ad6cda6fbe1550e9bbfc7
Niklas Fehn <fehn@lnm.mw.tum.de>
2019-10-14 09:37:26 +0200

    extend tum.h test case to 3D, write surface mesh and boundary IDs

--- applications/incompressible_navier_stokes_test_cases/tum.h
+++ applications/incompressible_navier_stokes_test_cases/tum.h
@@ -16,9 +16,11 @@
 /*                                                                                                          */
 /************************************************************************************************************/
 
+unsigned int const DIMENSION = 3;
+
 // convergence studies in space or time
-unsigned int const DEGREE_MIN = 2;
-unsigned int const DEGREE_MAX = 2;
+unsigned int const DEGREE_MIN = 3;
+unsigned int const DEGREE_MAX = 3;
 
 unsigned int const REFINE_SPACE_MIN = 3;
 unsigned int const REFINE_SPACE_MAX = 3;
@@ -32,16 +34,16 @@ const double L = 1.0;
 const double MAX_VELOCITY = 1.0;
 const double VISCOSITY = 1.0e-3;
 
-std::string OUTPUT_FOLDER = "output/tum/";
+std::string OUTPUT_FOLDER = "/data/fehn/navierstokes/applications/output/tum_test/";
 std::string OUTPUT_FOLDER_VTU = OUTPUT_FOLDER + "vtu/";
-std::string OUTPUT_NAME = "test";
+std::string OUTPUT_NAME = "tum_Re1000_3d";
 
 namespace IncNS
 {
 void set_input_parameters(InputParameters &param)
 {
   // MATHEMATICAL MODEL
-  param.dim = 2;
+  param.dim = DIMENSION;
   param.problem_type = PROBLEM_TYPE;
   param.equation_type = EquationType::NavierStokes;
   param.formulation_viscous_term = FormulationViscousTerm::LaplaceFormulation;
@@ -58,13 +60,13 @@ void set_input_parameters(InputParameters &param)
 
   // TEMPORAL DISCRETIZATION
   param.solver_type = SolverType::Unsteady;
-  param.temporal_discretization = TemporalDiscretization::BDFPressureCorrection;
-  param.treatment_of_convective_term = TreatmentOfConvectiveTerm::Implicit;
+  param.temporal_discretization = TemporalDiscretization::BDFDualSplittingScheme;
+  param.treatment_of_convective_term = TreatmentOfConvectiveTerm::Explicit;
   param.calculation_of_time_step_size = TimeStepCalculation::CFL;
   // best practice: use adaptive time stepping for this test case to avoid adjusting the CFL number
   param.adaptive_time_stepping = true;
   param.max_velocity = 1.0;
-  param.cfl = 0.25;
+  param.cfl = 0.4;
   param.cfl_exponent_fe_degree_velocity = 1.5;
   param.time_step_size = 5.0e-2;
   param.order_time_integrator = 2; // 1; // 2; // 3;
@@ -101,12 +103,21 @@ void set_input_parameters(InputParameters &param)
 
   // pressure Poisson equation
   param.solver_pressure_poisson = SolverPressurePoisson::CG;
-  param.solver_data_pressure_poisson = SolverData(1000,1.e-12,1.e-6,100);
+  param.solver_data_pressure_poisson = SolverData(1000,1.e-12,1.e-3,100);
   param.preconditioner_pressure_poisson = PreconditionerPressurePoisson::Multigrid;
+  param.multigrid_data_pressure_poisson.type = MultigridType::cphMG;
+  param.multigrid_data_pressure_poisson.p_sequence = PSequenceType::Bisect;
+  // MG smoother
+  param.multigrid_data_pressure_poisson.smoother_data.smoother = MultigridSmoother::Chebyshev;
+  param.multigrid_data_pressure_poisson.smoother_data.iterations = 5;
+  // MG coarse grid solver
+  param.multigrid_data_pressure_poisson.coarse_problem.solver = MultigridCoarseGridSolver::CG;
+  param.multigrid_data_pressure_poisson.coarse_problem.preconditioner = MultigridCoarseGridPreconditioner::AMG;
+  param.multigrid_data_pressure_poisson.coarse_problem.solver_data.rel_tol = 1.e-3;
 
   // projection step
   param.solver_projection = SolverProjection::CG;
-  param.solver_data_projection = SolverData(1000, 1.e-12, 1.e-6);
+  param.solver_data_projection = SolverData(1000, 1.e-12, 1.e-3);
   param.preconditioner_projection = PreconditionerProjection::InverseMassMatrix;
 
   // HIGH-ORDER DUAL SPLITTING SCHEME
@@ -116,8 +127,8 @@ void set_input_parameters(InputParameters &param)
 
   // viscous step
   param.solver_viscous = SolverViscous::CG;
-  param.solver_data_viscous = SolverData(1000,1.e-12,1.e-6);
-  param.preconditioner_viscous = PreconditionerViscous::InverseMassMatrix; //Multigrid;
+  param.solver_data_viscous = SolverData(1000,1.e-12,1.e-3);
+  param.preconditioner_viscous = PreconditionerViscous::InverseMassMatrix;
 
   // PRESSURE-CORRECTION SCHEME
 
@@ -171,6 +182,68 @@ void set_input_parameters(InputParameters &param)
 /*                                                                                                          */
 /************************************************************************************************************/
 
+void create_triangulation(Triangulation<2> &tria)
+{
+  Triangulation<2> tria_h1, tria_h2, tria_h3, tria_v1, tria_v2, tria_v3, tria_v4, tria_v5;
+
+  GridGenerator::subdivided_hyper_rectangle(tria_h1,
+                                           std::vector<unsigned int>({4,1}),
+                                           Point<2>(0.0,0.0),
+                                           Point<2>(4.0*L,-1.0*L));
+
+  GridGenerator::subdivided_hyper_rectangle(tria_h2,
+                                           std::vector<unsigned int>({1,1}),
+                                           Point<2>(4.0*L,-4.0*L),
+                                           Point<2>(5.0*L,-5.0*L));
+
+  GridGenerator::subdivided_hyper_rectangle(tria_h3,
+                                           std::vector<unsigned int>({5,1}),
+                                           Point<2>(5.0*L,0.0*L),
+                                           Point<2>(10.0*L,-1.0*L));
+
+  GridGenerator::subdivided_hyper_rectangle(tria_v1,
+                                           std::vector<unsigned int>({1,4}),
+                                           Point<2>(1.0*L,-1.0*L),
+                                           Point<2>(2.0*L,-5.0*L));
+
+  GridGenerator::subdivided_hyper_rectangle(tria_v2,
+                                           std::vector<unsigned int>({1,4}),
+                                           Point<2>(3.0*L,-1.0*L),
+                                           Point<2>(4.0*L,-5.0*L));
+
+  GridGenerator::subdivided_hyper_rectangle(tria_v3,
+                                           std::vector<unsigned int>({1,4}),
+                                           Point<2>(5.0*L,-1.0*L),
+                                           Point<2>(6.0*L,-5.0*L));
+
+  GridGenerator::subdivided_hyper_rectangle(tria_v4,
+                                           std::vector<unsigned int>({1,4}),
+                                           Point<2>(7.0*L,-1.0*L),
+                                           Point<2>(8.0*L,-5.0*L));
+
+  GridGenerator::subdivided_hyper_rectangle(tria_v5,
+                                           std::vector<unsigned int>({1,4}),
+                                           Point<2>(9.0*L,-1.0*L),
+                                           Point<2>(10.0*L,-5.0*L));
+
+  // merge
+  Triangulation<2> tmp1, tmp2;
+  GridGenerator::merge_triangulations (tria_h1, tria_v1, tmp1);
+  GridGenerator::merge_triangulations (tmp1, tria_v2, tmp2);
+  GridGenerator::merge_triangulations (tmp2, tria_h2, tmp1);
+  GridGenerator::merge_triangulations (tmp1, tria_v3, tmp2);
+  GridGenerator::merge_triangulations (tmp2, tria_h3, tmp1);
+  GridGenerator::merge_triangulations (tmp1, tria_v4, tmp2);
+  GridGenerator::merge_triangulations (tmp2, tria_v5, tria);
+}
+
+void create_triangulation(Triangulation<3> &tria)
+{
+  Triangulation<2> tria_2d;
+  create_triangulation(tria_2d);
+  GridGenerator::extrude_triangulation(tria_2d, 2, L, tria);
+}
+
 template<int dim>
 void
 create_grid_and_set_boundary_ids(std::shared_ptr<parallel::TriangulationBase<dim>> triangulation,
@@ -180,67 +253,10 @@ create_grid_and_set_boundary_ids(std::shared_ptr<parallel::TriangulationBase<dim
 {
  (void)periodic_faces;
 
- if(dim == 2)
- {
-   Triangulation<dim> tria_h1, tria_h2, tria_h3, tria_v1, tria_v2, tria_v3, tria_v4, tria_v5;
+ create_triangulation(*triangulation);
 
-   GridGenerator::subdivided_hyper_rectangle(tria_h1,
-                                             std::vector<unsigned int>({4,1}),
-                                             Point<dim>(0.0,0.0),
-                                             Point<dim>(4.0*L,-1.0*L));
-
-   GridGenerator::subdivided_hyper_rectangle(tria_h2,
-                                             std::vector<unsigned int>({1,1}),
-                                             Point<dim>(4.0*L,-4.0*L),
-                                             Point<dim>(5.0*L,-5.0*L));
-
-   GridGenerator::subdivided_hyper_rectangle(tria_h3,
-                                             std::vector<unsigned int>({5,1}),
-                                             Point<dim>(5.0*L,0.0*L),
-                                             Point<dim>(10.0*L,-1.0*L));
-
-   GridGenerator::subdivided_hyper_rectangle(tria_v1,
-                                             std::vector<unsigned int>({1,4}),
-                                             Point<dim>(1.0*L,-1.0*L),
-                                             Point<dim>(2.0*L,-5.0*L));
-
-   GridGenerator::subdivided_hyper_rectangle(tria_v2,
-                                             std::vector<unsigned int>({1,4}),
-                                             Point<dim>(3.0*L,-1.0*L),
-                                             Point<dim>(4.0*L,-5.0*L));
-
-   GridGenerator::subdivided_hyper_rectangle(tria_v3,
-                                             std::vector<unsigned int>({1,4}),
-                                             Point<dim>(5.0*L,-1.0*L),
-                                             Point<dim>(6.0*L,-5.0*L));
-
-   GridGenerator::subdivided_hyper_rectangle(tria_v4,
-                                             std::vector<unsigned int>({1,4}),
-                                             Point<dim>(7.0*L,-1.0*L),
-                                             Point<dim>(8.0*L,-5.0*L));
-
-   GridGenerator::subdivided_hyper_rectangle(tria_v5,
-                                             std::vector<unsigned int>({1,4}),
-                                             Point<dim>(9.0*L,-1.0*L),
-                                             Point<dim>(10.0*L,-5.0*L));
-
-   // merge
-   Triangulation<dim> tmp1, tmp2;
-   GridGenerator::merge_triangulations (tria_h1, tria_v1, tmp1);
-   GridGenerator::merge_triangulations (tmp1, tria_v2, tmp2);
-   GridGenerator::merge_triangulations (tmp2, tria_h2, tmp1);
-   GridGenerator::merge_triangulations (tmp1, tria_v3, tmp2);
-   GridGenerator::merge_triangulations (tmp2, tria_h3, tmp1);
-   GridGenerator::merge_triangulations (tmp1, tria_v4, tmp2);
-   GridGenerator::merge_triangulations (tmp2, tria_v5, *triangulation);
-
-   // global refinements
-   triangulation->refine_global(n_refine_space);
- }
- else if(dim == 3)
- {
-   AssertThrow(false, ExcMessage("NotImplemented"));
- }
+ // global refinements
+ triangulation->refine_global(n_refine_space);
 
  // set boundary indicator
  // all boundaries have ID = 0 by default -> Dirichlet boundaries
@@ -256,6 +272,13 @@ create_grid_and_set_boundary_ids(std::shared_ptr<parallel::TriangulationBase<dim
     // outflow boundary
     if ((std::fabs(cell->face(face_number)->center()(1) - (-5.0*L))< 1e-12) && (cell->face(face_number)->center()(0)- 9.0*L)>=0)
        cell->face(face_number)->set_boundary_id (2);
+
+    // after extrude triangulation to create the 3d mesh, the boundary IDs on the z-orthogonal boundaries are not 0!
+    if(dim == 3)
+    {
+      if ((std::fabs(cell->face(face_number)->center()(2))< 1e-12) || (std::fabs(cell->face(face_number)->center()(2) - (L))< 1e-12))
+        cell->face(face_number)->set_boundary_id (0);
+    }
    }
  }
 }
@@ -355,7 +378,13 @@ construct_postprocessor(InputParameters const &param)
   pp_data.output_data.output_start_time = param.start_time;
   pp_data.output_data.output_interval_time = (param.end_time-param.start_time)/200;
   pp_data.output_data.write_divergence = true;
-  pp_data.output_data.write_streamfunction = false;
+  pp_data.output_data.write_vorticity = true;
+  pp_data.output_data.write_vorticity_magnitude = true;
+  pp_data.output_data.write_q_criterion = true;
+  pp_data.output_data.write_velocity_magnitude = true;
+  pp_data.output_data.write_surface_mesh = true;
+  pp_data.output_data.write_boundary_IDs = true;
+  pp_data.output_data.write_higher_order = false;
   pp_data.output_data.degree = param.degree_u;
 
   std::shared_ptr<PostProcessorBase<dim,Number> > pp;
