d5e0b8056df8be2c61d130cc75a624b168af27e4
Niklas Fehn <fehn@lnm.mw.tum.de>
2020-02-14 10:39:50 +0100

    forgot to add new file matrix_free_wrapper.h

---
+++ include/functionalities/matrix_free_wrapper.h
@@ -0,0 +1,81 @@
+/*
+ * matrix_free_wrapper.h
+ *
+ *  Created on: Feb 14, 2020
+ *      Author: fehn
+ */
+
+#ifndef INCLUDE_FUNCTIONALITIES_MATRIX_FREE_WRAPPER_H_
+#define INCLUDE_FUNCTIONALITIES_MATRIX_FREE_WRAPPER_H_
+
+#include <deal.II/matrix_free/matrix_free.h>
+
+#include "mesh.h"
+
+using namespace dealii;
+
+template<int dim, typename Number>
+struct MatrixFreeWrapper
+{
+  MatrixFreeWrapper(std::shared_ptr<Mesh<dim>> mesh_in) : mesh(mesh_in)
+  {
+    matrix_free.reset(new MatrixFree<dim, Number>());
+  }
+
+  /*
+   * Returns pointer to actual matrix-free object.
+   */
+  std::shared_ptr<MatrixFree<dim, Number>>
+  get_matrix_free()
+  {
+    return matrix_free;
+  }
+
+  /*
+   * This function performs a "complete" reinit() of MatrixFree<dim, Number>.
+   */
+  void
+  reinit()
+  {
+    matrix_free->reinit(mesh->get_mapping(), dof_handler_vec, constraint_vec, quadrature_vec, data);
+  }
+
+  /*
+   * This function only updates geometry terms and is called in combination with
+   * moving mesh (ALE) methods.
+   *
+   * TODO: MatrixFree<dim, Number> should provide a separate function
+   *
+   *  matrix_free->update_geometry()
+   *
+   * for this purpose.
+   */
+  void
+  update_geometry()
+  {
+    matrix_free->reinit(
+      mesh->get_mapping(), dof_handler_vec, constraint_vec, quadrature_vec, data_update_geometry);
+  }
+
+  // the actual MatrixFree object
+  std::shared_ptr<MatrixFree<dim, Number>> matrix_free;
+
+  // collection of data structures required for initialization and update of matrix_free
+
+  // mesh containing mapping information
+  std::shared_ptr<Mesh<dim>> mesh;
+
+  // additional data
+  typename MatrixFree<dim, Number>::AdditionalData data;
+  // another data object since we only want to update what is really necessary, so that
+  // the update is computationally efficient
+  typename MatrixFree<dim, Number>::AdditionalData data_update_geometry;
+
+  // DoFHandler, Constraint, Quadrature vectors
+  std::vector<DoFHandler<dim> const *>           dof_handler_vec;
+  std::vector<AffineConstraints<double> const *> constraint_vec;
+  std::vector<Quadrature<1>>                     quadrature_vec;
+};
+
+
+#endif /* INCLUDE_FUNCTIONALITIES_MATRIX_FREE_WRAPPER_H_ */
