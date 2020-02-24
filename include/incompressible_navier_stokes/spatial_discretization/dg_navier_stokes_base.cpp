562fe7d67f296291bbb9e8590aecc4191df7bf64
Niklas Fehn <fehn@lnm.mw.tum.de>
2020-02-24 13:03:22 +0100

    IncNS: in function update_after_mesh_movement(), call member function of viscous_kernel instead of viscous_operator

--- include/incompressible_navier_stokes/spatial_discretization/dg_navier_stokes_base.cpp
+++ include/incompressible_navier_stokes/spatial_discretization/dg_navier_stokes_base.cpp
@@ -1201,9 +1201,7 @@ DGNavierStokesBase<dim, Number>::update_after_mesh_movement()
   {
     // update SIPG penalty parameter of viscous operator which depends on the deformation
     // of elements
-    // viscous operator has a pointer to viscous_kernel and updates penalty parameter of
-    // viscous_kernel
-    viscous_operator.update();
+    viscous_kernel->calculate_penalty_parameter(*matrix_free, get_dof_index_velocity());
   }
 
   // note that the update of div-div and continuity penalty terms is done separately
