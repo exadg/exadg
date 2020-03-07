9942bb6616e9532047f06e077b019a41de5d6a55
Peter Munch <peterrmuench@gmail.com>
2020-03-07 08:55:58 +0100

    First draft of using NoncontiguousPartitioner

--- 3rdparty/deal.spectrum/src/deal-spectrum.h
+++ 3rdparty/deal.spectrum/src/deal-spectrum.h
@@ -16,6 +16,8 @@
 #include "./includes/spectrum.h"
 #include "./util/timer.h"
 
+#include <deal.II/base/mpi_noncontiguous_partitioner.h>
+
 namespace dealspectrum
 {
 /**
@@ -59,19 +61,50 @@ public:
    */
   template<class Tria>
   void
-  init(int dim, int cells, int points_src, int points_dst, Tria & tria)
+  init(int dim, int n_cells_1D, int points_src, int points_dst, Tria & tria)
   {
     // init setup ...
-    s.init(dim, cells, points_src, points_dst);
+    s.init(dim, n_cells_1D, points_src, points_dst);
 
     // ... mapper
     timer.start("Init-Map");
     h.init(tria);
     timer.stop("Init-Map");
-
+    
+    std::vector<types::global_dof_index> local_cells;
+    
+    auto norm_point_to_lex = [&](const auto c) {
+      // convert normalized point [0, 1] to lex
+      if (dim == 2)
+        return std::floor(c[0]) + n_cells_1D * std::floor(c[1]);
+      else
+        return std::floor(c[0]) + n_cells_1D * std::floor(c[1]) +
+               n_cells_1D * n_cells_1D * std::floor(c[2]);
+    };
+    
+    for (const auto &cell : tria.active_cell_iterators())
+      if (cell->is_active() && cell->is_locally_owned())
+        {
+          auto c = cell->center();
+          for (int i = 0; i < dim; i++)
+            c[i] = (c[i]+dealii::numbers::PI) / (2 * dealii::numbers::PI / n_cells_1D);
+    
+          local_cells.push_back(norm_point_to_lex(c));
+    
+        }
+    
+    //for(auto i : local_cells)
+    //    std::cout << i << std::endl;
+    //std::cout << std::endl << std::endl;
+    
+    int n_local_cells = local_cells.size();
+    int global_offset = 0;
+    
+    MPI_Exscan(&n_local_cells, &global_offset, 1, MPI_INT, MPI_SUM, comm);
+    
     // ... interpolation
     timer.start("Init-Ipol");
-    ipol.init(h);
+    ipol.init(global_offset, global_offset + n_local_cells);
     timer.stop("Init-Ipol");
     
     // FFTW will be called externally
@@ -82,6 +115,34 @@ public:
     timer.start("Init-FFTW");
     fftw.init();
     timer.stop("Init-FFTW");
+    
+    int start;
+    int end;
+    fftw.getLocalRange(start, end);
+    
+    std::vector<types::global_dof_index> indices_has, indices_want;
+    
+    for(const auto & I : local_cells)
+      for(unsigned int i = 0; i < Utilities::pow(points_dst, dim); i++)
+        for(types::global_dof_index d =0; d  < dim ; d++)
+        {
+            
+            types::global_dof_index index = 
+                    (I / (n_cells_1D * n_cells_1D) * points_dst + i / (points_dst * points_dst)) * Utilities::pow(n_cells_1D * points_dst, 2) + 
+                    (((I / n_cells_1D) % n_cells_1D) * points_dst + ((i / points_dst) % points_dst)) * Utilities::pow(n_cells_1D * points_dst, 1) + 
+                    (I % (n_cells_1D             ) * points_dst + i % (points_dst             )); 
+                    
+            
+            //std::cout << index << std::endl;
+            indices_has.push_back(d * Utilities::pow(points_dst * n_cells_1D, dim) + index);
+        }
+    
+    for(types::global_dof_index d =0; d  < dim ; d++)
+      for(int j = start; j < end; j++)
+        for(unsigned int i = 0; i < Utilities::pow(points_dst*n_cells_1D, dim-1); i++)
+            indices_want.push_back(d * Utilities::pow(points_dst * n_cells_1D, dim) + j * Utilities::pow(points_dst*n_cells_1D, dim-1) + i);
+    
+    nonconti = std::make_shared<Utilities::MPI::NoncontiguousPartitioner<double>>(indices_has, indices_want, comm);
 
     // ... permutation
     timer.start("Init-Perm");
@@ -118,8 +179,46 @@ public:
 
       // ... permute
       timer.start("Permutation");
-      fftc.ipermute(ipol.dst, fftw.u_real);
-      fftc.iwait();
+      ArrayView<double> dst(fftw.u_real, s.dim * Utilities::pow(static_cast<types::global_dof_index>(s.cells * s.points_dst), s.dim) *2);
+      ArrayView<double> src_(ipol.dst, s.dim * Utilities::pow(static_cast<types::global_dof_index>(s.cells * s.points_dst), s.dim));
+      nonconti->update_values(dst, src_);
+      
+      types::global_dof_index N  = s.cells * s.points_dst;
+      types::global_dof_index Nx = (N / 2 + 1) * 2;
+      
+      int start;
+      int end;
+      fftw.getLocalRange(start, end);
+      
+      //for (int k = 0; k < (end - start)*s.dim ; k++)
+      //  for (int j = 0; j < N; j++)
+      //    for (int i = 0; i < N; i++)
+      //      std::cout << dst[N * N * k + N * j + i] << " ";
+      //  
+      //std::cout << std::endl << std::endl;
+      
+      for (int k = (end - start)*s.dim - 1; k >= 0 ; k--)
+        for (int j = N - 1; j >= 0; j--)
+          for (int i = N-1; i >= 0; i--)
+            dst[N * Nx * k + Nx * j + i] = dst[N * N * k + N * j + i];
+      
+      //for (int k = 0; k < (end - start)*s.dim ; k++)
+      //  for (int j = 0; j < N; j++)
+      //    for (int i = 0; i < N; i++)
+      //      std::cout << dst[N * Nx * k + Nx * j + i] << " ";
+      //  
+      //std::cout << std::endl << std::endl;
+      
+      //fftc.ipermute(ipol.dst, fftw.u_real);
+      //fftc.iwait();
+      
+      //for (int k = 0; k < (end - start)*s.dim ; k++)
+      //  for (int j = 0; j < N; j++)
+      //    for (int i = 0; i < N; i++)
+      //      std::cout << dst[N * Nx * k + Nx * j + i] << " ";
+      //  
+      //std::cout << std::endl << std::endl;
+        
       timer.append("Permutation");
 
       // ... fft
@@ -183,6 +282,8 @@ private:
 
   // Timer
   DealSpectrumTimer timer;
+  
+  std::shared_ptr<Utilities::MPI::NoncontiguousPartitioner<double>> nonconti;
 };
 
 } // namespace dealspectrum
