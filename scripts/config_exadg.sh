28920a6fe70d983c38d70ecf079f1eabe092d877
Niklas Fehn <fehn@lnm.mw.tum.de>
2020-08-17 18:26:26 +0200

    adjust config_exadg.sh script and gitignore

--- scripts/config_exadg.sh
+++ scripts/config_exadg.sh
@@ -2,6 +2,6 @@ rm -rf CMakeFiles/ CMakeCache.txt
 
 # issue the build - must usually not be modified
 cmake \
-    -D DEAL_II_DIR="$WORKING_DIRECTORY/sw/build" \
+    -D DEAL_II_DIR="$WORKING_DIRECTORY/sw/dealii_build" \
     -D USE_DEAL_SPECTRUM=ON \
-    .
+    ../exadg
