311372bb9475a7bd592da5e5252cc494529d1fb4
Niklas Fehn <fehn@lnm.mw.tum.de>
2020-09-10 17:44:46 +0200

    use cmake options instead of ENV

--- scripts/config_exadg.sh
+++ scripts/config_exadg.sh
@@ -6,4 +6,6 @@ cmake \
     -D USE_DEAL_SPECTRUM=ON \
     -D FFTW_INC="$WORKING_DIRECTORY/sw/fftw-3.3.7-install/include" \
     -D FFTW_LIB="$WORKING_DIRECTORY/sw/fftw-3.3.7-install/lib/combined" \
+    -D LIKWID_LIB="/path_to_likwid/lib" \
+    -D LIKWID_INCLUDE="/path_to_likwid/include" \
     ../
