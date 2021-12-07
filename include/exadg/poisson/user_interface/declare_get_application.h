7140a5b5152cee6fb7a57baf6c53aa05fd4e4913
Niklas Fehn <fehn@lnm.mw.tum.de>
2021-12-07 15:24:28 +0100

    add ExaDG header to all new files

--- include/exadg/poisson/user_interface/declare_get_application.h
+++ include/exadg/poisson/user_interface/declare_get_application.h
@@ -1,8 +1,22 @@
-/*
- * declare_get_application.h
+/*  ______________________________________________________________________
  *
- *  Created on: Dec 7, 2021
- *      Author: fehn
+ *  ExaDG - High-Order Discontinuous Galerkin for the Exa-Scale
+ *
+ *  Copyright (C) 2021 by the ExaDG authors
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  ______________________________________________________________________
  */
 
 #ifndef INCLUDE_EXADG_POISSON_USER_INTERFACE_DECLARE_GET_APPLICATION_H_
