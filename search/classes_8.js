b3644a0c07980abcedd6dfc4355e43d6b6eabdad
kronbichler <kronbichler@users.noreply.github.com>
2022-01-11 17:56:35 +0000

    deploy: 570531a9854dc684002c62161480c6d6d0811808

--- search/classes_8.js
+++ search/classes_8.js
@@ -3,13 +3,12 @@ var searchData=
   ['inflowdata_0',['InflowData',['../structExaDG_1_1IncNS_1_1InflowData.html',1,'ExaDG::IncNS']]],
   ['inflowdatacalculator_1',['InflowDataCalculator',['../classExaDG_1_1IncNS_1_1InflowDataCalculator.html',1,'ExaDG::IncNS']]],
   ['integratorflags_2',['IntegratorFlags',['../structExaDG_1_1IntegratorFlags.html',1,'ExaDG']]],
-  ['interfacecommunicator_3',['InterfaceCommunicator',['../classExaDG_1_1InterfaceCommunicator.html',1,'ExaDG']]],
-  ['interfacecoupling_4',['InterfaceCoupling',['../classExaDG_1_1InterfaceCoupling.html',1,'ExaDG']]],
-  ['interpolator_5',['Interpolator',['../classdealspectrum_1_1Interpolator.html',1,'dealspectrum::Interpolator'],['../structExaDG_1_1Interpolator.html',1,'ExaDG::Interpolator&lt; rank, dim, Number &gt;']]],
-  ['interpolator_3c_200_2c_20dim_2c_20number_20_3e_6',['Interpolator&lt; 0, dim, Number &gt;',['../structExaDG_1_1Interpolator_3_010_00_01dim_00_01Number_01_4.html',1,'ExaDG']]],
-  ['interpolator_3c_201_2c_20dim_2c_20number_20_3e_7',['Interpolator&lt; 1, dim, Number &gt;',['../structExaDG_1_1Interpolator_3_011_00_01dim_00_01Number_01_4.html',1,'ExaDG']]],
-  ['inversemassoperator_8',['InverseMassOperator',['../classExaDG_1_1InverseMassOperator.html',1,'ExaDG']]],
-  ['inversemasspreconditioner_9',['InverseMassPreconditioner',['../classExaDG_1_1Elementwise_1_1InverseMassPreconditioner.html',1,'ExaDG::Elementwise::InverseMassPreconditioner&lt; dim, n_components, Number &gt;'],['../classExaDG_1_1InverseMassPreconditioner.html',1,'ExaDG::InverseMassPreconditioner&lt; dim, n_components, Number &gt;']]],
-  ['iterativesolver_10',['IterativeSolver',['../classExaDG_1_1Elementwise_1_1IterativeSolver.html',1,'ExaDG::Elementwise']]],
-  ['iterativesolverdata_11',['IterativeSolverData',['../structExaDG_1_1Elementwise_1_1IterativeSolverData.html',1,'ExaDG::Elementwise']]]
+  ['interfacecoupling_3',['InterfaceCoupling',['../classExaDG_1_1InterfaceCoupling.html',1,'ExaDG']]],
+  ['interpolator_4',['Interpolator',['../classdealspectrum_1_1Interpolator.html',1,'dealspectrum::Interpolator'],['../structExaDG_1_1Interpolator.html',1,'ExaDG::Interpolator&lt; rank, dim, Number &gt;']]],
+  ['interpolator_3c_200_2c_20dim_2c_20number_20_3e_5',['Interpolator&lt; 0, dim, Number &gt;',['../structExaDG_1_1Interpolator_3_010_00_01dim_00_01Number_01_4.html',1,'ExaDG']]],
+  ['interpolator_3c_201_2c_20dim_2c_20number_20_3e_6',['Interpolator&lt; 1, dim, Number &gt;',['../structExaDG_1_1Interpolator_3_011_00_01dim_00_01Number_01_4.html',1,'ExaDG']]],
+  ['inversemassoperator_7',['InverseMassOperator',['../classExaDG_1_1InverseMassOperator.html',1,'ExaDG']]],
+  ['inversemasspreconditioner_8',['InverseMassPreconditioner',['../classExaDG_1_1Elementwise_1_1InverseMassPreconditioner.html',1,'ExaDG::Elementwise::InverseMassPreconditioner&lt; dim, n_components, Number &gt;'],['../classExaDG_1_1InverseMassPreconditioner.html',1,'ExaDG::InverseMassPreconditioner&lt; dim, n_components, Number &gt;']]],
+  ['iterativesolver_9',['IterativeSolver',['../classExaDG_1_1Elementwise_1_1IterativeSolver.html',1,'ExaDG::Elementwise']]],
+  ['iterativesolverdata_10',['IterativeSolverData',['../structExaDG_1_1Elementwise_1_1IterativeSolverData.html',1,'ExaDG::Elementwise']]]
 ];
--- search/functions_5.js
+++ search/functions_5.js
@@ -3,13 +3,12 @@ var searchData=
   ['get_5fdynamic_5fmapping_0',['get_dynamic_mapping',['../classExaDG_1_1Grid.html#a161679f78e03efa20d72ed864684607c',1,'ExaDG::Grid']]],
   ['get_5fequidistant_5finner_1',['get_equidistant_inner',['../classdealspectrum_1_1Interpolator.html#a1d9b8f5517c976d12a6b3fb499e21f73',1,'dealspectrum::Interpolator']]],
   ['get_5fmapping_2',['get_mapping',['../classExaDG_1_1GridMotionBase.html#a7556b2f9c23f5d779a60522587a65337',1,'ExaDG::GridMotionBase::get_mapping()'],['../classExaDG_1_1GridMotionInterface.html#a7e956de280a8172ca323656a48bff805',1,'ExaDG::GridMotionInterface::get_mapping()']]],
-  ['get_5fremote_5fquadrature_5fpoints_3',['get_remote_quadrature_points',['../classExaDG_1_1InterfaceCommunicator.html#aaeb7ceb83c0a4cd22b6cd9082086a573',1,'ExaDG::InterfaceCommunicator']]],
-  ['get_5fresults_4',['get_results',['../classdealspectrum_1_1SpectralAnalysis.html#a83c895003af56a97b5004e8a429527d3',1,'dealspectrum::SpectralAnalysis']]],
-  ['get_5fstatic_5fmapping_5',['get_static_mapping',['../classExaDG_1_1Grid.html#a2362d0077a381cbb795d7242dcdb6cb5',1,'ExaDG::Grid']]],
-  ['getlocalrange_6',['getLocalRange',['../classdealspectrum_1_1SpectralAnalysis.html#ad521541cf55622d07d58e83ddb360ed7',1,'dealspectrum::SpectralAnalysis']]],
-  ['grid_7',['Grid',['../classExaDG_1_1Grid.html#a297f77ffd33774085f95d9c98e4f9add',1,'ExaDG::Grid']]],
-  ['gridmotionanalytical_8',['GridMotionAnalytical',['../classExaDG_1_1GridMotionAnalytical.html#aa7d4ce366b3528ca39e1a26c7fb3966d',1,'ExaDG::GridMotionAnalytical']]],
-  ['gridmotionbase_9',['GridMotionBase',['../classExaDG_1_1GridMotionBase.html#aa8fde42c8063336691faaceece56386d',1,'ExaDG::GridMotionBase']]],
-  ['gridmotionelasticity_10',['GridMotionElasticity',['../classExaDG_1_1GridMotionElasticity.html#a4e95b2cdf0702d9e8744cfd53b3d427f',1,'ExaDG::GridMotionElasticity']]],
-  ['gridmotionpoisson_11',['GridMotionPoisson',['../classExaDG_1_1GridMotionPoisson.html#af7251d0b2c7aae6e1757cc26ab62bdc4',1,'ExaDG::GridMotionPoisson']]]
+  ['get_5fresults_3',['get_results',['../classdealspectrum_1_1SpectralAnalysis.html#a83c895003af56a97b5004e8a429527d3',1,'dealspectrum::SpectralAnalysis']]],
+  ['get_5fstatic_5fmapping_4',['get_static_mapping',['../classExaDG_1_1Grid.html#a2362d0077a381cbb795d7242dcdb6cb5',1,'ExaDG::Grid']]],
+  ['getlocalrange_5',['getLocalRange',['../classdealspectrum_1_1SpectralAnalysis.html#ad521541cf55622d07d58e83ddb360ed7',1,'dealspectrum::SpectralAnalysis']]],
+  ['grid_6',['Grid',['../classExaDG_1_1Grid.html#a297f77ffd33774085f95d9c98e4f9add',1,'ExaDG::Grid']]],
+  ['gridmotionanalytical_7',['GridMotionAnalytical',['../classExaDG_1_1GridMotionAnalytical.html#aa7d4ce366b3528ca39e1a26c7fb3966d',1,'ExaDG::GridMotionAnalytical']]],
+  ['gridmotionbase_8',['GridMotionBase',['../classExaDG_1_1GridMotionBase.html#aa8fde42c8063336691faaceece56386d',1,'ExaDG::GridMotionBase']]],
+  ['gridmotionelasticity_9',['GridMotionElasticity',['../classExaDG_1_1GridMotionElasticity.html#a4e95b2cdf0702d9e8744cfd53b3d427f',1,'ExaDG::GridMotionElasticity']]],
+  ['gridmotionpoisson_10',['GridMotionPoisson',['../classExaDG_1_1GridMotionPoisson.html#af7251d0b2c7aae6e1757cc26ab62bdc4',1,'ExaDG::GridMotionPoisson']]]
 ];
