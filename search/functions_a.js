03b59501d6dd0e52422e9bdee61ee1aa7f9f96de
nfehn <nfehn@users.noreply.github.com>
2022-03-10 08:37:42 +0000

    deploy: cd64b71c62b421db5f9e55c6ab770438d121df69

--- search/functions_a.js
+++ search/functions_a.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['serialize_0',['serialize',['../classdealspectrum_1_1Interpolator.html#a8283a193758ed7bc6fd68fd2f3cd9847',1,'dealspectrum::Interpolator::serialize(char const *filename)'],['../classdealspectrum_1_1Interpolator.html#a6b09f98b6b45095290fa3ea8269f0c16',1,'dealspectrum::Interpolator::serialize(char const *filename, double const *&amp;src)'],['../classdealspectrum_1_1SpectralAnalysis.html#a5823986be9b1e140e8a408cdcdd5b59c',1,'dealspectrum::SpectralAnalysis::serialize()']]],
   ['setup_1',['Setup',['../classdealspectrum_1_1Setup.html#ae48fcc6968bba36f4300ec7e88009717',1,'dealspectrum::Setup']]],
-  ['spectralanalysis_2',['SpectralAnalysis',['../classdealspectrum_1_1SpectralAnalysis.html#a34a974fb314f0807d96902741be6e156',1,'dealspectrum::SpectralAnalysis']]],
-  ['start_3',['start',['../classdealspectrum_1_1DealSpectrumTimer.html#a140b943f9881e4a1d917941f896cb472',1,'dealspectrum::DealSpectrumTimer']]],
-  ['stop_4',['stop',['../classdealspectrum_1_1DealSpectrumTimer.html#ac7f6fa0118b49c8c58f1b8357cc2a371',1,'dealspectrum::DealSpectrumTimer']]]
+  ['setup_2',['setup',['../classExaDG_1_1InterfaceCoupling.html#a56a1527c3c5de8e9c3c96b3761c35a45',1,'ExaDG::InterfaceCoupling']]],
+  ['spectralanalysis_3',['SpectralAnalysis',['../classdealspectrum_1_1SpectralAnalysis.html#a34a974fb314f0807d96902741be6e156',1,'dealspectrum::SpectralAnalysis']]],
+  ['start_4',['start',['../classdealspectrum_1_1DealSpectrumTimer.html#a140b943f9881e4a1d917941f896cb472',1,'dealspectrum::DealSpectrumTimer']]],
+  ['stop_5',['stop',['../classdealspectrum_1_1DealSpectrumTimer.html#ac7f6fa0118b49c8c58f1b8357cc2a371',1,'dealspectrum::DealSpectrumTimer']]]
 ];
