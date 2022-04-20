497aaecb2998831ed3a55206472459fc825c56a5
nfehn <nfehn@users.noreply.github.com>
2022-04-20 11:06:32 +0000

    deploy: 8c391f5bb06edc9080b76b3d7cd78ce98d236e7f

--- search/classes_1.js
+++ search/classes_1.js
@@ -1,15 +1,16 @@
 var searchData=
 [
-  ['bdftimeintegratorconstants_0',['BDFTimeIntegratorConstants',['../classExaDG_1_1BDFTimeIntegratorConstants.html',1,'ExaDG']]],
-  ['blockjacobipreconditioner_1',['BlockJacobiPreconditioner',['../classExaDG_1_1BlockJacobiPreconditioner.html',1,'ExaDG']]],
-  ['blockpreconditioner_2',['BlockPreconditioner',['../classExaDG_1_1IncNS_1_1BlockPreconditioner.html',1,'ExaDG::IncNS']]],
-  ['blockpreconditioner_3c_20dim_2c_20double_20_3e_3',['BlockPreconditioner&lt; dim, double &gt;',['../classExaDG_1_1IncNS_1_1BlockPreconditioner.html',1,'ExaDG::IncNS']]],
-  ['bodyforcedata_4',['BodyForceData',['../structExaDG_1_1Structure_1_1BodyForceData.html',1,'ExaDG::Structure']]],
-  ['bodyforceoperator_5',['BodyForceOperator',['../classExaDG_1_1CompNS_1_1BodyForceOperator.html',1,'ExaDG::CompNS::BodyForceOperator&lt; dim, Number &gt;'],['../classExaDG_1_1Structure_1_1BodyForceOperator.html',1,'ExaDG::Structure::BodyForceOperator&lt; dim, Number &gt;']]],
-  ['bodyforceoperatordata_6',['BodyForceOperatorData',['../structExaDG_1_1CompNS_1_1BodyForceOperatorData.html',1,'ExaDG::CompNS']]],
-  ['boundarydescriptor_7',['BoundaryDescriptor',['../structExaDG_1_1CompNS_1_1BoundaryDescriptor.html',1,'ExaDG::CompNS::BoundaryDescriptor&lt; dim &gt;'],['../structExaDG_1_1ConvDiff_1_1BoundaryDescriptor.html',1,'ExaDG::ConvDiff::BoundaryDescriptor&lt; dim &gt;'],['../structExaDG_1_1IncNS_1_1BoundaryDescriptor.html',1,'ExaDG::IncNS::BoundaryDescriptor&lt; dim &gt;'],['../structExaDG_1_1Poisson_1_1BoundaryDescriptor.html',1,'ExaDG::Poisson::BoundaryDescriptor&lt; rank, dim &gt;'],['../structExaDG_1_1Structure_1_1BoundaryDescriptor.html',1,'ExaDG::Structure::BoundaryDescriptor&lt; dim &gt;']]],
-  ['boundarydescriptorenergy_8',['BoundaryDescriptorEnergy',['../structExaDG_1_1CompNS_1_1BoundaryDescriptorEnergy.html',1,'ExaDG::CompNS']]],
-  ['boundarydescriptorp_9',['BoundaryDescriptorP',['../structExaDG_1_1IncNS_1_1BoundaryDescriptorP.html',1,'ExaDG::IncNS']]],
-  ['boundarydescriptorstd_10',['BoundaryDescriptorStd',['../structExaDG_1_1CompNS_1_1BoundaryDescriptorStd.html',1,'ExaDG::CompNS']]],
-  ['boundarydescriptoru_11',['BoundaryDescriptorU',['../structExaDG_1_1IncNS_1_1BoundaryDescriptorU.html',1,'ExaDG::IncNS']]]
+  ['balancedgranularitypartitionpolicy_0',['BalancedGranularityPartitionPolicy',['../classExaDG_1_1BalancedGranularityPartitionPolicy.html',1,'ExaDG']]],
+  ['bdftimeintegratorconstants_1',['BDFTimeIntegratorConstants',['../classExaDG_1_1BDFTimeIntegratorConstants.html',1,'ExaDG']]],
+  ['blockjacobipreconditioner_2',['BlockJacobiPreconditioner',['../classExaDG_1_1BlockJacobiPreconditioner.html',1,'ExaDG']]],
+  ['blockpreconditioner_3',['BlockPreconditioner',['../classExaDG_1_1IncNS_1_1BlockPreconditioner.html',1,'ExaDG::IncNS']]],
+  ['blockpreconditioner_3c_20dim_2c_20double_20_3e_4',['BlockPreconditioner&lt; dim, double &gt;',['../classExaDG_1_1IncNS_1_1BlockPreconditioner.html',1,'ExaDG::IncNS']]],
+  ['bodyforcedata_5',['BodyForceData',['../structExaDG_1_1Structure_1_1BodyForceData.html',1,'ExaDG::Structure']]],
+  ['bodyforceoperator_6',['BodyForceOperator',['../classExaDG_1_1CompNS_1_1BodyForceOperator.html',1,'ExaDG::CompNS::BodyForceOperator&lt; dim, Number &gt;'],['../classExaDG_1_1Structure_1_1BodyForceOperator.html',1,'ExaDG::Structure::BodyForceOperator&lt; dim, Number &gt;']]],
+  ['bodyforceoperatordata_7',['BodyForceOperatorData',['../structExaDG_1_1CompNS_1_1BodyForceOperatorData.html',1,'ExaDG::CompNS']]],
+  ['boundarydescriptor_8',['BoundaryDescriptor',['../structExaDG_1_1CompNS_1_1BoundaryDescriptor.html',1,'ExaDG::CompNS::BoundaryDescriptor&lt; dim &gt;'],['../structExaDG_1_1ConvDiff_1_1BoundaryDescriptor.html',1,'ExaDG::ConvDiff::BoundaryDescriptor&lt; dim &gt;'],['../structExaDG_1_1IncNS_1_1BoundaryDescriptor.html',1,'ExaDG::IncNS::BoundaryDescriptor&lt; dim &gt;'],['../structExaDG_1_1Poisson_1_1BoundaryDescriptor.html',1,'ExaDG::Poisson::BoundaryDescriptor&lt; rank, dim &gt;'],['../structExaDG_1_1Structure_1_1BoundaryDescriptor.html',1,'ExaDG::Structure::BoundaryDescriptor&lt; dim &gt;']]],
+  ['boundarydescriptorenergy_9',['BoundaryDescriptorEnergy',['../structExaDG_1_1CompNS_1_1BoundaryDescriptorEnergy.html',1,'ExaDG::CompNS']]],
+  ['boundarydescriptorp_10',['BoundaryDescriptorP',['../structExaDG_1_1IncNS_1_1BoundaryDescriptorP.html',1,'ExaDG::IncNS']]],
+  ['boundarydescriptorstd_11',['BoundaryDescriptorStd',['../structExaDG_1_1CompNS_1_1BoundaryDescriptorStd.html',1,'ExaDG::CompNS']]],
+  ['boundarydescriptoru_12',['BoundaryDescriptorU',['../structExaDG_1_1IncNS_1_1BoundaryDescriptorU.html',1,'ExaDG::IncNS']]]
 ];
