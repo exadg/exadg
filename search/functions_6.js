9264ed1008256a7d09b5b2bb55a8ac761dd227c1
nfehn <nfehn@users.noreply.github.com>
2022-09-07 08:53:15 +0000

    deploy: 44dd234ffa25c0f5c0e108191d861e4de102717b

--- search/functions_6.js
+++ search/functions_6.js
@@ -7,10 +7,11 @@ var searchData=
   ['insert_4',['insert',['../classExaDG_1_1TimerTree.html#a30aeaec14ddf595ceb550f4c6ac1e77b',1,'ExaDG::TimerTree::insert(std::vector&lt; std::string &gt; const ids, double const wall_time)'],['../classExaDG_1_1TimerTree.html#ada7bf148cf17e0cbd1d314afa1f6e71e',1,'ExaDG::TimerTree::insert(std::vector&lt; std::string &gt; ids, std::shared_ptr&lt; TimerTree &gt; sub_tree, std::string const new_name=&quot;&quot;)']]],
   ['interpolate_5',['interpolate',['../classdealspectrum_1_1Interpolator.html#a240edeb61f6724893fa89d70de13ddcd',1,'dealspectrum::Interpolator::interpolate(double const *&amp;src)'],['../classdealspectrum_1_1Interpolator.html#a71c409b52995d93824f34b757300afee',1,'dealspectrum::Interpolator::interpolate()']]],
   ['interpolate_5fsimple_6',['interpolate_simple',['../classdealspectrum_1_1Interpolator.html#a94b7b341d1a9f088d256287137c77381',1,'dealspectrum::Interpolator::interpolate_simple()'],['../classdealspectrum_1_1Interpolator.html#aeed0b7a32c90e41dca49a50c826668d5',1,'dealspectrum::Interpolator::interpolate_simple(double *&amp;src)']]],
-  ['interpolator_7',['Interpolator',['../classdealspectrum_1_1Interpolator.html#a67b56e08cbd7dbfdd57fe1325cab6f3d',1,'dealspectrum::Interpolator']]],
-  ['io_8',['io',['../classdealspectrum_1_1Interpolator.html#af628c6a95bdf35330f9eea3e10c221e7',1,'dealspectrum::Interpolator']]],
-  ['ipermute_9',['ipermute',['../classdealspectrum_1_1Permutator.html#a5676ead692118f167b72d5eade208a2f',1,'dealspectrum::Permutator']]],
-  ['is_5fcoupling_5fongoing_10',['is_coupling_ongoing',['../classExaDG_1_1preCICE_1_1Adapter.html#acd75a0a0d95f49c6a44ecfff5e72497b',1,'ExaDG::preCICE::Adapter']]],
-  ['is_5ftime_5fwindow_5fcomplete_11',['is_time_window_complete',['../classExaDG_1_1preCICE_1_1Adapter.html#af2409b1817f17d6c7754664d3cd073c6',1,'ExaDG::preCICE::Adapter']]],
-  ['iwait_12',['iwait',['../classdealspectrum_1_1Permutator.html#a2d3b0db2ac65d628b201ea9475342685',1,'dealspectrum::Permutator']]]
+  ['interpolate_5fsolution_7',['interpolate_solution',['../classExaDG_1_1SolutionInterpolationBetweenTriangulations.html#ad9ee5dec6acfac11eba23d600acd51e2',1,'ExaDG::SolutionInterpolationBetweenTriangulations']]],
+  ['interpolator_8',['Interpolator',['../classdealspectrum_1_1Interpolator.html#a67b56e08cbd7dbfdd57fe1325cab6f3d',1,'dealspectrum::Interpolator']]],
+  ['io_9',['io',['../classdealspectrum_1_1Interpolator.html#af628c6a95bdf35330f9eea3e10c221e7',1,'dealspectrum::Interpolator']]],
+  ['ipermute_10',['ipermute',['../classdealspectrum_1_1Permutator.html#a5676ead692118f167b72d5eade208a2f',1,'dealspectrum::Permutator']]],
+  ['is_5fcoupling_5fongoing_11',['is_coupling_ongoing',['../classExaDG_1_1preCICE_1_1Adapter.html#acd75a0a0d95f49c6a44ecfff5e72497b',1,'ExaDG::preCICE::Adapter']]],
+  ['is_5ftime_5fwindow_5fcomplete_12',['is_time_window_complete',['../classExaDG_1_1preCICE_1_1Adapter.html#af2409b1817f17d6c7754664d3cd073c6',1,'ExaDG::preCICE::Adapter']]],
+  ['iwait_13',['iwait',['../classdealspectrum_1_1Permutator.html#a2d3b0db2ac65d628b201ea9475342685',1,'dealspectrum::Permutator']]]
 ];
