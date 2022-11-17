dff508961932025b117bbf324ed5b4d4285819bb
nfehn <nfehn@users.noreply.github.com>
2022-11-17 12:30:04 +0000

    deploy: 1a1c57a9bf7b355b71b4cff3ee10d76eedbe2bde

--- search/functions_a.js
+++ search/functions_a.js
@@ -3,8 +3,8 @@ var searchData=
   ['save_5fcurrent_5fstate_5fif_5frequired_0',['save_current_state_if_required',['../classExaDG_1_1preCICE_1_1Adapter.html#a7a4afd77dd457397b831862cce372cde',1,'ExaDG::preCICE::Adapter']]],
   ['serialize_1',['serialize',['../classdealspectrum_1_1Interpolator.html#a8283a193758ed7bc6fd68fd2f3cd9847',1,'dealspectrum::Interpolator::serialize(char const *filename)'],['../classdealspectrum_1_1Interpolator.html#a6b09f98b6b45095290fa3ea8269f0c16',1,'dealspectrum::Interpolator::serialize(char const *filename, double const *&amp;src)'],['../classdealspectrum_1_1SpectralAnalysis.html#a5823986be9b1e140e8a408cdcdd5b59c',1,'dealspectrum::SpectralAnalysis::serialize()']]],
   ['set_5fwrite_5fdata_5ftype_2',['set_write_data_type',['../classExaDG_1_1preCICE_1_1CouplingBase.html#aae21d6fc764dbd5051ec7c27cc38d11a',1,'ExaDG::preCICE::CouplingBase']]],
-  ['setup_3',['Setup',['../classdealspectrum_1_1Setup.html#ae48fcc6968bba36f4300ec7e88009717',1,'dealspectrum::Setup']]],
-  ['setup_4',['setup',['../classExaDG_1_1InterfaceCoupling.html#aff5ea829ee074454b6c2efad28d2daa2',1,'ExaDG::InterfaceCoupling']]],
+  ['setup_3',['setup',['../classExaDG_1_1InterfaceCoupling.html#aff5ea829ee074454b6c2efad28d2daa2',1,'ExaDG::InterfaceCoupling']]],
+  ['setup_4',['Setup',['../classdealspectrum_1_1Setup.html#ae48fcc6968bba36f4300ec7e88009717',1,'dealspectrum::Setup']]],
   ['spectralanalysis_5',['SpectralAnalysis',['../classdealspectrum_1_1SpectralAnalysis.html#a34a974fb314f0807d96902741be6e156',1,'dealspectrum::SpectralAnalysis']]],
   ['start_6',['start',['../classdealspectrum_1_1DealSpectrumTimer.html#a140b943f9881e4a1d917941f896cb472',1,'dealspectrum::DealSpectrumTimer']]],
   ['stop_7',['stop',['../classdealspectrum_1_1DealSpectrumTimer.html#ac7f6fa0118b49c8c58f1b8357cc2a371',1,'dealspectrum::DealSpectrumTimer']]]
