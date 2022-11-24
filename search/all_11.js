618f7e15fae79e99cbd2b9569da413716f46c74c
nfehn <nfehn@users.noreply.github.com>
2022-11-24 14:19:19 +0000

    deploy: 9d9fb8b2662bf4e46e8a56039d6aae131fbd48b8

--- search/all_11.js
+++ search/all_11.js
@@ -3,7 +3,7 @@ var searchData=
   ['readheader_0',['readHeader',['../classdealspectrum_1_1Setup.html#a398598d40bbb1da95cde226559de2fe9',1,'dealspectrum::Setup']]],
   ['rectanglemeshmovementfunctions_1',['RectangleMeshMovementFunctions',['../classExaDG_1_1RectangleMeshMovementFunctions.html',1,'ExaDG']]],
   ['refine_5flocal_2',['refine_local',['../namespaceExaDG.html#ac26ac75e95c9fbc45eaa00fa0c05a2b7',1,'ExaDG']]],
-  ['reinit_3',['reinit',['../classExaDG_1_1SolutionInterpolationBetweenTriangulations.html#a6e7d1f14bedd4b51c35525417a1feb20',1,'ExaDG::SolutionInterpolationBetweenTriangulations']]],
+  ['reinit_3',['reinit',['../classExaDG_1_1SolutionInterpolationBetweenTriangulations.html#a6e7d1f14bedd4b51c35525417a1feb20',1,'ExaDG::SolutionInterpolationBetweenTriangulations::reinit()'],['../classExaDG_1_1SolutionField.html#a11a769db2ab15d1be7a96acc1a2cd23b',1,'ExaDG::SolutionField::reinit()']]],
   ['reload_5fold_5fstate_5fif_5frequired_4',['reload_old_state_if_required',['../classExaDG_1_1preCICE_1_1Adapter.html#a0ce4e88f107c66747490f6747705f4d7',1,'ExaDG::preCICE::Adapter']]],
   ['residualoperator_5',['ResidualOperator',['../classExaDG_1_1Structure_1_1ResidualOperator.html',1,'ExaDG::Structure']]],
   ['resolutionparameters_6',['ResolutionParameters',['../structExaDG_1_1ResolutionParameters.html',1,'ExaDG']]],
--- search/functions_9.js
+++ search/functions_9.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['readheader_0',['readHeader',['../classdealspectrum_1_1Setup.html#a398598d40bbb1da95cde226559de2fe9',1,'dealspectrum::Setup']]],
   ['refine_5flocal_1',['refine_local',['../namespaceExaDG.html#ac26ac75e95c9fbc45eaa00fa0c05a2b7',1,'ExaDG']]],
-  ['reinit_2',['reinit',['../classExaDG_1_1SolutionInterpolationBetweenTriangulations.html#a6e7d1f14bedd4b51c35525417a1feb20',1,'ExaDG::SolutionInterpolationBetweenTriangulations']]],
+  ['reinit_2',['reinit',['../classExaDG_1_1SolutionField.html#a11a769db2ab15d1be7a96acc1a2cd23b',1,'ExaDG::SolutionField::reinit()'],['../classExaDG_1_1SolutionInterpolationBetweenTriangulations.html#a6e7d1f14bedd4b51c35525417a1feb20',1,'ExaDG::SolutionInterpolationBetweenTriangulations::reinit()']]],
   ['reload_5fold_5fstate_5fif_5frequired_3',['reload_old_state_if_required',['../classExaDG_1_1preCICE_1_1Adapter.html#a0ce4e88f107c66747490f6747705f4d7',1,'ExaDG::preCICE::Adapter']]]
 ];
