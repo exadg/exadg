b79bfc7d4c3a009386e09bef9a7f4cad96ef59d8
Niklas Fehn <fehn@lnm.mw.tum.de>
2022-11-25 09:25:23 +0100

    shift ContainerInterfaceData to separate file

--- include/exadg/functions_and_boundary_conditions/function_cached.cpp
+++ include/exadg/functions_and_boundary_conditions/function_cached.cpp
@@ -24,54 +24,6 @@
 
 namespace ExaDG
 {
-template<int rank, int dim, typename number_type>
-ContainerInterfaceData<rank, dim, number_type>::ContainerInterfaceData()
-{
-}
-
-template<int rank, int dim, typename number_type>
-typename ContainerInterfaceData<rank, dim, number_type>::data_type
-ContainerInterfaceData<rank, dim, number_type>::get_data(unsigned int const q_index,
-                                                         unsigned int const face,
-                                                         unsigned int const q,
-                                                         unsigned int const v) const
-{
-  Assert(map_vector_index.find(q_index) != map_vector_index.end(),
-         dealii::ExcMessage("Specified q_index does not exist in map_vector_index."));
-
-  Assert(map_solution.find(q_index) != map_solution.end(),
-         dealii::ExcMessage("Specified q_index does not exist in map_solution."));
-
-  Id                              id    = std::make_tuple(face, q, v);
-  dealii::types::global_dof_index index = map_vector_index.find(q_index)->second.find(id)->second;
-
-  ArraySolutionValues const & array_solution = map_solution.find(q_index)->second;
-  Assert(index < array_solution.size(), dealii::ExcMessage("Index exceeds dimensions of vector."));
-
-  return array_solution[index];
-}
-
-template<int rank, int dim, typename number_type>
-std::vector<typename ContainerInterfaceData<rank, dim, number_type>::quad_index> const &
-ContainerInterfaceData<rank, dim, number_type>::get_quad_indices()
-{
-  return quad_indices;
-}
-
-template<int rank, int dim, typename number_type>
-typename ContainerInterfaceData<rank, dim, number_type>::ArrayQuadraturePoints &
-ContainerInterfaceData<rank, dim, number_type>::get_array_q_points(quad_index const & q_index)
-{
-  return map_q_points[q_index];
-}
-
-template<int rank, int dim, typename number_type>
-typename ContainerInterfaceData<rank, dim, number_type>::ArraySolutionValues &
-ContainerInterfaceData<rank, dim, number_type>::get_array_solution(quad_index const & q_index)
-{
-  return map_solution[q_index];
-}
-
 template<int rank, int dim>
 FunctionCached<rank, dim>::FunctionCached()
 {
@@ -84,11 +36,6 @@ FunctionCached<rank, dim>::set_data_pointer(
 {
   interface_data = interface_data_;
 }
-
-template class ContainerInterfaceData<0, 2, double>;
-template class ContainerInterfaceData<1, 2, double>;
-template class ContainerInterfaceData<0, 3, double>;
-template class ContainerInterfaceData<1, 3, double>;
 
 template class FunctionCached<0, 2>;
 template class FunctionCached<0, 3>;
