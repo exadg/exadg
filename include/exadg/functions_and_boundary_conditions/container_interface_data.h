b71d0954ec3d04c4b6def926d1ab907bcf9c1029
Niklas Fehn <fehn@lnm.mw.tum.de>
2022-11-28 11:24:45 +0100

    first draft for volume coupling and the initialization of q-point arrays

--- include/exadg/functions_and_boundary_conditions/container_interface_data.h
+++ include/exadg/functions_and_boundary_conditions/container_interface_data.h
@@ -31,6 +31,7 @@
 
 // ExaDG
 #include <exadg/matrix_free/integrators.h>
+#include <exadg/operators/variable_coefficients.h>
 #include <exadg/utilities/n_components_to_rank.h>
 
 namespace ExaDG
@@ -196,7 +197,229 @@ private:
 template<int rank, int dim, typename number_type>
 class CouplingDataVolume : public CouplingDataBase<rank, dim, number_type>
 {
-  // TODO: fill this class
+public:
+  using data_type = typename CouplingDataBase<rank, dim, number_type>::data_type;
+
+private:
+  using quad_index = typename CouplingDataBase<rank, dim, number_type>::quad_index;
+
+  using ArrayQuadraturePoints =
+    typename CouplingDataBase<rank, dim, number_type>::ArrayQuadraturePoints;
+
+  using ArraySolutionValues =
+    typename CouplingDataBase<rank, dim, number_type>::ArraySolutionValues;
+
+public:
+  template<typename Number>
+  void
+  setup(std::shared_ptr<dealii::MatrixFree<dim, Number>> matrix_free_,
+        unsigned int const                               dof_index_,
+        std::vector<quad_index> const &                  quad_indices_,
+        bool const                                       is_dg)
+  {
+    // TODO: the parameter is_dg should be deduced from matrix_free_ and dof_index_ if possible
+
+    this->quad_indices = quad_indices_;
+
+    n_cells = matrix_free_->n_cell_batches();
+
+    if(is_dg)
+    {
+      n_inner_faces    = matrix_free_->n_inner_face_batches();
+      n_boundary_faces = matrix_free_->n_boundary_face_batches();
+    }
+    else
+    {
+      n_inner_faces    = 0;
+      n_boundary_faces = 0;
+    }
+
+    for(auto q_index : this->quad_indices)
+    {
+      // initialize maps
+      this->map_q_points.emplace(q_index, ArrayQuadraturePoints());
+      this->map_solution.emplace(q_index, ArraySolutionValues());
+      this->map_solution_vectorized.emplace(q_index, VariableCoefficients<dim, number_type>());
+
+      ArrayQuadraturePoints & array_q_points_dst = this->map_q_points.find(q_index)->second;
+      ArraySolutionValues &   array_solution_dst = this->map_solution.find(q_index)->second;
+
+      array_q_points_dst.resize(get_array_size());
+
+      // fill array of quadrature points by looping over all cells, inner faces, and boundary faces
+      // cells
+      for(unsigned int cell = 0; cell < matrix_free_->n_cell_batches(); ++cell)
+      {
+        FaceIntegrator<dim, this->n_components, Number> integrator(*matrix_free_,
+                                                                   dof_index_,
+                                                                   q_index);
+
+        integrator.reinit(cell);
+
+        for(unsigned int q = 0; q < integrator.n_q_points; ++q)
+        {
+          dealii::Point<dim, dealii::VectorizedArray<Number>> q_points =
+            integrator.quadrature_point(q);
+
+          for(unsigned int v = 0; v < dealii::VectorizedArray<Number>::size(); ++v)
+          {
+            dealii::Point<dim> q_point;
+            for(unsigned int d = 0; d < dim; ++d)
+              q_point[d] = q_points[d][v];
+
+            array_q_points_dst[get_index_cell(cell, q, v)] = q_point;
+          }
+        }
+      }
+
+      if(is_dg)
+      {
+        // interior faces
+        for(unsigned int face = 0; face < matrix_free_->n_inner_face_batches(); ++face)
+        {
+          FaceIntegrator<dim, this->n_components, Number> integrator_m(*matrix_free_,
+                                                                       true,
+                                                                       dof_index_,
+                                                                       q_index);
+
+          integrator_m.reinit(face);
+
+          for(unsigned int q = 0; q < integrator_m.n_q_points; ++q)
+          {
+            dealii::Point<dim, dealii::VectorizedArray<Number>> q_points =
+              integrator_m.quadrature_point(q);
+
+            for(unsigned int v = 0; v < dealii::VectorizedArray<Number>::size(); ++v)
+            {
+              dealii::Point<dim> q_point;
+              for(unsigned int d = 0; d < dim; ++d)
+                q_point[d] = q_points[d][v];
+
+              array_q_points_dst[get_index_inner_face(face, q, v)] = q_point;
+            }
+          }
+
+          FaceIntegrator<dim, this->n_components, Number> integrator_p(*matrix_free_,
+                                                                       false,
+                                                                       dof_index_,
+                                                                       q_index);
+
+          integrator_p.reinit(face);
+
+          for(unsigned int q = 0; q < integrator_p.n_q_points; ++q)
+          {
+            dealii::Point<dim, dealii::VectorizedArray<Number>> q_points =
+              integrator_p.quadrature_point(q);
+
+            for(unsigned int v = 0; v < dealii::VectorizedArray<Number>::size(); ++v)
+            {
+              dealii::Point<dim> q_point;
+              for(unsigned int d = 0; d < dim; ++d)
+                q_point[d] = q_points[d][v];
+
+              array_q_points_dst[get_index_inner_face_neighbor(face, q, v)] = q_point;
+            }
+          }
+        }
+
+        // boundary faces
+        for(unsigned int face = matrix_free_->n_inner_face_batches();
+            face < matrix_free_->n_inner_face_batches() + matrix_free_->n_boundary_face_batches();
+            ++face)
+        {
+          FaceIntegrator<dim, this->n_components, Number> integrator(*matrix_free_,
+                                                                     true,
+                                                                     dof_index_,
+                                                                     q_index);
+          integrator.reinit(face);
+
+          for(unsigned int q = 0; q < integrator.n_q_points; ++q)
+          {
+            dealii::Point<dim, dealii::VectorizedArray<Number>> q_points =
+              integrator.quadrature_point(q);
+
+            for(unsigned int v = 0; v < dealii::VectorizedArray<Number>::size(); ++v)
+            {
+              dealii::Point<dim> q_point;
+              for(unsigned int d = 0; d < dim; ++d)
+                q_point[d] = q_points[d][v];
+
+              array_q_points_dst[get_index_boundary_face(face, q, v)] = q_point;
+            }
+          }
+        }
+      }
+
+      array_solution_dst.resize(array_q_points_dst.size(), data_type());
+    }
+  }
+
+  /*
+   * This function copies the data from the array of solution values to the VariableCoefficients
+   * data structure
+   */
+  void
+  update_solution_vectorized()
+  {
+    // TODO
+  }
+
+private:
+  inline dealii::types::global_dof_index
+  get_index_cell(unsigned int const cell, unsigned int const q, unsigned int const v) const
+  {
+    return (cell * n_q_points_per_cell + q) * n_vectorized_array_entries + v;
+  }
+
+  inline dealii::types::global_dof_index
+  get_index_inner_face(unsigned int const face, unsigned int const q, unsigned int const v) const
+  {
+    return n_cells * n_q_points_per_cell * n_vectorized_array_entries +
+           (face * n_q_points_per_face + q) * n_vectorized_array_entries + v;
+  }
+
+  inline dealii::types::global_dof_index
+  get_index_inner_face_neighbor(unsigned int const face,
+                                unsigned int const q,
+                                unsigned int const v) const
+  {
+    return (n_cells * n_q_points_per_cell + n_inner_faces * n_q_points_per_face) *
+             n_vectorized_array_entries +
+           (face * n_q_points_per_face + q) * n_vectorized_array_entries + v;
+  }
+
+  inline dealii::types::global_dof_index
+  get_index_boundary_face(unsigned int const face, unsigned int const q, unsigned int const v) const
+  {
+    return (n_cells * n_q_points_per_cell + 2 * n_inner_faces * n_q_points_per_face) *
+             n_vectorized_array_entries +
+           (face * n_q_points_per_face + q) * n_vectorized_array_entries + v;
+  }
+
+  inline dealii::types::global_dof_index
+  get_array_size() const
+  {
+    return (n_cells * n_q_points_per_cell +
+            (2 * n_inner_faces + n_boundary_faces) * n_q_points_per_face) *
+           n_vectorized_array_entries;
+  }
+
+  // TODO: does not work like this since we have different quadrature rules with different number of
+  // points
+  unsigned int n_q_points_per_cell;
+  unsigned int n_q_points_per_face;
+
+  unsigned int n_vectorized_array_entries;
+
+  unsigned int n_cells;
+  unsigned int n_inner_faces;
+  unsigned int n_boundary_faces;
+
+  /*
+   * Stores the data in the layout required by matrix-free loops, i.e. in the form
+   * VectorizedArray<number_type> and not as plain arrays
+   */
+  std::map<quad_index, VariableCoefficients<dim, number_type>> map_solution_vectorized;
 };
 } // namespace ExaDG
 
