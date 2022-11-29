6887fc598c603285f066c90123f9642a5e32f88e
nfehn <nfehn@users.noreply.github.com>
2022-11-29 13:21:55 +0000

    deploy: 62a3addc0c383e4dc9188e9589b61789b9226dbe

--- search/variables_1.js
+++ search/variables_1.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['mapping_0',['mapping',['../classExaDG_1_1Grid.html#a4d54c3a7e8c952ba484c9635cdc8d48d',1,'ExaDG::Grid']]],
   ['matrix_5ffree_1',['matrix_free',['../classExaDG_1_1preCICE_1_1CouplingBase.html#a9023ffde51ff77d11e2d065c415e4003',1,'ExaDG::preCICE::CouplingBase']]],
-  ['mesh_5fname_2',['mesh_name',['../classExaDG_1_1preCICE_1_1CouplingBase.html#a36e684b3ced1553f0017f75f42737fdb',1,'ExaDG::preCICE::CouplingBase']]]
+  ['mesh_5fname_2',['mesh_name',['../classExaDG_1_1preCICE_1_1CouplingBase.html#a36e684b3ced1553f0017f75f42737fdb',1,'ExaDG::preCICE::CouplingBase']]],
+  ['mesh_5fsmoothing_3',['mesh_smoothing',['../classExaDG_1_1Grid.html#aaa8f445c88805fc4e5cc3d9984b9e18d',1,'ExaDG::Grid']]]
 ];
