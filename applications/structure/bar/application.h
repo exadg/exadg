30d816f763f16c81cb4bfdcc7b72b1649de4e828
Niklas Fehn <fehn@lnm.mw.tum.de>
2022-12-23 16:38:06 +0100

    adjust test case

--- applications/structure/bar/application.h
+++ applications/structure/bar/application.h
@@ -209,7 +209,7 @@ private:
   void
   set_parameters() final
   {
-    this->param.problem_type         = ProblemType::QuasiStatic; //Steady;
+    this->param.problem_type         = ProblemType::QuasiStatic;
     this->param.body_force           = use_volume_force;
     this->param.large_deformation    = true;
     this->param.pull_back_body_force = false;
@@ -227,11 +227,11 @@ private:
     this->param.grid.triangulation_type = TriangulationType::Distributed;
     this->param.grid.mapping_degree     = 1;
 
-    this->param.load_increment            = 0.025;
+    this->param.load_increment            = 0.1;
     this->param.adjust_load_increment     = false;
     this->param.desired_newton_iterations = 20;
 
-    this->param.newton_solver_data                   = Newton::SolverData(1e4, 1.e-10, 1.e-10);
+    this->param.newton_solver_data                   = Newton::SolverData(1e2, 1.e-8, 1.e-8);
     this->param.solver                               = Solver::FGMRES;
     this->param.solver_data                          = SolverData(1e4, 1.e-12, 1.e-6, 100);
     this->param.preconditioner                       = Preconditioner::Multigrid;
@@ -463,7 +463,7 @@ private:
     pp_data.output_data.time_control_data.trigger_interval = (end_time - start_time) / 20.0;
     pp_data.output_data.directory          = this->output_parameters.directory + "vtu/";
     pp_data.output_data.filename           = this->output_parameters.filename;
-    pp_data.output_data.write_higher_order = false;
+    pp_data.output_data.write_higher_order = true;
     pp_data.output_data.degree             = this->param.degree;
 
     pp_data.error_data.time_control_data.is_active = true;
