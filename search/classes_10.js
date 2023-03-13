1b5174ef6a6095924183571afa904ea851929929
nfehn <nfehn@users.noreply.github.com>
2023-03-13 09:31:55 +0000

    deploy: 5fdc4399e9feef2e080f4cc2f598caed341aad11

--- search/classes_10.js
+++ search/classes_10.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['qcriterioncalculator_0',['QCriterionCalculator',['../classExaDG_1_1IncNS_1_1QCriterionCalculator.html',1,'ExaDG::IncNS']]],
-  ['qcriterioncalculator_3c_20dim_2c_20double_20_3e_1',['QCriterionCalculator&lt; dim, double &gt;',['../classExaDG_1_1IncNS_1_1QCriterionCalculator.html',1,'ExaDG::IncNS']]],
+  ['qcriterioncalculator_0',['QCriterionCalculator',['../classExaDG_1_1QCriterionCalculator.html',1,'ExaDG']]],
+  ['qcriterioncalculator_3c_20dim_2c_20double_20_3e_1',['QCriterionCalculator&lt; dim, double &gt;',['../classExaDG_1_1QCriterionCalculator.html',1,'ExaDG']]],
   ['quadcoupling_2',['QuadCoupling',['../classExaDG_1_1preCICE_1_1QuadCoupling.html',1,'ExaDG::preCICE']]],
   ['quantity_3',['Quantity',['../structExaDG_1_1IncNS_1_1Quantity.html',1,'ExaDG::IncNS']]],
   ['quantitypressurecoefficient_4',['QuantityPressureCoefficient',['../structExaDG_1_1IncNS_1_1QuantityPressureCoefficient.html',1,'ExaDG::IncNS']]],
