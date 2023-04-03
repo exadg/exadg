6221c5c7821ac5c38e5c76b4b1c2e99bf770fe1a
peterrum <peterrum@users.noreply.github.com>
2023-04-03 16:54:09 +0000

    deploy: 9f7a5e9bef38ca8e61f1d276d7c27836ceb23ec8

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,7 +1,9 @@
 var searchData=
 [
   ['calculate_5fenergy_5fspectrum_0',['calculate_energy_spectrum',['../classdealspectrum_1_1SpectralAnalysis.html#a192f1677ecc3281dfe9f19628d06b14f',1,'dealspectrum::SpectralAnalysis']]],
-  ['clear_1',['clear',['../classExaDG_1_1TimerTree.html#a2886f1d3bbd4e82cb54458d94fe82979',1,'ExaDG::TimerTree']]],
-  ['copy_5fto_5fmg_2',['copy_to_mg',['../classExaDG_1_1MGTransferH.html#a9c0e0187ca29cbeff492774a99979ddf',1,'ExaDG::MGTransferH']]],
-  ['create_5fdirectories_3',['create_directories',['../namespaceExaDG.html#ae37213007b3d71515b9d35a598e17e22',1,'ExaDG']]]
+  ['calculate_5fgradient_5fflux_1',['calculate_gradient_flux',['../classExaDG_1_1IncNS_1_1Operators_1_1ViscousKernel.html#a231e82673d1fd134748a4605aed289dd',1,'ExaDG::IncNS::Operators::ViscousKernel']]],
+  ['calculate_5fvalue_5fflux_2',['calculate_value_flux',['../classExaDG_1_1IncNS_1_1Operators_1_1ViscousKernel.html#a068b47893755fa37fe567bf3c01c021a',1,'ExaDG::IncNS::Operators::ViscousKernel']]],
+  ['clear_3',['clear',['../classExaDG_1_1TimerTree.html#a2886f1d3bbd4e82cb54458d94fe82979',1,'ExaDG::TimerTree']]],
+  ['copy_5fto_5fmg_4',['copy_to_mg',['../classExaDG_1_1MGTransferH.html#a9c0e0187ca29cbeff492774a99979ddf',1,'ExaDG::MGTransferH']]],
+  ['create_5fdirectories_5',['create_directories',['../namespaceExaDG.html#ae37213007b3d71515b9d35a598e17e22',1,'ExaDG']]]
 ];
