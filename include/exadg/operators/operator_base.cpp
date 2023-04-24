0c557131787236a70f5fca0960f9cb9f09a1eefa
fazil necioglu <fazilnecioglu@gmail.com>
2023-04-24 16:11:05 +0200

    indent

--- include/exadg/operators/operator_base.cpp
+++ include/exadg/operators/operator_base.cpp
@@ -561,8 +561,7 @@ OperatorBase<dim, Number, n_components>::initialize_block_diagonal_preconditione
   {
     typedef Elementwise::PreconditionerIdentity<dealii::VectorizedArray<Number>> IDENTITY;
 
-    elementwise_preconditioner =
-      std::make_shared<IDENTITY>(integrator->dofs_per_cell);
+    elementwise_preconditioner = std::make_shared<IDENTITY>(integrator->dofs_per_cell);
   }
   else if(data.preconditioner_block_diagonal == Elementwise::Preconditioner::InverseMassMatrix)
   {
