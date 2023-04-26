726ac85d27ea9269b8d493bc281e53d002e9e816
nfehn <nfehn@users.noreply.github.com>
2023-04-26 13:43:56 +0000

    deploy: 0cf62751f9b1aed465be59855132988f179b4b0a

--- search/functions_5.js
+++ search/functions_5.js
@@ -1,14 +1,18 @@
 var searchData=
 [
-  ['get_5fdynamic_5fmapping_0',['get_dynamic_mapping',['../namespaceExaDG.html#a7ece5f60d30685552c07158c85d74f2d',1,'ExaDG']]],
-  ['get_5fequidistant_5finner_1',['get_equidistant_inner',['../classdealspectrum_1_1Interpolator.html#a1d9b8f5517c976d12a6b3fb499e21f73',1,'dealspectrum::Interpolator']]],
-  ['get_5fmapping_2',['get_mapping',['../classExaDG_1_1GridMotionBase.html#a7868b20c56ef4b299e9cda7883407420',1,'ExaDG::GridMotionBase::get_mapping()'],['../classExaDG_1_1GridMotionInterface.html#a67fa50d1ffc075e1d5e610aa30778f7f',1,'ExaDG::GridMotionInterface::get_mapping()']]],
-  ['get_5fmax_5flevel_3',['get_max_level',['../classExaDG_1_1TimerTree.html#a84ea0da9640ad1883cf374123959c285',1,'ExaDG::TimerTree']]],
-  ['get_5fresults_4',['get_results',['../classdealspectrum_1_1SpectralAnalysis.html#a83c895003af56a97b5004e8a429527d3',1,'dealspectrum::SpectralAnalysis']]],
-  ['getlocalrange_5',['getLocalRange',['../classdealspectrum_1_1SpectralAnalysis.html#ad521541cf55622d07d58e83ddb360ed7',1,'dealspectrum::SpectralAnalysis']]],
-  ['grid_6',['Grid',['../classExaDG_1_1Grid.html#ad5140eefba0bf93ceaa72282a89225c1',1,'ExaDG::Grid']]],
-  ['gridmotionbase_7',['GridMotionBase',['../classExaDG_1_1GridMotionBase.html#acda8e7ed786a239bd0ccd3df1c41e649',1,'ExaDG::GridMotionBase']]],
-  ['gridmotionelasticity_8',['GridMotionElasticity',['../classExaDG_1_1GridMotionElasticity.html#a3046ce1c701713328fd7d08ccce7d4df',1,'ExaDG::GridMotionElasticity']]],
-  ['gridmotionfunction_9',['GridMotionFunction',['../classExaDG_1_1GridMotionFunction.html#ae792ee6f20e878e261d1a92337c7da1c',1,'ExaDG::GridMotionFunction']]],
-  ['gridmotionpoisson_10',['GridMotionPoisson',['../classExaDG_1_1GridMotionPoisson.html#a00b8f321182c24d9b03d2c82f68f1866',1,'ExaDG::GridMotionPoisson']]]
+  ['get_5fcoefficient_5fcell_0',['get_coefficient_cell',['../classExaDG_1_1VariableCoefficients.html#a46f7dd2e11958b25e6612a0c6fac9f1f',1,'ExaDG::VariableCoefficients']]],
+  ['get_5fcoefficient_5fface_1',['get_coefficient_face',['../classExaDG_1_1VariableCoefficients.html#af9011c9ad2a8f8548abc3145c87adf16',1,'ExaDG::VariableCoefficients']]],
+  ['get_5fcoefficient_5fface_5fcell_5fbased_2',['get_coefficient_face_cell_based',['../classExaDG_1_1VariableCoefficients.html#acdccaa1620ad0d01ad6dfb0399b2a845',1,'ExaDG::VariableCoefficients']]],
+  ['get_5fcoefficient_5fface_5fneighbor_3',['get_coefficient_face_neighbor',['../classExaDG_1_1VariableCoefficients.html#a678d10eeced6f4da25c685c1f74d4dc1',1,'ExaDG::VariableCoefficients']]],
+  ['get_5fdynamic_5fmapping_4',['get_dynamic_mapping',['../namespaceExaDG.html#a7ece5f60d30685552c07158c85d74f2d',1,'ExaDG']]],
+  ['get_5fequidistant_5finner_5',['get_equidistant_inner',['../classdealspectrum_1_1Interpolator.html#a1d9b8f5517c976d12a6b3fb499e21f73',1,'dealspectrum::Interpolator']]],
+  ['get_5fmapping_6',['get_mapping',['../classExaDG_1_1GridMotionBase.html#a7868b20c56ef4b299e9cda7883407420',1,'ExaDG::GridMotionBase::get_mapping()'],['../classExaDG_1_1GridMotionInterface.html#a67fa50d1ffc075e1d5e610aa30778f7f',1,'ExaDG::GridMotionInterface::get_mapping()']]],
+  ['get_5fmax_5flevel_7',['get_max_level',['../classExaDG_1_1TimerTree.html#a84ea0da9640ad1883cf374123959c285',1,'ExaDG::TimerTree']]],
+  ['get_5fresults_8',['get_results',['../classdealspectrum_1_1SpectralAnalysis.html#a83c895003af56a97b5004e8a429527d3',1,'dealspectrum::SpectralAnalysis']]],
+  ['getlocalrange_9',['getLocalRange',['../classdealspectrum_1_1SpectralAnalysis.html#ad521541cf55622d07d58e83ddb360ed7',1,'dealspectrum::SpectralAnalysis']]],
+  ['grid_10',['Grid',['../classExaDG_1_1Grid.html#ad5140eefba0bf93ceaa72282a89225c1',1,'ExaDG::Grid']]],
+  ['gridmotionbase_11',['GridMotionBase',['../classExaDG_1_1GridMotionBase.html#acda8e7ed786a239bd0ccd3df1c41e649',1,'ExaDG::GridMotionBase']]],
+  ['gridmotionelasticity_12',['GridMotionElasticity',['../classExaDG_1_1GridMotionElasticity.html#a3046ce1c701713328fd7d08ccce7d4df',1,'ExaDG::GridMotionElasticity']]],
+  ['gridmotionfunction_13',['GridMotionFunction',['../classExaDG_1_1GridMotionFunction.html#ae792ee6f20e878e261d1a92337c7da1c',1,'ExaDG::GridMotionFunction']]],
+  ['gridmotionpoisson_14',['GridMotionPoisson',['../classExaDG_1_1GridMotionPoisson.html#a00b8f321182c24d9b03d2c82f68f1866',1,'ExaDG::GridMotionPoisson']]]
 ];
--- search/functions_a.js
+++ search/functions_a.js
@@ -3,10 +3,15 @@ var searchData=
   ['save_5fcurrent_5fstate_5fif_5frequired_0',['save_current_state_if_required',['../classExaDG_1_1preCICE_1_1Adapter.html#a7a4afd77dd457397b831862cce372cde',1,'ExaDG::preCICE::Adapter']]],
   ['serialize_1',['serialize',['../classdealspectrum_1_1Interpolator.html#a8283a193758ed7bc6fd68fd2f3cd9847',1,'dealspectrum::Interpolator::serialize(char const *filename)'],['../classdealspectrum_1_1Interpolator.html#a6b09f98b6b45095290fa3ea8269f0c16',1,'dealspectrum::Interpolator::serialize(char const *filename, double const *&amp;src)'],['../classdealspectrum_1_1SpectralAnalysis.html#a5823986be9b1e140e8a408cdcdd5b59c',1,'dealspectrum::SpectralAnalysis::serialize()']]],
   ['serialized_5fstring_2',['serialized_string',['../namespaceExaDG_1_1Utilities.html#a37f982dd84fb544d186dde3dda54073b',1,'ExaDG::Utilities']]],
-  ['set_5fwrite_5fdata_5ftype_3',['set_write_data_type',['../classExaDG_1_1preCICE_1_1CouplingBase.html#aae21d6fc764dbd5051ec7c27cc38d11a',1,'ExaDG::preCICE::CouplingBase']]],
-  ['setup_4',['Setup',['../classdealspectrum_1_1Setup.html#ae48fcc6968bba36f4300ec7e88009717',1,'dealspectrum::Setup']]],
-  ['setup_5',['setup',['../classExaDG_1_1InterfaceCoupling.html#a7aa3cfb167de7d9384a7686e7e1f964e',1,'ExaDG::InterfaceCoupling']]],
-  ['spectralanalysis_6',['SpectralAnalysis',['../classdealspectrum_1_1SpectralAnalysis.html#a34a974fb314f0807d96902741be6e156',1,'dealspectrum::SpectralAnalysis']]],
-  ['start_7',['start',['../classdealspectrum_1_1DealSpectrumTimer.html#a140b943f9881e4a1d917941f896cb472',1,'dealspectrum::DealSpectrumTimer']]],
-  ['stop_8',['stop',['../classdealspectrum_1_1DealSpectrumTimer.html#ac7f6fa0118b49c8c58f1b8357cc2a371',1,'dealspectrum::DealSpectrumTimer']]]
+  ['set_5fcoefficient_5fcell_3',['set_coefficient_cell',['../classExaDG_1_1VariableCoefficients.html#aac75b341696a34a88505a352269f5ed6',1,'ExaDG::VariableCoefficients']]],
+  ['set_5fcoefficient_5fface_4',['set_coefficient_face',['../classExaDG_1_1VariableCoefficients.html#ab5f2f622fa96ed41c6ffecd248ef0ac3',1,'ExaDG::VariableCoefficients']]],
+  ['set_5fcoefficient_5fface_5fcell_5fbased_5',['set_coefficient_face_cell_based',['../classExaDG_1_1VariableCoefficients.html#ae5b82fe6876340c749177f1558fc70ee',1,'ExaDG::VariableCoefficients']]],
+  ['set_5fcoefficient_5fface_5fneighbor_6',['set_coefficient_face_neighbor',['../classExaDG_1_1VariableCoefficients.html#aef6d49252b3b89cc167f576cd2171019',1,'ExaDG::VariableCoefficients']]],
+  ['set_5fcoefficients_7',['set_coefficients',['../classExaDG_1_1VariableCoefficients.html#ac2463a7113784b16e28c72fa8a091e4d',1,'ExaDG::VariableCoefficients']]],
+  ['set_5fwrite_5fdata_5ftype_8',['set_write_data_type',['../classExaDG_1_1preCICE_1_1CouplingBase.html#aae21d6fc764dbd5051ec7c27cc38d11a',1,'ExaDG::preCICE::CouplingBase']]],
+  ['setup_9',['setup',['../classExaDG_1_1InterfaceCoupling.html#a7aa3cfb167de7d9384a7686e7e1f964e',1,'ExaDG::InterfaceCoupling']]],
+  ['setup_10',['Setup',['../classdealspectrum_1_1Setup.html#ae48fcc6968bba36f4300ec7e88009717',1,'dealspectrum::Setup']]],
+  ['spectralanalysis_11',['SpectralAnalysis',['../classdealspectrum_1_1SpectralAnalysis.html#a34a974fb314f0807d96902741be6e156',1,'dealspectrum::SpectralAnalysis']]],
+  ['start_12',['start',['../classdealspectrum_1_1DealSpectrumTimer.html#a140b943f9881e4a1d917941f896cb472',1,'dealspectrum::DealSpectrumTimer']]],
+  ['stop_13',['stop',['../classdealspectrum_1_1DealSpectrumTimer.html#ac7f6fa0118b49c8c58f1b8357cc2a371',1,'dealspectrum::DealSpectrumTimer']]]
 ];
