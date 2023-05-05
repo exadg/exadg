acc2e054469034844943fa4578ce1e76928f2904
fazil necioglu <fazilnecioglu@gmail.com>
2023-05-05 12:33:40 +0200

    move asserts deeper in code

--- include/exadg/incompressible_navier_stokes/spatial_discretization/operator_dual_splitting.cpp
+++ include/exadg/incompressible_navier_stokes/spatial_discretization/operator_dual_splitting.cpp
@@ -801,7 +801,10 @@ OperatorDualSplitting<dim, Number>::local_interpolate_velocity_dirichlet_bc_boun
   VectorType const &,
   Range const & face_range) const
 {
-  unsigned int const dof_index  = this->get_dof_index_velocity();
+  unsigned int const dof_index = this->get_dof_index_velocity();
+  AssertThrow(
+    matrix_free.get_dof_handler(dof_index).get_triangulation().all_reference_cells_are_hyper_cube(),
+    dealii::ExcMessage("This function is not supported for simplices."));
   unsigned int const quad_index = this->get_quad_index_velocity_gauss_lobatto();
 
   FaceIntegratorU integrator(matrix_free, true, dof_index, quad_index);
