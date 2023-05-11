c085446389787846aae542100957bfa40dfc0281
nfehn <nfehn@users.noreply.github.com>
2023-05-11 09:29:22 +0000

    deploy: 91de37fd912ec7602f08fad051bdea1a1e0d5ce3

--- search/all_9.js
+++ search/all_9.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['jacobipreconditioner_0',['JacobiPreconditioner',['../classExaDG_1_1JacobiPreconditioner.html',1,'ExaDG']]],
+  ['jacobipreconditioner_0',['JacobiPreconditioner',['../classExaDG_1_1Elementwise_1_1JacobiPreconditioner.html',1,'ExaDG::Elementwise::JacobiPreconditioner&lt; dim, n_components, Number, Operator &gt;'],['../classExaDG_1_1JacobiPreconditioner.html',1,'ExaDG::JacobiPreconditioner&lt; Operator &gt;']]],
   ['jacobismoother_1',['JacobiSmoother',['../classExaDG_1_1JacobiSmoother.html',1,'ExaDG']]]
 ];
