271737acc8b2d3a8e504a0646e75f7cb8f66d94e
Niklas Fehn <fehnnikl@ibiza.math.uni-augsburg.de>
2023-05-12 08:42:00 +0200

    introduce a utility function for subdivided hyper rectangle (simplex vs. hypercube)

---
+++ include/exadg/grid/hyper_rectangle.h
@@ -0,0 +1,65 @@
+/*  ______________________________________________________________________
+ *
+ *  ExaDG - High-Order Discontinuous Galerkin for the Exa-Scale
+ *
+ *  Copyright (C) 2023 by the ExaDG authors
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  ______________________________________________________________________
+ */
+
+#ifndef INCLUDE_EXADG_GRID_HYPER_RECTANGLE_H_
+#define INCLUDE_EXADG_GRID_HYPER_RECTANGLE_H_
+
+// deal.II
+#include <deal.II/distributed/tria.h>
+#include <deal.II/grid/grid_generator.h>
+
+// ExaDG
+#include <exadg/grid/enum_types.h>
+
+namespace ExaDG
+{
+/**
+ * This function wraps around dealii::GridGenerator::subdivided_hyper_rectangle() taking an
+ * additional argument ElementType.
+ */
+void
+create_subdivided_hyper_rectangle(dealii::Triangulation<2> &        tria,
+                                  std::vector<unsigned int> const & repetitions,
+                                  dealii::Point<2> const &          p1,
+                                  dealii::Point<2> const &          p2,
+                                  ElementType const &               element_type,
+                                  bool const                        colorize = false)
+{
+  if(element_type == ElementType::Hypercube)
+  {
+    dealii::GridGenerator::subdivided_hyper_rectangle(tria, repetitions, p1, p2, colorize);
+  }
+  else if(element_type == ElementType::Simplex)
+  {
+    dealii::GridGenerator::subdivided_hyper_rectangle_with_simplices(
+      tria, repetitions, p1, p2, colorize);
+  }
+  else
+  {
+    AssertThrow(false,
+                dealii::ExcMessage("The function create_subdivided_hyper_rectangle() currently "
+                                   "supports ElementType::Hypercube and ElementType::Simplex."));
+  }
+}
+} // namespace ExaDG
+
+
+#endif /* INCLUDE_EXADG_GRID_HYPER_RECTANGLE_H_ */
