e3201a355f37162428a9482f1261a821549e1bc7
nfehn <nfehn@users.noreply.github.com>
2023-06-09 09:20:09 +0000

    deploy: e366c8bc5fef39e1c06354769f53a9d80524957b

--- search/functions_a.js
+++ search/functions_a.js
@@ -10,8 +10,8 @@ var searchData=
   ['set_5fcoefficients_7',['set_coefficients',['../classExaDG_1_1VariableCoefficients.html#ac2463a7113784b16e28c72fa8a091e4d',1,'ExaDG::VariableCoefficients']]],
   ['set_5fviscosity_8',['set_viscosity',['../classExaDG_1_1IncNS_1_1GeneralizedNewtonianModel.html#aa97bcec81031e00e48d670b25bb05d7d',1,'ExaDG::IncNS::GeneralizedNewtonianModel::set_viscosity()'],['../classExaDG_1_1IncNS_1_1TurbulenceModel.html#ab375dbb3b31526efa1250df78c3ab7d2',1,'ExaDG::IncNS::TurbulenceModel::set_viscosity()'],['../classExaDG_1_1IncNS_1_1ViscosityModelBase.html#a6de08076571b34dcf105b8d44a43a0d4',1,'ExaDG::IncNS::ViscosityModelBase::set_viscosity()']]],
   ['set_5fwrite_5fdata_5ftype_9',['set_write_data_type',['../classExaDG_1_1preCICE_1_1CouplingBase.html#aae21d6fc764dbd5051ec7c27cc38d11a',1,'ExaDG::preCICE::CouplingBase']]],
-  ['setup_10',['Setup',['../classdealspectrum_1_1Setup.html#ae48fcc6968bba36f4300ec7e88009717',1,'dealspectrum::Setup']]],
-  ['setup_11',['setup',['../classExaDG_1_1InterfaceCoupling.html#a7aa3cfb167de7d9384a7686e7e1f964e',1,'ExaDG::InterfaceCoupling']]],
+  ['setup_10',['setup',['../classExaDG_1_1InterfaceCoupling.html#a7aa3cfb167de7d9384a7686e7e1f964e',1,'ExaDG::InterfaceCoupling']]],
+  ['setup_11',['Setup',['../classdealspectrum_1_1Setup.html#ae48fcc6968bba36f4300ec7e88009717',1,'dealspectrum::Setup']]],
   ['spectralanalysis_12',['SpectralAnalysis',['../classdealspectrum_1_1SpectralAnalysis.html#a34a974fb314f0807d96902741be6e156',1,'dealspectrum::SpectralAnalysis']]],
   ['start_13',['start',['../classdealspectrum_1_1DealSpectrumTimer.html#a140b943f9881e4a1d917941f896cb472',1,'dealspectrum::DealSpectrumTimer']]],
   ['stop_14',['stop',['../classdealspectrum_1_1DealSpectrumTimer.html#ac7f6fa0118b49c8c58f1b8357cc2a371',1,'dealspectrum::DealSpectrumTimer']]]
