a4b40f103564e1cbc83e762b353ae0f74fd6cedd
Niklas Fehn <niklas.fehn@uni-a.de>
2023-07-25 16:42:59 +0200

    add header

--- include/exadg/incompressible_navier_stokes/spatial_discretization/spatial_operator_base.cpp
+++ include/exadg/incompressible_navier_stokes/spatial_discretization/spatial_operator_base.cpp
@@ -27,6 +27,7 @@
 #include <exadg/incompressible_navier_stokes/preconditioners/multigrid_preconditioner_projection.h>
 #include <exadg/incompressible_navier_stokes/spatial_discretization/spatial_operator_base.h>
 #include <exadg/operators/finite_element.h>
+#include <exadg/operators/quadrature.h>
 #include <exadg/solvers_and_preconditioners/preconditioners/block_jacobi_preconditioner.h>
 #include <exadg/solvers_and_preconditioners/preconditioners/inverse_mass_preconditioner.h>
 #include <exadg/solvers_and_preconditioners/preconditioners/jacobi_preconditioner.h>
