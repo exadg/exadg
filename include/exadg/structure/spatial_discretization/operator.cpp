2bc30c4f04597da17ab2793d1003a870c10a019a
Richard Schussnig <richard.schussnig@uni-a.de>
2023-08-04 22:10:02 +0200

    use copy_from and omit superfluous locally_relelevant_dofs setup

--- include/exadg/structure/spatial_discretization/operator.cpp
+++ include/exadg/structure/spatial_discretization/operator.cpp
@@ -131,18 +131,9 @@ Operator<dim, Number>::distribute_dofs()
                                                *this->grid,
                                                dof_handler);
 
-  affine_constraints_periodicity_and_hanging_nodes.close();
-
   // copy periodicity and hanging node constraints, and add further constraints stemming from
   // Dirichlet boundary conditions
-  affine_constraints.clear();
-
-  dealii::IndexSet locally_relevant_dofs;
-  dealii::DoFTools::extract_locally_relevant_dofs(dof_handler, locally_relevant_dofs);
-  affine_constraints.reinit(locally_relevant_dofs);
-
-  // affine constraints to copy from need to be non-closed to add further constraints
-  affine_constraints.merge(affine_constraints_periodicity_and_hanging_nodes);
+  affine_constraints.copy_from(affine_constraints_periodicity_and_hanging_nodes);
 
   // use all the component masks defined by the user
   std::map<dealii::types::boundary_id, dealii::ComponentMask> map_bid_to_mask =
@@ -165,6 +156,9 @@ Operator<dim, Number>::distribute_dofs()
   // call deal.II utility function to add Dirichlet constraints
   add_homogeneous_dirichlet_constraints(affine_constraints, dof_handler, map_bid_to_mask);
 
+  // compress constraints *once after* complete setup, since affine constraints to copy from need to
+  // be non-closed to add further constraints
+  affine_constraints_periodicity_and_hanging_nodes.close();
   affine_constraints.close();
 
   pcout << std::endl
