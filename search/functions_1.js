f0cf51c094de345989a8c45100c08b673cee516f
nfehn <nfehn@users.noreply.github.com>
2023-08-14 09:51:30 +0000

    deploy: f35d138373548eac4d07ae3947e792f4472ec97f

--- search/functions_1.js
+++ search/functions_1.js
@@ -4,7 +4,7 @@ var searchData=
   ['calculate_5fenergy_5fspectrum_1',['calculate_energy_spectrum',['../classdealspectrum_1_1SpectralAnalysis.html#a192f1677ecc3281dfe9f19628d06b14f',1,'dealspectrum::SpectralAnalysis']]],
   ['calculate_5ffilter_5fwidth_2',['calculate_filter_width',['../classExaDG_1_1IncNS_1_1TurbulenceModel.html#ae739ac5631983a50b2a5bf6e664c50b5',1,'ExaDG::IncNS::TurbulenceModel']]],
   ['calculate_5fgradient_5fflux_3',['calculate_gradient_flux',['../classExaDG_1_1IncNS_1_1Operators_1_1ViscousKernel.html#a231e82673d1fd134748a4605aed289dd',1,'ExaDG::IncNS::Operators::ViscousKernel']]],
-  ['calculate_5fminimum_5fvertex_5fdistance_4',['calculate_minimum_vertex_distance',['../namespaceExaDG.html#ac9fadac802fcfe05f9d3ad88a8636d02',1,'ExaDG']]],
+  ['calculate_5fminimum_5fvertex_5fdistance_4',['calculate_minimum_vertex_distance',['../namespaceExaDG.html#ab4e8cd1d7dfdaf430424ebe151e08bfe',1,'ExaDG']]],
   ['calculate_5fvalue_5fflux_5',['calculate_value_flux',['../classExaDG_1_1IncNS_1_1Operators_1_1ViscousKernel.html#a068b47893755fa37fe567bf3c01c021a',1,'ExaDG::IncNS::Operators::ViscousKernel']]],
   ['clear_6',['clear',['../classExaDG_1_1TimerTree.html#a2886f1d3bbd4e82cb54458d94fe82979',1,'ExaDG::TimerTree']]],
   ['create_5fdirectories_7',['create_directories',['../namespaceExaDG.html#ae37213007b3d71515b9d35a598e17e22',1,'ExaDG']]],
