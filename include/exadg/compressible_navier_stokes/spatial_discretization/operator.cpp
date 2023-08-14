c914720f983c580ade12770b49cfcb80f00d6020
Niklas Fehn <niklas.fehn@uni-a.de>
2023-08-14 15:15:13 +0200

    apply changes to CompNS module

--- include/exadg/compressible_navier_stokes/spatial_discretization/operator.cpp
+++ include/exadg/compressible_navier_stokes/spatial_discretization/operator.cpp
@@ -251,6 +251,29 @@ Operator<dim, Number>::setup_operators()
 
 template<int dim, typename Number>
 void
+Operator<dim, Number>::setup()
+{
+  // initialize MatrixFree and MatrixFreeData
+  std::shared_ptr<dealii::MatrixFree<dim, Number>> mf =
+    std::make_shared<dealii::MatrixFree<dim, Number>>();
+  std::shared_ptr<MatrixFreeData<dim, Number>> mf_data =
+    std::make_shared<MatrixFreeData<dim, Number>>();
+
+  fill_matrix_free_data(*mf_data);
+
+  mf->reinit(get_mapping(),
+             mf_data->get_dof_handler_vector(),
+             mf_data->get_constraint_vector(),
+             mf_data->get_quadrature_vector(),
+             mf_data->data);
+
+  // Subsequently, call the other setup function with MatrixFree/MatrixFreeData objects as
+  // arguments.
+  this->setup(mf, mf_data);
+}
+
+template<int dim, typename Number>
+void
 Operator<dim, Number>::setup(std::shared_ptr<dealii::MatrixFree<dim, Number> const> matrix_free_in,
                              std::shared_ptr<MatrixFreeData<dim, Number> const> matrix_free_data_in)
 {
