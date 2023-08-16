167738fe72c087c60d0f38658c889c95b66328c1
nfehn <nfehn@users.noreply.github.com>
2023-08-16 08:32:25 +0000

    deploy: cdf431c0bb9bd82f7bad28f847fa2bf8728d381a

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,14 +1,15 @@
 var searchData=
 [
-  ['calculate_5fcharacteristic_5felement_5flength_0',['calculate_characteristic_element_length',['../namespaceExaDG.html#a234ab5185472bd283373a7dee57fb584',1,'ExaDG']]],
+  ['calculate_5fcharacteristic_5felement_5flength_0',['calculate_characteristic_element_length',['../namespaceExaDG.html#afca01d4aad096a68644bb099f38840bb',1,'ExaDG']]],
   ['calculate_5fenergy_5fspectrum_1',['calculate_energy_spectrum',['../classdealspectrum_1_1SpectralAnalysis.html#a192f1677ecc3281dfe9f19628d06b14f',1,'dealspectrum::SpectralAnalysis']]],
   ['calculate_5ffilter_5fwidth_2',['calculate_filter_width',['../classExaDG_1_1IncNS_1_1TurbulenceModel.html#ae739ac5631983a50b2a5bf6e664c50b5',1,'ExaDG::IncNS::TurbulenceModel']]],
   ['calculate_5fgradient_5fflux_3',['calculate_gradient_flux',['../classExaDG_1_1IncNS_1_1Operators_1_1ViscousKernel.html#a231e82673d1fd134748a4605aed289dd',1,'ExaDG::IncNS::Operators::ViscousKernel']]],
-  ['calculate_5fminimum_5fvertex_5fdistance_4',['calculate_minimum_vertex_distance',['../namespaceExaDG.html#ab4e8cd1d7dfdaf430424ebe151e08bfe',1,'ExaDG']]],
-  ['calculate_5fvalue_5fflux_5',['calculate_value_flux',['../classExaDG_1_1IncNS_1_1Operators_1_1ViscousKernel.html#a068b47893755fa37fe567bf3c01c021a',1,'ExaDG::IncNS::Operators::ViscousKernel']]],
-  ['clear_6',['clear',['../classExaDG_1_1TimerTree.html#a2886f1d3bbd4e82cb54458d94fe82979',1,'ExaDG::TimerTree']]],
-  ['create_5fdirectories_7',['create_directories',['../namespaceExaDG.html#ae37213007b3d71515b9d35a598e17e22',1,'ExaDG']]],
-  ['create_5ffinite_5felement_8',['create_finite_element',['../namespaceExaDG.html#a4ee9607ef908f9a2d5599d52a185abce',1,'ExaDG']]],
-  ['create_5fquadrature_9',['create_quadrature',['../namespaceExaDG.html#a32fb3bfa0090de5af5646a74c84160ba',1,'ExaDG']]],
-  ['create_5fsubdivided_5fhyper_5frectangle_10',['create_subdivided_hyper_rectangle',['../namespaceExaDG.html#a11fdfa75b66139f3547f5ed4d7c1ef58',1,'ExaDG']]]
+  ['calculate_5fhigh_5forder_5felement_5flength_4',['calculate_high_order_element_length',['../namespaceExaDG.html#aab8cd107751c9426891494c743716b12',1,'ExaDG']]],
+  ['calculate_5fminimum_5fvertex_5fdistance_5',['calculate_minimum_vertex_distance',['../namespaceExaDG.html#ab4e8cd1d7dfdaf430424ebe151e08bfe',1,'ExaDG']]],
+  ['calculate_5fvalue_5fflux_6',['calculate_value_flux',['../classExaDG_1_1IncNS_1_1Operators_1_1ViscousKernel.html#a068b47893755fa37fe567bf3c01c021a',1,'ExaDG::IncNS::Operators::ViscousKernel']]],
+  ['clear_7',['clear',['../classExaDG_1_1TimerTree.html#a2886f1d3bbd4e82cb54458d94fe82979',1,'ExaDG::TimerTree']]],
+  ['create_5fdirectories_8',['create_directories',['../namespaceExaDG.html#ae37213007b3d71515b9d35a598e17e22',1,'ExaDG']]],
+  ['create_5ffinite_5felement_9',['create_finite_element',['../namespaceExaDG.html#a4ee9607ef908f9a2d5599d52a185abce',1,'ExaDG']]],
+  ['create_5fquadrature_10',['create_quadrature',['../namespaceExaDG.html#a32fb3bfa0090de5af5646a74c84160ba',1,'ExaDG']]],
+  ['create_5fsubdivided_5fhyper_5frectangle_11',['create_subdivided_hyper_rectangle',['../namespaceExaDG.html#a11fdfa75b66139f3547f5ed4d7c1ef58',1,'ExaDG']]]
 ];
