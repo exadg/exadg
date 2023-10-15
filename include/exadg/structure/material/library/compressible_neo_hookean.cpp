e3644657f10be223a5f65f885da9654146ae5d82
Richard Schussnig <richard.schussnig@uni-a.de>
2023-10-15 13:25:51 +0200

    refactor compNH do_set_cell_lin_data

--- include/exadg/structure/material/library/compressible_neo_hookean.cpp
+++ include/exadg/structure/material/library/compressible_neo_hookean.cpp
@@ -92,26 +92,19 @@ CompressibleNeoHookean<dim, Number>::CompressibleNeoHookean(
         kirchhoff_stress_coefficients.initialize(matrix_free, quad_index, false, false);
         kirchhoff_stress_coefficients.set_coefficients(zero_tensor);
       }
+      else
+      {
+        F_inv_coefficients.initialize(matrix_free, quad_index, false, false);
+        F_inv_coefficients.set_coefficients(get_identity<dim, Number>());
+
+        C_inv_coefficients.initialize(matrix_free, quad_index, false, false);
+  		C_inv_coefficients.set_coefficients(get_identity<dim, Number>());
+      }
 
       if(force_material_residual or not spatial_integration)
       {
-        second_piola_kirchhoff_stress_coefficients.initialize(matrix_free,
-                                                              quad_index,
-                                                              false,
-                                                              false);
+        second_piola_kirchhoff_stress_coefficients.initialize(matrix_free, quad_index, false, false);
         second_piola_kirchhoff_stress_coefficients.set_coefficients(zero_tensor);
-
-        F_inv_coefficients.initialize(matrix_free,
-                                      quad_index,
-                                      false,
-                                      false);
-        F_inv_coefficients.set_coefficients(get_identity<dim, Number>());
-
-        C_inv_coefficients.initialize(matrix_free,
-                                      quad_index,
-                                      false,
-                                      false);
-		C_inv_coefficients.set_coefficients(get_identity<dim, Number>());
       }
 
       AssertThrow(cache_level < 3, dealii::ExcMessage("Cache level > 2 not implemented."));
@@ -127,60 +120,47 @@ CompressibleNeoHookean<dim, Number>::do_set_cell_linearization_data(
 {
   AssertThrow(cache_level < 3, dealii::ExcMessage("Cache level > 2 not implemented."));
 
-  if(spatial_integration)
-  {
-    for(unsigned int q = 0; q < integrator_lin->n_q_points; ++q)
-    {
-      tensor const F = get_F(integrator_lin->get_gradient(q));
-      scalar const J = determinant(F);
+	for(unsigned int q = 0; q < integrator_lin->n_q_points; ++q)
+	{
+	  tensor const F = get_F(integrator_lin->get_gradient(q));
+	  scalar const J = determinant(F);
 
-      log_J_coefficients.set_coefficient_cell(cell, q, log(J));
-      one_over_J_coefficients.set_coefficient_cell(cell, q, 1.0 / J);
+	  log_J_coefficients.set_coefficient_cell(cell, q, log(J));
 
-      if(cache_level > 1)
-      {
-        deformation_gradient_coefficients.set_coefficient_cell(cell, q, F);
+	  if(spatial_integration)
+	  {
+		one_over_J_coefficients.set_coefficient_cell(cell, q, 1.0 / J);
+	  }
 
-        tensor const Grad_d_lin = subtract_identity<dim, Number>(F);
-        tensor const tau_lin =
-          this->kirchhoff_stress(Grad_d_lin, cell, q, true /* force_evaluation */);
-        kirchhoff_stress_coefficients.set_coefficient_cell(cell, q, tau_lin);
+	  if(cache_level > 1)
+	  {
+		deformation_gradient_coefficients.set_coefficient_cell(cell, q, F);
 
-        if(force_material_residual)
-        {
-          tensor const S_lin =
-            this->second_piola_kirchhoff_stress(Grad_d_lin, cell, q, true /* force_evaluation */);
-          second_piola_kirchhoff_stress_coefficients.set_coefficient_cell(cell, q, S_lin);
-        }
-      }
-    }
-  }
-  else
-  {
-    for(unsigned int q = 0; q < integrator_lin->n_q_points; ++q)
-    {
-      tensor const F = get_F(integrator_lin->get_gradient(q));
-      scalar const J = determinant(F);
+		tensor const Grad_d_lin = subtract_identity<dim, Number>(F);
 
-      log_J_coefficients.set_coefficient_cell(cell, q, log(J));
+		if(spatial_integration)
+		{
+			tensor const tau_lin =
+			  this->kirchhoff_stress(Grad_d_lin, cell, q, true /* force_evaluation */);
+			kirchhoff_stress_coefficients.set_coefficient_cell(cell, q, tau_lin);
+		}
+		else
+		{
+			tensor const F_inv = invert(F);
+			F_inv_coefficients.set_coefficient_cell(cell, q, F_inv);
 
-      if(cache_level > 1)
-      {
-        deformation_gradient_coefficients.set_coefficient_cell(cell, q, F);
+			tensor const C_inv = F_inv * transpose(F_inv);
+			C_inv_coefficients.set_coefficient_cell(cell, q, C_inv);
+		}
 
-        tensor const Grad_d_lin = subtract_identity<dim, Number>(F);
-        tensor const S_lin =
-          this->second_piola_kirchhoff_stress(Grad_d_lin, cell, q, true /* force_evaluation */);
-        second_piola_kirchhoff_stress_coefficients.set_coefficient_cell(cell, q, S_lin);
-
-        tensor const F_inv = invert(F);
-        F_inv_coefficients.set_coefficient_cell(cell, q, F_inv);
-
-        tensor const C_inv = F_inv * transpose(F_inv);
-        C_inv_coefficients.set_coefficient_cell(cell, q, C_inv);
-      }
-    }
-  }
+		if(force_material_residual or not spatial_integration)
+		{
+		  tensor const S_lin =
+			this->second_piola_kirchhoff_stress(Grad_d_lin, cell, q, true /* force_evaluation */);
+		  second_piola_kirchhoff_stress_coefficients.set_coefficient_cell(cell, q, S_lin);
+		}
+	  }
+	}
 }
 
 template<int dim, typename Number>
