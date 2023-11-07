271d9a2a1157840638cea57f850cf7b873d4ab67
nfehn <nfehn@users.noreply.github.com>
2023-11-07 13:51:15 +0000

    deploy: f9ea9672b31cb62df4619a469314f770ab718f13

--- search/classes_a.js
+++ search/classes_a.js
@@ -1,8 +1,9 @@
 var searchData=
 [
-  ['kineticenergycalculator_0',['KineticEnergyCalculator',['../classExaDG_1_1KineticEnergyCalculator.html',1,'ExaDG']]],
-  ['kineticenergycalculatordetailed_1',['KineticEnergyCalculatorDetailed',['../classExaDG_1_1IncNS_1_1KineticEnergyCalculatorDetailed.html',1,'ExaDG::IncNS']]],
-  ['kineticenergydata_2',['KineticEnergyData',['../structExaDG_1_1KineticEnergyData.html',1,'ExaDG']]],
-  ['kineticenergyspectrumcalculator_3',['KineticEnergySpectrumCalculator',['../classExaDG_1_1KineticEnergySpectrumCalculator.html',1,'ExaDG']]],
-  ['kineticenergyspectrumdata_4',['KineticEnergySpectrumData',['../structExaDG_1_1KineticEnergySpectrumData.html',1,'ExaDG']]]
+  ['kernel_0',['Kernel',['../classExaDG_1_1Acoustics_1_1Operators_1_1Kernel.html',1,'ExaDG::Acoustics::Operators']]],
+  ['kineticenergycalculator_1',['KineticEnergyCalculator',['../classExaDG_1_1KineticEnergyCalculator.html',1,'ExaDG']]],
+  ['kineticenergycalculatordetailed_2',['KineticEnergyCalculatorDetailed',['../classExaDG_1_1IncNS_1_1KineticEnergyCalculatorDetailed.html',1,'ExaDG::IncNS']]],
+  ['kineticenergydata_3',['KineticEnergyData',['../structExaDG_1_1KineticEnergyData.html',1,'ExaDG']]],
+  ['kineticenergyspectrumcalculator_4',['KineticEnergySpectrumCalculator',['../classExaDG_1_1KineticEnergySpectrumCalculator.html',1,'ExaDG']]],
+  ['kineticenergyspectrumdata_5',['KineticEnergySpectrumData',['../structExaDG_1_1KineticEnergySpectrumData.html',1,'ExaDG']]]
 ];
