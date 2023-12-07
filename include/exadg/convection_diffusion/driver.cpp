d09968d13dbb94a140802c3f80164759ba06e415
Niklas Fehn <niklas.fehn@uni-a.de>
2023-12-07 15:54:36 +0100

    apply changes to ConvDiff module

--- include/exadg/convection_diffusion/driver.cpp
+++ include/exadg/convection_diffusion/driver.cpp
@@ -57,11 +57,7 @@ Driver<dim, Number>::setup()
 
   pcout << std::endl << "Setting up scalar convection-diffusion solver:" << std::endl;
 
-  application->setup(grid, mapping);
-
-  // TODO: needs to be shifted to application in order to allow mappings realized as
-  // MappingDoFVector
-  multigrid_mappings = std::make_shared<MultigridMappings<dim, Number>>(mapping);
+  application->setup(grid, mapping, multigrid_mappings);
 
   bool const ale = application->get_parameters().ale_formulation;
 
