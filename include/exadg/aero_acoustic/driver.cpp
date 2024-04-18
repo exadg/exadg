6a024cd4c59a5c7d5e2266b34be6adc6e621e569
Johannes Heinz <43043310+jh66637@users.noreply.github.com>
2024-04-18 19:33:19 +0200

    include review

--- include/exadg/aero_acoustic/driver.cpp
+++ include/exadg/aero_acoustic/driver.cpp
@@ -214,14 +214,14 @@ Driver<dim, Number>::print_performance_results(double const total_time) const
 
   pcout << std::endl << "Throughput related to one macro time step:";
   print_throughput_unsteady(pcout,
-                            (double)DoFs_f + sub_dt_per_macro_dt * (double)DoFs_a,
+                            DoFs_f + DoFs_a,
                             time_solvers_side_by_side_avg,
                             acoustic->get_number_of_macro_time_steps(),
                             N_mpi_processes);
 
   pcout << std::endl << "Throughput related to one sub time step:";
   print_throughput_unsteady(pcout,
-                            DoFs_f + DoFs_a,
+                            (double)DoFs_f / sub_dt_per_macro_dt + (double)DoFs_a,
                             time_solvers_side_by_side_avg,
                             acoustic->get_number_of_sub_time_steps(),
                             N_mpi_processes);
