f9e4b4e0a90c4444e5117ce53ccf3e18a508b0d5
Martin Kronbichler <martin.kronbichler@rub.de>
2025-08-03 23:16:23 +0200

    Fix one more warning

--- include/exadg/solvers_and_preconditioners/multigrid/multigrid_preconditioner_base.cpp
+++ include/exadg/solvers_and_preconditioners/multigrid/multigrid_preconditioner_base.cpp
@@ -432,7 +432,7 @@ MultigridPreconditionerBase<dim, Number, MultigridNumber>::
 
       dealii::IndexSet const locally_relevant_dofs =
         dealii::DoFTools::extract_locally_relevant_dofs(*dof_handler);
-      affine_constraints_own->reinit(locally_relevant_dofs);
+      affine_constraints_own->reinit(dof_handler->locally_owned_dofs(), locally_relevant_dofs);
 
       // hanging nodes (needs to be done before imposing periodicity constraints and boundary
       // conditions)
