0f65a6172e2daad89b03c8ffd44a9d0eb0baba35
richardschu <richardschu@users.noreply.github.com>
2025-10-01 21:29:07 +0000

    deploy: a51f1bed640f00b4e99cb8d9673538623cdac67c

--- search/functions_a.js
+++ search/functions_a.js
@@ -1,8 +1,7 @@
 var searchData=
 [
-  ['read_5fwrite_5fdistributed_5fvector_0',['read_write_distributed_vector',['../namespaceExaDG.html#a5b0e6da132568638daf46851b20078a1',1,'ExaDG']]],
-  ['readheader_1',['readHeader',['../classdealspectrum_1_1Setup.html#a398598d40bbb1da95cde226559de2fe9',1,'dealspectrum::Setup']]],
-  ['refine_5flocal_2',['refine_local',['../namespaceExaDG.html#ac26ac75e95c9fbc45eaa00fa0c05a2b7',1,'ExaDG']]],
-  ['reinit_3',['reinit',['../classExaDG_1_1SolutionInterpolationBetweenTriangulations.html#a6e7d1f14bedd4b51c35525417a1feb20',1,'ExaDG::SolutionInterpolationBetweenTriangulations::reinit()'],['../classExaDG_1_1SolutionField.html#a11a769db2ab15d1be7a96acc1a2cd23b',1,'ExaDG::SolutionField::reinit()']]],
-  ['reload_5fold_5fstate_5fif_5frequired_4',['reload_old_state_if_required',['../classExaDG_1_1preCICE_1_1Adapter.html#a0ce4e88f107c66747490f6747705f4d7',1,'ExaDG::preCICE::Adapter']]]
+  ['readheader_0',['readHeader',['../classdealspectrum_1_1Setup.html#a398598d40bbb1da95cde226559de2fe9',1,'dealspectrum::Setup']]],
+  ['refine_5flocal_1',['refine_local',['../namespaceExaDG.html#ac26ac75e95c9fbc45eaa00fa0c05a2b7',1,'ExaDG']]],
+  ['reinit_2',['reinit',['../classExaDG_1_1SolutionInterpolationBetweenTriangulations.html#a6e7d1f14bedd4b51c35525417a1feb20',1,'ExaDG::SolutionInterpolationBetweenTriangulations::reinit()'],['../classExaDG_1_1SolutionField.html#a11a769db2ab15d1be7a96acc1a2cd23b',1,'ExaDG::SolutionField::reinit()']]],
+  ['reload_5fold_5fstate_5fif_5frequired_3',['reload_old_state_if_required',['../classExaDG_1_1preCICE_1_1Adapter.html#a0ce4e88f107c66747490f6747705f4d7',1,'ExaDG::preCICE::Adapter']]]
 ];
