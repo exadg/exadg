68cedccb501c449f3ef2194f758c0a04ad47c7a4
nfehn <nfehn@users.noreply.github.com>
2026-03-18 20:59:05 +0000

    deploy: df48f9a2e3ef93b232b72c1f7af1876a22819717

--- search/typedefs_0.js
+++ search/typedefs_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['fefaceintegrator_0',['FEFaceIntegrator',['../classExaDG_1_1preCICE_1_1CouplingBase.html#a78ce70584dbfb2bf4ce7b9f01c7f759c',1,'ExaDG::preCICE::CouplingBase::FEFaceIntegrator'],['../classExaDG_1_1preCICE_1_1QuadCoupling.html#a99b0cd396595b5f4ed54513f00c57cb0',1,'ExaDG::preCICE::QuadCoupling::FEFaceIntegrator']]]
+  ['fefaceintegrator_0',['FEFaceIntegrator',['../classExaDG_1_1preCICE_1_1CouplingBase.html#a78ce70584dbfb2bf4ce7b9f01c7f759c',1,'ExaDG::preCICE::CouplingBase::FEFaceIntegrator'],['../classExaDG_1_1preCICE_1_1QuadCoupling.html#a64c3dd6a3dbb6c94d1be72de33cca519',1,'ExaDG::preCICE::QuadCoupling::FEFaceIntegrator']]]
 ];
